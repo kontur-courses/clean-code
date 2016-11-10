8c42ea5252425760f2f640737879ed2f4c0758e0
Maxim Akulin <maxim.akulin@gmail.com>
2016-11-10 11:34:23 +0500

    added test for tokenizer

--- Markdown.Tests/Tokenizer_Should.cs
+++ Markdown.Tests/Tokenizer_Should.cs
@@ -1,10 +1,33 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Markdown.Tests
 {
     [TestFixture]
     public class Tokenizer_Should
     {
+        private Tokenizer tokenizer;
+        private List<IShell> shells;
+        [SetUp]
+        public void SetUp()
+        {
+            shells = new List<IShell>()
+            {
+                new SingleUnderline(),
+                new DoubleUnderline()
+            };
+            tokenizer = new Tokenizer();
+        }
+
+        [Test]
+        public void notChangeText_WhenNotFormatting()
+        {
+            var tokens = tokenizer.SplitToTokens("some text without shell", shells).ToList();
+            tokens.Count.Should().Be(1);
+            tokens.First().HasShell().Should().BeFalse();
+        }
 
     }
 }
