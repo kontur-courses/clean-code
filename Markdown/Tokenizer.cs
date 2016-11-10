b29f20fe5c3c57125533afe03f8f85b2fb58c166
Maxim Akulin <maxim.akulin@gmail.com>
2016-11-10 21:48:32 +0500

    renaming

--- Markdown/Tokenizer.cs
+++ Markdown/Tokenizer.cs
@@ -12,16 +12,16 @@ namespace Markdown
             var currentPosition = 0;
             while (currentPosition < text.Length)
             {
-                yield return GetNextToken(text, ref currentPosition);
+                yield return ReadNextToken(text, ref currentPosition);
             }
         }
 
-        public Token GetNextToken(string text, ref int startPosition)
+        public Token ReadNextToken(string text, ref int startPosition)
         {
             throw new NotImplementedException();
         }
 
-        public IShell GetNextShell(string text, ref int startPosition, List<IShell> shells )
+        public IShell ReadNextShell(string text, ref int startPosition, List<IShell> shells )
         {
             var currentPosition = startPosition;
             var prefix = new StringBuilder();
@@ -54,7 +54,7 @@ namespace Markdown
             return rightShell;
         }
 
-        public int GetEndPositionToken(string text, ref int startPosition, IShell currenShell)
+        public Tuple<int, IShell> GetEndPositionToken(string text, int startPosition, List<IShell> shells, IShell currentShell)
         {
             throw new NotImplementedException();
         }
