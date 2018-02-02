619d5793a70b2dfc906281c258ebf0c34ea15465
aminopyridin <aminopyrodin@gmail.com>
2018-02-02 11:25:02 +0500

    add repeat in controlDigit

--- js/ControlDigit/.solved/ControlDigitExtensions.js
+++ js/ControlDigit/.solved/ControlDigitExtensions.js
@@ -36,10 +36,19 @@ function getDigitsFromLeastSignificant (number) {
 }
 
 function createWeights () {
-    return Array.from(new Array(20), (val, index) => index % 2 === 0 ? 1 : 3);
+    const generator = repeat([1, 3])
+    return Array.from(new Array(20), () => generator.next().value);
 }
 
 function sumWithWeights (digits, weights) {
     const factoredDigits = digits.map((item, index) => item * weights[index]);
     return factoredDigits.reduce((sum, item) => sum + item);
+}
+
+function* repeat (items) {
+    while (true) {
+        for (let i = 0; i < items.length; i++) {
+            yield items[i];
+        }
+    }
 }
