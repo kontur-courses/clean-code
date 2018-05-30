50360a87b3ba0de7f5b01475bee58f1d096b1c85
Pavel Egorov <xoposhiy@mail.ru>
2018-05-30 16:52:05 +0500

    Improve ControlDigit task: Upc, Isbn, Snils

---
+++ cs/ControlDigit/Snils/SnilsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ControlDigit
+{
+	public static class SnilsExtensions
+	{
+		public static int CalculateSnils(this long number)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
