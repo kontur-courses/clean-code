fd5caa7fa51c63a0f1513b0e0dd202162be779f7
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-11-10 13:39:36 +0500

    Markdown project created

---
+++ cs/Markdown/Markdown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Markdown
+    {
+        public string Render(string markdownText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
