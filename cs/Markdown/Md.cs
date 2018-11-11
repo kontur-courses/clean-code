7870638046c0b9eb0e1ebca52496be0b07ab0252
Ilya Shakov <eliyy4hu@outlook.com>
2018-11-11 18:04:11 +0500

    Init

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace Markdown
+{
+    class Md
+    {
+        public Md()
+        {
+        }
+
+        public string Render(string markDownInput)
+        {
+            return string.Empty;
+        }
+    }
+
+    [TestFixture]
+    public class Md_Tests
+    {
+        private Md md;
+
+        [SetUp]
+        public void SetUp()
+        {
+            md = new Md();
+        }
+
+        [Test]
+        public void Render_ShouldReturnEmptyString_OnEmptyInput()
+        {
+            md.Render("").Should().BeEmpty();
+        }
+    }
+}
