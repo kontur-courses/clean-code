b1e44b8e4a4d6697939f2745ad4730e167f91839
DimaIvanovskiy <dima.ivanovskiy.99@mail.ru>
2018-11-11 16:35:20 +0500

    simple architecture

---
+++ cs/Markdown/Markdown/HTMLBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    class HtmlBuilder
+    {
+        private readonly Dictionary<TokenType, string> markupReplacement;
+
+        public HtmlBuilder(Dictionary<TokenType, string> markupReplacement)
+        {
+            this.markupReplacement = markupReplacement;
+        }
+
+        public string Build(IEnumerable<Token> tokens)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/Markdown/Markdown/Md.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    class Md
+    {
+        private readonly TokenParser tokenParser;
+
+        private readonly HtmlBuilder htmlBuilder;
+
+        public Md(TokenParser tokenParser, HtmlBuilder htmlBuilder)
+        {
+            this.tokenParser = tokenParser;
+            this.htmlBuilder = htmlBuilder;
+        }
+
+        public string Render(string text)
+        {
+            var tokens = tokenParser.Parse(text);
+            return htmlBuilder.Build(tokens);
+        }
+    }
+}
