25045a1fdebabe85cd3184da1873c4597b1cc9f0
Egor <egor.druzhinin97@gmail.com>
2018-11-14 23:41:24 +0500

    Intial tests added

---
+++ cs/MarkdownTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarkdownTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MarkdownTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06dd50b8-9540-4c40-8e70-b8e7cd069326")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ cs/MarkdownTests/TokenizerShould.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Markdown;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    [TestClass]
+    public class TokenizerShould
+    {
+        [TestMethod]
+        public void ReturnEmptyListOnEmptyString()
+        {
+            var markdownSource = "";
+
+            Tokenizer.Tokenize(markdownSource).Should().BeEquivalentTo(new List<Token>());
+        }
+
+        [TestMethod]
+        public void ReturnEmptyListOnNullString()
+        {
+            string markdownSource = null;
+
+            Tokenizer.Tokenize(markdownSource).Should().BeEquivalentTo(new List<Token>());
+        }
+
+        [Test]
+        public void ReturnOneTokenOnRawString()
+        {
+            var markdownSource = "Hello, world!";
+
+            Tokenizer.Tokenize(markdownSource).Should().BeEquivalentTo(new[]
+            {
+                new Token(0, 13, Tag.Raw)
+            });
+        }
+
+        [TestCase("hello _people", Description = "Single updaired underscore")]
+        [TestCase("hello __people", Description = "Single unpaired double-underscore")]
+        [TestCase("hel_lo__people", Description = "Mix of unpaired underscores")]
+        public void ReturnOnlyRawTokensOnUnpairedUnderscores(string markdownSource)
+        {
+            Tokenizer.Tokenize(markdownSource).Should().OnlyContain(token => token.Tag == Tag.Raw);
+        }
+
+        [TestCase("hello_ people_!", Description = "Opening underscore before space")]
+        [TestCase("hello_\tpeople_!", Description = "Opening underscore before whitespace")]
+        [TestCase("hello _people _!", Description = "Closing underscore after space")]
+        [TestCase("hello _people\t_!", Description = "Closing underscore after whitespace")]
+        public void ReturnOnlyRawTokensOnUnderscoresBeforeWhitespaces(string markdownSource)
+        {
+            Tokenizer.Tokenize(markdownSource).Should().OnlyContain(token => token.Tag == Tag.Raw);
+        }
+    }
+}
