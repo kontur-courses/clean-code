1dff3468c5df92c800e8e654597a4ee092ac2b0d
devmentality <nikita.volnov.1999@mail.ru>
2018-11-16 00:19:32 +0500

    Slow and dirty HtmlRender implementation

--- cs/Markdown/HtmlRenderer.cs
+++ cs/Markdown/HtmlRenderer.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using Markdown.Elements;
+using System.Linq;
 
 namespace Markdown
 {
-    class HtmlRenderer
+    public class HtmlRenderer
     {
+        private static readonly Dictionary<IElementType, (string open, string close)> htmlTagsMapping =
+            new Dictionary<IElementType, (string open, string close)>
+                {
+                    { RootElementType.Create(), (open: "", close: "") },
+                    { UnderscoreElementType.Create(), (open: "<em>", close: "</em>") },
+                    { DoubleUnderscoreElementType.Create(), (open: "<strong>", close: "</strong>") }
+                };
+    
         public static string RenderToHtml(MarkdownElement markdownRoot)
         {
-            throw new NotImplementedException();
+            var currentPosition = markdownRoot.StartPosition;
+            var result = "";
+
+            foreach (var innerElement in markdownRoot.InnerElements)
+            {
+                var innerElementIndicatorStart = innerElement.StartPosition - innerElement.ElementType.Indicator.Length;
+                var partBeforeInnerElement = markdownRoot.Markdown.Substring(
+                    currentPosition, innerElementIndicatorStart - currentPosition);
+
+                var innerElementRenderResult = RenderToHtml(innerElement);
+
+                result += partBeforeInnerElement + innerElementRenderResult;
+                currentPosition = innerElement.EndPosition + innerElement.ElementType.Indicator.Length;
+            }
+
+            var remainder = markdownRoot.Markdown.Substring(
+                currentPosition, markdownRoot.EndPosition - currentPosition);
+            result += remainder;
+
+            return WrapString(result, markdownRoot.ElementType);
+        }
+
+        private static string WrapString(string str, IElementType element)
+        {
+            return $"{htmlTagsMapping[element].open}{str}{htmlTagsMapping[element].close}";
         }
     }
 }
---
+++ cs/Markdown_Tests/HtmlRenderer_Should.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Markdown;
+using Markdown.Elements;
+
+namespace Markdown_Tests
+{
+    [TestFixture]
+    class HtmlRenderer_Should
+    {
+        [Test]
+        public void ReturnSameString_WhenOnlyRoot()
+        {
+            var content = "content";
+            var markdownRoot = new MarkdownElement(RootElementType.Create(), content, 0,
+                content.Length, new List<MarkdownElement>());
+            
+            var resultHtml = HtmlRenderer.RenderToHtml(markdownRoot);
+
+            Assert.AreEqual(content, resultHtml);
+        }
+
+        [Test]
+        public void ReturnWrappedToEm_WhenUnderscoreElement()
+        {
+            var content = "_content_";
+            var markdownRoot = new MarkdownElement(UnderscoreElementType.Create(), content, 
+                1, content.Length - 1, new List<MarkdownElement>());
+            var expected = "<em>content</em>";
+
+            var resultHtml = HtmlRenderer.RenderToHtml(markdownRoot);
+
+            Assert.AreEqual(expected, resultHtml);
+        }
+
+        [Test]
+        public void ReturnWrappedToStrong_WhenDoubleUnderscoreElement()
+        {
+            var content = "__content__";
+            var markdownRoot = new MarkdownElement(DoubleUnderscoreElementType.Create(), content,
+                2, content.Length - 2, new List<MarkdownElement>());
+            var expected = "<strong>content</strong>";
+
+            var resultHtml = HtmlRenderer.RenderToHtml(markdownRoot);
+
+            Assert.AreEqual(expected, resultHtml);
+        }
+
+        [Test]
+        public void ReturnRightMarkup_WhenRootHasOneInnerElement()
+        {
+            var content = "hello _world_";
+            var markdownRoot = new MarkdownElement(
+                RootElementType.Create(), content, 0, content.Length, 
+                new List<MarkdownElement>
+                {
+                    new MarkdownElement(
+                        UnderscoreElementType.Create(), content, 7, 12,
+                        new List<MarkdownElement>())
+                });
+
+            var expected = "hello <em>world</em>";
+            var actual = HtmlRenderer.RenderToHtml(markdownRoot);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnRightMarkup_WhenRootHasSeveralInnerElements()
+        {
+            var content = "__hello__ _world_";
+            var markdownRoot = new MarkdownElement(
+                RootElementType.Create(), content, 0, content.Length,
+                new List<MarkdownElement>
+                {
+                    new MarkdownElement(
+                        DoubleUnderscoreElementType.Create(), content, 2, 7,
+                        new List<MarkdownElement>()),
+                    new MarkdownElement(
+                        UnderscoreElementType.Create(), content, 11, 16,
+                        new List<MarkdownElement>())
+                });
+
+            var expected = "<strong>hello</strong> <em>world</em>";
+            var actual = HtmlRenderer.RenderToHtml(markdownRoot);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnRightMarkup_WhenDepthIsMoreThanOne()
+        {
+            var content = "__hello _world_.__";
+            var markdownRoot = new MarkdownElement(
+                RootElementType.Create(), content, 0, content.Length,
+                new List<MarkdownElement>
+                {
+                    new MarkdownElement(
+                        DoubleUnderscoreElementType.Create(), content, 2, content.Length - 2,
+                        new List<MarkdownElement>
+                        {
+                            new MarkdownElement(
+                                UnderscoreElementType.Create(), content, 9, 14,
+                                new List<MarkdownElement>())
+                        })
+                });
+
+            var expected = "<strong>hello <em>world</em>.</strong>";
+            var actual = HtmlRenderer.RenderToHtml(markdownRoot);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
