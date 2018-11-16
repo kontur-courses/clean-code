fa32c63ef3a07d1b1816d0dd4cb0019607445241
vaspahomov <vas2142553@gmail.com>
2018-11-16 23:10:18 +0500

    TokenFinderTests refactoring

--- cs/MarkdownTests/TokenFinderTests.cs
+++ cs/MarkdownTests/TokenFinderTests.cs
@@ -9,24 +9,29 @@ namespace MarkdownTests
     [TestFixture]
     class TokenFinderTests
     {
+
+        private List<TokenPosition> GetTokenPositions(int[] indexes)
+        {
+            var result = new List<TokenPosition>();
+            for (var i = 0; i < indexes.Length; i += 2)
+                result.Add(new TokenPosition(indexes[i], indexes[i + 1]));
+            return result;
+        }
+
         [TestCase("__f _d_ f__", new[] { 4, 6 }, new[] { 0, 9 }, TestName = "Should find simple and double token")]
         public void FindDoubleAndSimple(string paragraph, int[] simpleUnderscorePositions,
             int[] doubleUnderscorePositions)
         {
-            var expectedDoubleUnderscorePositions = new List<TokenPosition>();
-            var expectedSimpleUnderscorePositions = new List<TokenPosition>();
-            for (var i = 0; i < simpleUnderscorePositions.Length; i += 2)
-                expectedSimpleUnderscorePositions.Add(new TokenPosition(simpleUnderscorePositions[i],
-                    simpleUnderscorePositions[i + 1]));
-            for (var i = 0; i < doubleUnderscorePositions.Length; i += 2)
-                expectedDoubleUnderscorePositions.Add(new TokenPosition(doubleUnderscorePositions[i],
-                    doubleUnderscorePositions[i + 1]));
+            var expectedDoubleUnderscorePositions = GetTokenPositions(doubleUnderscorePositions);
+            var expectedSimpleUnderscorePositions = GetTokenPositions(simpleUnderscorePositions);
 
             var finder = new TokenFinder();
             var tokensWithPositions = finder.GetTokensWithPositions(paragraph);
 
-            tokensWithPositions.First(token => token.Key.Name == "simpleUnderscore").Value.ShouldBeEquivalentTo(expectedSimpleUnderscorePositions);
-            tokensWithPositions.First(token => token.Key.Name == "doubleUnderscore").Value.ShouldBeEquivalentTo(expectedDoubleUnderscorePositions);
+            tokensWithPositions.First(token => token.Key.Name == "simpleUnderscore").Value
+                .ShouldBeEquivalentTo(expectedSimpleUnderscorePositions);
+            tokensWithPositions.First(token => token.Key.Name == "doubleUnderscore").Value
+                .ShouldBeEquivalentTo(expectedDoubleUnderscorePositions);
         }
 
         [Test]
@@ -37,7 +42,8 @@ namespace MarkdownTests
             var paragraph = "__f__";
             var tokensWithPositions = tokenFinder.GetTokensWithPositions(paragraph);
 
-            tokensWithPositions.First(token => token.Key.Name == "doubleUnderscore").Value.First().ShouldBeEquivalentTo(new TokenPosition(0, 3));
+            tokensWithPositions.First(token => token.Key.Name == "doubleUnderscore").Value.First()
+                .ShouldBeEquivalentTo(new TokenPosition(0, 3));
         }
 
         [TestCase("_ff\\_", TestName = "Should not find finishing token with screening")]
@@ -55,18 +61,18 @@ namespace MarkdownTests
         }
 
         [TestCase("_ff_", new[] { 0, 3 }, TestName = "Should find simple token")]
-        [TestCase("_f _f_ _f_ f_", new[] { 0, 12, 3, 5, 7, 9 }, TestName = "Should find multiple token on one nesting level")]
+        [TestCase("_f _f_ _f_ f_", new[] { 0, 12, 3, 5, 7, 9 }, TestName =
+            "Should find multiple token on one nesting level")]
         [TestCase("_f _f _f_ f_ f_", new[] { 0, 14, 3, 11, 6, 8 }, TestName = "Should find multiple nesting")]
         public void FindSimpleUnderscore(string paragraph, int[] positions)
         {
-            var listOfExpectedPositions = new List<TokenPosition>();
-            for (var i = 0; i < positions.Length; i += 2)
-                listOfExpectedPositions.Add(new TokenPosition(positions[i], positions[i + 1]));
-            
+            var listOfExpectedPositions = GetTokenPositions(positions);
+
             var tokenFinder = new TokenFinder();
             var tokensWithPositions = tokenFinder.GetTokensWithPositions(paragraph);
 
-            tokensWithPositions.First(token => token.Key.Name == "simpleUnderscore").Value.ShouldBeEquivalentTo(listOfExpectedPositions);
+            tokensWithPositions.First(token => token.Key.Name == "simpleUnderscore").Value
+                .ShouldBeEquivalentTo(listOfExpectedPositions);
         }
     }
 }
