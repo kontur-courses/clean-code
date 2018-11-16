1cac3994954ea00d6befde27791ed5b76f8847fd
Ivan Plaksin <zero.netto.o@gmail.com>
2018-11-16 05:04:52 +0500

    Add main structures

---
+++ cs/Homework/Markdown/Markdown/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace Markdown
+{
+    public struct Tag
+    {
+        public readonly string MD;
+        public readonly string HTML;
+
+        public Tag(string md, string html)
+        {
+            MD = md;
+            HTML = html;
+        }
+    }
+}
---
+++ cs/Homework/Markdown/Markdown/Token.cs
@@ -0,0 +1,16 @@
+﻿namespace Markdown
+{
+    public class Token
+    {
+        public readonly Tag Tag;
+        public readonly int Position;
+        public readonly bool IsOpen;
+
+        public Token(Tag tag, int position, bool isOpen = true)
+        {
+            Tag = tag;
+            Position = position;
+            IsOpen = isOpen;
+        }
+    }
+}
