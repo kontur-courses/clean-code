d0da31c0970ba9c37a948ffcccedbcd2cab40cdb
Ivan Plaksin <zero.netto.o@gmail.com>
2018-11-16 05:06:01 +0500

    Add main MD Render logic + starter

---
+++ cs/Homework/Markdown/Markdown/Md.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public string Render(string mdParagraph)
+        {
+            var possibleTokens = TokenLogic.GetPossibleTokens(mdParagraph);
+
+            var validTokens = possibleTokens.GetValidTokens();
+            validTokens.RemoveStrongInEmphasis();
+
+            return GetHtmlParagraph(mdParagraph, validTokens);
+        }
+
+        private static string GetHtmlParagraph(string mdParagraph, List<Token> tokens)
+        {
+            tokens = tokens
+                .OrderBy(token => token.Position)
+                .ToList();
+            var htmlParagraph = new StringBuilder();
+            var previousPosition = 0;
+
+            foreach (var token in tokens)
+            {
+                htmlParagraph.Append(mdParagraph.Substring(previousPosition, token.Position - previousPosition));
+                htmlParagraph.Append(token.GetHTMLTag());
+                previousPosition = token.Position + token.Tag.MD.Length;
+            }
+
+            htmlParagraph.Append(mdParagraph.Substring(previousPosition, mdParagraph.Length -  previousPosition));
+
+            return htmlParagraph.ToString();
+        }
+    }
+}
---
+++ cs/Homework/Markdown/Markdown/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Markdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new StringBuilder();
+            var md = new Md();
+            using (StreamReader reader = new StreamReader(@"examples.md"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var mdParagraph = new StringBuilder();
+                    var currentString = reader.ReadLine();
+
+                    while (currentString.Length != 0)
+                    {
+                        mdParagraph.Append(currentString);
+                        if (!reader.EndOfStream)
+                            currentString = reader.ReadLine();
+                        else
+                            break;
+                    }
+                    result.Append(md.Render(mdParagraph.ToString()));
+                    if (reader.EndOfStream)
+                        result.Append("\n");
+                    else
+                        result.Append("\n\n");
+                    mdParagraph.Clear();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(@"examples.html"))
+            {
+                writer.Write(result);
+            }
+        }
+    }
+}
