0af48c969dd142778e6515e20bded0db5838feac
root <root@DESKTOP-8B3MLSU.localdomain>
2018-11-16 22:58:07 +0500

    add Md simple tests

---
+++ cs/Markdown/Md_should.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Markdown
+{
+    [TestFixture]
+    public class Md_should
+    {
+        private Md md;
+
+        [SetUp]
+        public void CreateMd() => md = new Md();
+
+        [Test]
+        public void Render_ShouldThrowArgumentException_WhenTextIsNull() =>
+            Assert.Throws<ArgumentException>(
+                () => md.Render(null),
+                "text should not be null"
+            );
+
+        [Test]
+        public void Render_ShouldReturnEmptyString_WhenTextIsEmpty() =>
+            md.Render("").Should().Be("");
+    }
+}
