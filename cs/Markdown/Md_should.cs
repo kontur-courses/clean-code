02c5d848a266137f55500b21526e9e6bb500acf2
root <root@DESKTOP-8B3MLSU.localdomain>
2018-11-16 23:18:53 +0500

    add specification tests

--- cs/Markdown/Md_should.cs
+++ cs/Markdown/Md_should.cs
@@ -23,5 +23,18 @@ namespace Markdown
         [TestCase("some text", TestName = "text not contains special symbols")]
         public void Render_ShouldReturnText_When(string text) =>
             md.Render(text).Should().Be(text);
+
+        [Test]
+        public void Render_ShouldMarkEmTag_WhenText_UnderlineWrapped() =>
+            md.Render("_some text_").Should().Be("<em>some text</em>");
+
+        [Test]
+        public void Render_ShouldMarkStrongTag_WhenText_DoubleUnderlineWrapped() =>
+            md.Render("__some text__").Should().Be("<strong>some text</strong>");
+
+        [Test]
+        public void Render_ShouldMarkEmInsideStrong() =>
+            md.Render("__ _some_ text__")
+              .Should().Be("<strong> <em>some</em> text</strong>");
     }
 }
