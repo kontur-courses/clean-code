5a579406216fe8752194a2936ad10ee4fd7a7ad7
vaspahomov <vas2142553@gmail.com>
2018-11-17 13:33:15 +0500

    SingleToken addition

---
+++ cs/Markdown/SingleToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    public class SingleToken
+    {
+        public SingleToken(TokenType tokenType, TokenPosition tokenPosition, LocationType locationType)
+        {
+            TokenType = tokenType;
+            TokenPosition = tokenPosition;
+            LocationType = locationType;
+        }
+
+        public TokenType TokenType { get; }
+        public TokenPosition TokenPosition { get; }
+        public LocationType LocationType { get; }
+    }
+}
