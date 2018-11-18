5c9f3dd5bd2efc6d302c77c036953fe28d68a3a6
Egor <egor.druzhinin97@gmail.com>
2018-11-19 02:30:26 +0500

    Added tests

--- cs/MarkdownTests/MarkdownTokenizerShould.cs
+++ cs/MarkdownTests/MarkdownTokenizerShould.cs
@@ -18,13 +18,15 @@ namespace MarkdownTests
             act.Should().ThrowExactly<ArgumentException>().And.Message.Should().Contain("can't be null or empty");
         }
 
-        [Test, TestCaseSource(nameof(EmphasizeTagTestCases))]
-        public void ProcessEmphasizeTagsCorrectly(string source, params Token[] expected)
+        #region Tag.Emphasize
+
+        [Test, TestCaseSource(nameof(WrapInEmphasizeTagTestCases))]
+        public void WrapInEmphasizeTag(string source, params Token[] expected)
         {
             MarkdownTokenizer.Tokenize(source).Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
         }
 
-        private static IEnumerable EmphasizeTagTestCases
+        private static IEnumerable WrapInEmphasizeTagTestCases
         {
             get
             {
@@ -34,7 +36,7 @@ namespace MarkdownTests
                     new Token(Tag.Raw, false, "people"),
                     new Token(Tag.Emphasize, false),
                     new Token(Tag.Raw, false, " hello"),
-                }).SetName("OnTagAtTheStart");
+                }).SetName("when tag is at the start");
 
                 yield return new TestCaseData("hello _people_", new[]
                 {
@@ -42,34 +44,176 @@ namespace MarkdownTests
                     new Token(Tag.Emphasize, true),
                     new Token(Tag.Raw, false, "people"),
                     new Token(Tag.Emphasize, false),
-                }).SetName("OnTagAtTheEnd");
+                }).SetName("when tag is at the end");
 
-                yield return new TestCaseData("_hello_ dear _people_ !", new[]
+                yield return new TestCaseData("start _middle_ end!", new[]
                 {
+                    new Token(Tag.Raw, false, "start "),
                     new Token(Tag.Emphasize, true),
-                    new Token(Tag.Raw, false, "hello"),
+                    new Token(Tag.Raw, false, "middle"),
                     new Token(Tag.Emphasize, false),
-                    new Token(Tag.Raw, false, " dear "),
+                    new Token(Tag.Raw, false, " end!"),
+                }).SetName("when tag is in the middle");
+
+                yield return new TestCaseData("Word _another_ pretty _word_ !", new[]
+                {
+                    new Token(Tag.Raw, false, "Word "),
                     new Token(Tag.Emphasize, true),
-                    new Token(Tag.Raw, false, "people"),
+                    new Token(Tag.Raw, false, "another"),
+                    new Token(Tag.Emphasize, false),
+                    new Token(Tag.Raw, false, " pretty "),
+                    new Token(Tag.Emphasize, true),
+                    new Token(Tag.Raw, false, "word"),
                     new Token(Tag.Emphasize, false),
                     new Token(Tag.Raw, false, " !"),
-                }).SetName("OnMultipleTags");
-
-                yield return new TestCaseData(@"hello \_people\_", new[]
-                {
-                    new Token(Tag.Raw, false, @"hello \_people\_"),
-                }).SetName("OnEscapedCharacters");
-
-                yield return new TestCaseData(".. _hello_ _people !", new[]
-                {
-                    new Token(Tag.Raw, false, ".. "),
-                    new Token(Tag.Emphasize, true),
-                    new Token(Tag.Raw, false, "hello"),
-                    new Token(Tag.Emphasize, false),
-                    new Token(Tag.Raw, false, " _people !"),
-                }).SetName("OnUnpairedTags");
+                }).SetName("with multiple tags");
             }
         }
+
+        [Test, TestCaseSource(nameof(NotWrapInEmphasizeTagTestCases))]
+        public void NotWrapInEmphasizeTag(string source, params Token[] expected)
+        {
+            MarkdownTokenizer.Tokenize(source).Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        private static IEnumerable NotWrapInEmphasizeTagTestCases()
+        {
+            yield return new TestCaseData(@"hello \_people_", new[]
+            {
+                new Token(Tag.Raw, false, "hello _people_"),
+            }).SetName("when first underscore is escaped");
+
+            yield return new TestCaseData(@"hello \_people", new[]
+            {
+                new Token(Tag.Raw, false, "hello _people"),
+            }).SetName("when first underscore is escaped without second underscore");
+
+            yield return new TestCaseData(@"hello _people\_", new[]
+            {
+                new Token(Tag.Raw, false, @"hello _people_"),
+            }).SetName("when second underscore is escaped");
+
+            yield return new TestCaseData(@"hello people\_", new[]
+            {
+                new Token(Tag.Raw, false, @"hello people_"),
+            }).SetName("when second underscore is escaped without first underscore");
+
+            yield return new TestCaseData(@"hello \_people\_", new[]
+            {
+                new Token(Tag.Raw, false, @"hello _people_"),
+            }).SetName("when both underscores are escaped");
+
+            yield return new TestCaseData("start _end", new[]
+            {
+                new Token(Tag.Raw, false, "start _end"),
+            }).SetName("when first underscore is unpaired");
+
+            yield return new TestCaseData("start_ end", new[]
+            {
+                new Token(Tag.Raw, false, "start_ end"),
+            }).SetName("when second underscore is unpaired");
+        }
+
+        #endregion
+
+
+        #region Tag.Strong
+
+        [Test, TestCaseSource(nameof(WrapInStrongTagTestCases))]
+        public void WrapInStrongTag(string source, params Token[] expected)
+        {
+            MarkdownTokenizer.Tokenize(source).Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        private static IEnumerable WrapInStrongTagTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("__people__ hello", new[]
+                {
+                    new Token(Tag.Strong, true),
+                    new Token(Tag.Raw, false, "people"),
+                    new Token(Tag.Strong, false),
+                    new Token(Tag.Raw, false, " hello"),
+                }).SetName("when tag is at the start");
+
+                yield return new TestCaseData("hello __people__", new[]
+                {
+                    new Token(Tag.Raw, false, "hello "),
+                    new Token(Tag.Strong, true),
+                    new Token(Tag.Raw, false, "people"),
+                    new Token(Tag.Strong, false),
+                }).SetName("when tag is at the end");
+
+                yield return new TestCaseData("start __middle__ end!", new[]
+                {
+                    new Token(Tag.Raw, false, "start "),
+                    new Token(Tag.Strong, true),
+                    new Token(Tag.Raw, false, "middle"),
+                    new Token(Tag.Strong, false),
+                    new Token(Tag.Raw, false, " end!"),
+                }).SetName("when tag is in the middle");
+
+                yield return new TestCaseData("Word __another__ pretty __word__ !", new[]
+                {
+                    new Token(Tag.Raw, false, "Word "),
+                    new Token(Tag.Strong, true),
+                    new Token(Tag.Raw, false, "another"),
+                    new Token(Tag.Strong, false),
+                    new Token(Tag.Raw, false, " pretty "),
+                    new Token(Tag.Strong, true),
+                    new Token(Tag.Raw, false, "word"),
+                    new Token(Tag.Strong, false),
+                    new Token(Tag.Raw, false, " !"),
+                }).SetName("with multiple tags");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(NotWrapInStrongTagTestCases))]
+        public void NotWrapInStrongTag(string source, params Token[] expected)
+        {
+            MarkdownTokenizer.Tokenize(source).Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        private static IEnumerable NotWrapInStrongTagTestCases()
+        {
+            yield return new TestCaseData(@"hello \__people__", new[]
+            {
+                new Token(Tag.Raw, false, @"hello __people__"),
+
+            }).SetName("when first double underscore is escaped");
+
+            yield return new TestCaseData(@"hello \__people", new[]
+            {
+                new Token(Tag.Raw, false, "hello __people"),
+            }).SetName("when first double underscore is escaped without second double underscore");
+
+            yield return new TestCaseData(@"hello __people\__", new[]
+            {
+                new Token(Tag.Raw, false, @"hello __people__"),
+            }).SetName("when second double underscore is escaped");
+
+            yield return new TestCaseData(@"hello people\__", new[]
+            {
+                new Token(Tag.Raw, false, @"hello people__"),
+            }).SetName("when second double  underscore is escaped without first double underscore");
+
+            yield return new TestCaseData(@"hello \__people\__", new[]
+            {
+                new Token(Tag.Raw, false, @"hello __people__"),
+            }).SetName("when both double underscores are escaped");
+
+            yield return new TestCaseData("start __end", new[]
+            {
+                new Token(Tag.Raw, false, "start __end"),
+            }).SetName("when first double underscore is unpaired");
+
+            yield return new TestCaseData("start__ end", new[]
+            {
+                new Token(Tag.Raw, false, "start__ end"),
+            }).SetName("when second double underscore is unpaired");
+        }
+
+        #endregion
     }
 }
