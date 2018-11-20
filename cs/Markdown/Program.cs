c0dd51f564e490f7058795ae5e6e38c15b1c88c1
Ilya Shakov <eliyy4hu@outlook.com>
2018-11-20 21:05:07 +0500

    add console interface

--- cs/Markdown/Program.cs
+++ cs/Markdown/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Markdown
+﻿using System;
+
+namespace Markdown
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+                return;
+            var mdInput = args[0];
+            var md = new Md();
+            var htmlResult = md.Render(mdInput);
+            Console.WriteLine(htmlResult);
         }
     }
 }
