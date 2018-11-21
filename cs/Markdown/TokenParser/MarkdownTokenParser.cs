3043cb8b6859531395d07dde95297094126fb589
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-11-21 15:33:17 +0500

    names refactoring

--- cs/Markdown/TokenParser/MarkdownTokenParser.cs
+++ cs/Markdown/TokenParser/MarkdownTokenParser.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Markdown.Data;
@@ -23,12 +22,12 @@ namespace Markdown.TokenParser
             var previousTokenType = TokenType.ParagraphStart;
             foreach (var symbol in text)
             {
-                var token = currentToken.ToString();
-                if (!IsPartOfToken(token, symbol, previousTokenType))
+                var tokenText = currentToken.ToString();
+                if (!IsPartOfToken(tokenText, symbol, previousTokenType))
                 {
-                    var newToken = GetToken(token, previousTokenType);
-                    yield return newToken;
-                    previousTokenType = newToken.Type;
+                    var token = GetToken(tokenText, previousTokenType);
+                    yield return token;
+                    previousTokenType = token.Type;
                     currentToken.Clear();
                 }
                 currentToken.Append(symbol);
@@ -36,32 +35,30 @@ namespace Markdown.TokenParser
             yield return GetToken(currentToken.ToString(), previousTokenType);
         }
 
-        private bool IsPartOfToken(string token, char nextSymbol, TokenType previousTokenType)
+        private bool IsPartOfToken(string tokenText, char nextSymbol, TokenType previousTokenType)
         {
-            if (token.Length == 0)
+            if (tokenText.Length == 0)
                 return true;
-            if (token == "\\" || previousTokenType == TokenType.EscapeSymbol)
+            if (tokenText == "\\" || previousTokenType == TokenType.EscapeSymbol)
                 return false;
-            if (tags.Any(tag => tag.StartsWith(token)))
-                return tags.Any(tag => tag.StartsWith(token + nextSymbol));
-            if (string.IsNullOrWhiteSpace(token))
+            if (tags.Any(tag => tag.StartsWith(tokenText)))
+                return tags.Any(tag => tag.StartsWith(tokenText + nextSymbol));
+            if (string.IsNullOrWhiteSpace(tokenText))
                 return char.IsWhiteSpace(nextSymbol);
             return char.IsLetterOrDigit(nextSymbol);
         }
 
-        private Token GetToken(string token, TokenType previousTokenType)
+        private Token GetToken(string tokenText, TokenType previousTokenType)
         {
-            if (string.IsNullOrEmpty(token))
-                throw new ArgumentException("token should be not empty string");
             if (previousTokenType == TokenType.EscapeSymbol)
-                return new Token(TokenType.Text, token);
-            if (token == "\\")
-                return new Token(TokenType.EscapeSymbol, token);
-            if (tags.Contains(token))
-                return new Token(TokenType.Tag, token);
-            if (string.IsNullOrWhiteSpace(token))
-                return new Token(TokenType.Space, token);
-            return new Token(TokenType.Text, token);
+                return new Token(TokenType.Text, tokenText);
+            if (tokenText == "\\")
+                return new Token(TokenType.EscapeSymbol, tokenText);
+            if (tags.Contains(tokenText))
+                return new Token(TokenType.Tag, tokenText);
+            if (string.IsNullOrWhiteSpace(tokenText))
+                return new Token(TokenType.Space, tokenText);
+            return new Token(TokenType.Text, tokenText);
         }
     }
 }
