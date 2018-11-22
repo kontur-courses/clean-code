4fb9bd7a35d939f02f67ad0eb1fb17647d637c79
alexeev <g.alexeev@wellsoft.pro>
2018-11-23 03:12:04 +0500

    сущности Прототип тега и Тег в маркдауне

---
+++ cs/Markdown/Tag/HtmlTagPrototype.cs
@@ -0,0 +1,11 @@
+﻿using Markdown.TagRendering;
+
+namespace Markdown.Tag
+{
+    public class HtmlTagPrototype
+    {
+        public TagNames TagName { get; set; }
+        public OpeningAndClosingTagPair<string, string> OpeningAndClosingTagPair { get; set; }
+        public string MarkdownStringOrSymbol { get; set; }
+    }
+}
