29736a2ee3e91e3b1e17086d99a6aa22dd70d02f
alexeev <g.alexeev@wellsoft.pro>
2018-11-23 03:11:24 +0500

    Перемещение, переименование, удаление классов и папок

---
+++ cs/Markdown/TagRendering/TagDetecter.cs
@@ -0,0 +1,35 @@
+﻿namespace Markdown.TagRendering
+{
+     class TagDetecter
+    {
+        public bool TryDetectOpening(string paragraph, int index)
+        {
+            return index + 1 < paragraph.Length 
+                   && paragraph[index + 1] != ' '   
+                   && !char.IsNumber(paragraph, index + 1);
+        }
+
+        public bool TryDetectClosing(string paragraph, int index)
+        {
+            return index > 0 
+                   && paragraph[index - 1] != ' ' 
+                   && !char.IsNumber(paragraph, index - 1); 
+        }
+
+        public bool EmTagDetectedIn(string paragraph, int index) => paragraph[index] == '_';
+
+        public bool StrongTagDetectedIn(string paragraph, int index)
+        {
+            return index < paragraph.Length - 1 
+                   && paragraph[index] == '_' 
+                   && paragraph[index + 1] == '_';
+        }
+        
+        public bool ShieldedTagDetected(string paragraph, int index)
+        {
+            return index < paragraph.Length - 1 
+                   && paragraph[index] == '\\' 
+                   && paragraph[index + 1] == '_';
+        }
+    }
+}
