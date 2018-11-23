1543aa3de352f3bd7b0126edbd0398947df170b3
Ivan Plaksin <zero.netto.o@gmail.com>
2018-11-23 11:00:37 +0500

    Added some tests + move to another solution

---
+++ cs/Homework/Markdown/MarkdownTests/MdParser_Should.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Markdown;
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    [TestFixture]
+    class MdParser_Should
+    {
+        [TestCase("", TestName = "Empty string")]
+        [TestCase(@"\_a_", TestName = "With escaping")]
+        [TestCase("_a", TestName = "No close tag")]
+        [TestCase("a_a", TestName = "Close tag with text part after")]
+        [TestCase("a _a ", TestName = "No closure")]
+        public void GetAllTokens_ReturnEmptyList(string rule)
+        {
+            var actual = MdParser.GetAllTokens(rule);
+
+            actual.Should().BeEmpty();
+        }
+
+        [TestCase("_a_", TestName = "Valid tag pair")]
+        [TestCase("_a_.", TestName = "Valid tag pair with punctuation after")]
+        [TestCase("_a_ ", TestName = "Valid tag pair with space after")]
+        [TestCase(" _a_ ", TestName = "Valid tag pair with space before")]
+        [TestCase("._a_", TestName = "Valid tag pair with punctuation before")]
+        public void GetAllTokens_ReturnToken(string rule)
+        {
+            var actual = MdParser.GetAllTokens(rule);
+
+            actual.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void GetNotIntersectingEndsTokens_ReturnLeftToken_EndsIntersecting()
+        {
+            var tokens = new List<Token>
+            {
+                new Token(MdParser.Tags[0], startPosition: 0, endPosition: 15),
+                new Token(MdParser.Tags[0], startPosition: 5, endPosition: 15)
+            };
+
+            var actual = MdParser.GetNotIntersectingEndsTokens(tokens);
+
+            actual.Should().HaveCount(1);
+            actual.First().Should().BeEquivalentTo(tokens.First());
+        }
+
+        [Test]
+        public void GetNotIntersectingEndsTokens_ReturnBoth_OneInAnother()
+        {
+            var tokens = new List<Token>
+            {
+                new Token(MdParser.Tags[0], startPosition: 0, endPosition: 15),
+                new Token(MdParser.Tags[0], startPosition: 5, endPosition: 10)
+            };
+
+            var actual = MdParser.GetNotIntersectingEndsTokens(tokens);
+
+            actual.Should().HaveCount(2);
+        }
+
+        [Test]
+        public void GetNotIntersectingEndsTokens_ReturnBoth_IntersectingButNotEnds()
+        {
+            var tokens = new List<Token>
+            {
+                new Token(MdParser.Tags[0], startPosition: 0, endPosition: 15),
+                new Token(MdParser.Tags[0], startPosition: 5, endPosition: 20)
+            };
+
+            var actual = MdParser.GetNotIntersectingEndsTokens(tokens);
+
+            actual.Should().HaveCount(2);
+        }
+
+        [Test]
+        public void RemoveNotWorkingNestedTokens_ReturnMainOnly_NestedNotWorking()
+        {
+            var tokens = new List<Token>
+            {
+                new Token(new Tag("_", "em"), startPosition: 0, endPosition: 20),
+                new Token(new Tag("__", "strong"), startPosition: 5, endPosition: 15)
+            };
+
+            var actual = MdParser.RemoveNotWorkingNestedTokens(tokens);
+
+            actual.Should().HaveCount(1);
+            actual.First().Should().BeEquivalentTo(tokens.First());
+        }
+    }
+}
---
+++ cs/Homework/Markdown/MarkdownTests/Render_Should.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Markdown;
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    [TestFixture]
+    public class Render_Should
+    {
+        private readonly Md md = new Md();
+
+        [TestCase("aaa", "aaa", TestName = "No tags")]
+        [TestCase("a __a__ a", "a <strong>a</strong> a", TestName = "Valid strong")]
+        [TestCase("a _a_ a", "a <em>a</em> a", TestName = "Valid emphasis")]
+        [TestCase("a __a__ _a_", "a <strong>a</strong> <em>a</em>", TestName = "Valid strong and emphasis")]
+        [TestCase("a _a_.", "a <em>a</em>.", TestName = "Valid with punctuation after")]
+        [TestCase("_a_", "<em>a</em>", TestName = "Valid with starting the line")]
+        [TestCase("_a __a__ a_", "<em>a __a__ a</em>", TestName = "Strong in emphasis")]
+        [TestCase("__a _a_ a__", "<strong>a <em>a</em> a</strong>", TestName = "Emphasis in strong")]
+        [TestCase(@"\_a\_", "_a_", TestName = "With escaping")]
+        [TestCase(@"\\\_a\\\_", "_a_", TestName = "With odd escaping count")]
+        [TestCase(@"\\_a_", "<em>a</em>", TestName = "With even escaping count")]
+        [TestCase("# _hello_\r", "<h1> <em>hello</em>", TestName = "Header not break nesting tags")]
+        [TestCase("# _hello_", "# <em>hello</em>", TestName = @"Without \r header not valid")]
+        public void ReturnTheSameString(string mdLine, string expected)
+        {
+            var actual = md.Render(mdLine);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
