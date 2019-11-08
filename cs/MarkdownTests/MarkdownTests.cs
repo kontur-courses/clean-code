28be49309648a23f102b60b86fca8394c453cffe
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2019-11-09 00:02:24 +0500

    Заготовка

---
+++ cs/MarkdownTests/MarkdownTests.cs
@@ -0,0 +1,25 @@
+﻿using Markdown;
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    [TestFixture]
+    public class MarkdownTests
+    {
+        private Md md;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            md = new Md();
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Render_DoesNotThrow_OnNullOrWhitespace(string mdText)
+        {
+            Assert.DoesNotThrow(() => md.Render(mdText));
+        }
+    }
+}
