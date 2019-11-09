a56b5ab3233b7a2d49dfd678f0b8758aacb43d52
Olga Bursina <bursina.olya@yandex.ru>
2019-11-09 12:12:22 +0500

    Added MarkDown structure

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public string Render(string markDownParagraph)
+        {
+            var tokens = GetTokens(markDownParagraph);
+            var tags = GetTags(tokens);
+            var tokensWithTags = GetTokensMapToTags(tokens, tags);
+
+            return GetHTMLCode(tokensWithTags);
+        }
+
+        private IEnumerable<Token> GetTokens(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<Tag> GetTags(IEnumerable<Token> tokens)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<Tuple<Tag, string>> GetTokensMapToTags(IEnumerable<Token> tokens, IEnumerable<Tag> tags)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GetHTMLCode(IEnumerable<Tuple<Tag, string>> tokensMapToTags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/Markdown/Tag.cs
@@ -0,0 +1,7 @@
+﻿namespace Markdown
+{
+    public class Tag
+    {
+        
+    }
+}
