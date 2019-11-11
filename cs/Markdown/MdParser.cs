b2c46421cf7c5391598d305f42fe8155bb3df4af
Olga Bursina <bursina.olya@yandex.ru>
2019-11-11 16:48:04 +0500

    Added MdParser

---
+++ cs/Markdown/MdParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class MdParser
+    {
+        private readonly string Text;
+
+        public MdParser(string text)
+        {
+            Text = text;
+        }
+        
+        public IEnumerable<Token> GetTokens()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
