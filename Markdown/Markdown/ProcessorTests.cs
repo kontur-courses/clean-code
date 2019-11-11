0f981df636b86159cf87ccb2734a6dfa992fda6a
HenrickBolender <40788127+HenrickBolender@users.noreply.github.com>
2019-11-11 16:17:53 +0500

    minor

--- Markdown/Markdown/ProcessorTests.cs
+++ Markdown/Markdown/ProcessorTests.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using FluentAssertions;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using NUnit.Framework.Internal;
 
 namespace Markdown
 {
-
     [TestFixture]
-    class ProcessorTests
+    internal class ProcessorTests
     {
         private Processor processor;
 
