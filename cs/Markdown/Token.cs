a735d40a6bba8afeee22e3e840615ae74a0ba571
Olga Bursina <bursina.olya@yandex.ru>
2019-11-11 16:45:22 +0500

    Update Token

--- cs/Markdown/Token.cs
+++ cs/Markdown/Token.cs
@@ -1,7 +1,32 @@
-﻿namespace Markdown
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown
 {
     public class Token
     {
+        public readonly int Position;
+        public readonly int Length;
+        public readonly string Value;
+        public readonly TagType TagType;
+
+        public Token(int position, string value, TagType tagType)
+        {
+            Position = position;
+            Value = value;
+            TagType = tagType;
+            Length = GetTokenLength(value, TagType);
+        }
+
+        public string ConvertToHtml()
+        {
+            throw new NotImplementedException();
+        }
         
+        private int GetTokenLength(string value, TagType tagType)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
