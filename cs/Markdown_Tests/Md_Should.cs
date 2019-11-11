4b939c53d02148dc038c3a96d5244d75cefd7b48
Crystalyx <skyres21@yandex.ru>
2019-11-12 02:26:29 +0500

    Tests

--- cs/Markdown_Tests/Md_Should.cs
+++ cs/Markdown_Tests/Md_Should.cs
@@ -1,7 +1,11 @@
+using System;
+using FluentAssertions;
+using Markdown;
 using NUnit.Framework;
 
 namespace Markdown_Tests
 {
+    [TestFixture]
     public class Md_Should
     {
         [SetUp]
@@ -10,9 +14,64 @@ namespace Markdown_Tests
         }
 
         [Test]
-        public void DullTest()
+        public void NotFall_WhenEmptyStringGiven()
         {
-            Assert.Pass();
+            Func<string> testDelegate = () => Md.Render("");
+            testDelegate.Should().NotThrow();
+        }
+
+        [Test]
+        public void NotReplaceEscapedGrounds()
+        {
+            Md.Render(@"\_testText\_").Should().Be(@"_testText_");
+        }
+
+        [Test]
+        public void NotReplaceGround_IfClosingGroundGoesAfterSpace()
+        {
+            Md.Render("_test _").Should().Be("_test _");
+        }
+
+        [Test]
+        public void NotReplaceGround_IfOpeningGroundGoesBeforeSpace()
+        {
+            Md.Render("_ test_").Should().Be("_ test_");
+        }
+
+        [Test]
+        public void NotReplaceGrounds_InsideTextWithDigitsDelimitedWithGrounds()
+        {
+            Md.Render("digit_12_3").Should().Be("digit_12_3");
+        }
+
+        [Test]
+        public void NotReplaceNotParedSelectionBorders()
+        {
+            Md.Render("__test _Text").Should().Be("__test _Text");
+        }
+
+        [Test]
+        public void NotReplaceTwoGroundsByStrongTag_InsideGrounds()
+        {
+            Md.Render("_test__a__Text_").Should().Be("<em>test__a__Text</em>");
+        }
+
+        [Test]
+        public void ReplaceGroundByEmTag()
+        {
+            Md.Render("_testText_").Should().Be("<em>testText</em>");
+        }
+
+        [Test]
+        public void ReplaceGroundsByEmTag_InsideTwoGrounds()
+        {
+            Md.Render("__test_a_Text__").Should().Be("<strong>test<em>a</em>Text</strong>");
+        }
+
+        [Test]
+        public void ReplaceTwoGroundsByStrongTag()
+        {
+            Md.Render("__testText__").Should().Be("<strong>testText</strong>");
         }
     }
 }
