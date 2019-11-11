e190c34b83322b23dc15369a3bc0d4cbaac30928
hitsedesen <hitsedesen@yandex.ru>
2019-11-11 13:21:41 +0500

    Create markdown project

---
+++ Markdown/Markdown/Md.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public static string Render(string mdSection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
