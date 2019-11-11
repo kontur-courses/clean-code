610322101dae2dd4acb04357d2a16cf0a9cd57b8
Olga Bursina <bursina.olya@yandex.ru>
2019-11-11 16:48:36 +0500

    Update Md

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Linq;
 
 namespace Markdown
 {
@@ -7,31 +6,11 @@ namespace Markdown
     {
         public string Render(string markDownParagraph)
         {
-            var tokens = GetTokens(markDownParagraph);
-            var tags = GetTags(tokens);
-            var tokensWithTags = GetTokensMapToTags(tokens, tags);
+            var parser = new MdParser(markDownParagraph);
+            var tokens = parser.GetTokens();
+            var htmlTokens = tokens.Select(token => token.ConvertToHtml());
 
-            return GetHTMLCode(tokensWithTags);
-        }
-
-        private IEnumerable<Token> GetTokens(string line)
-        {
-            throw new NotImplementedException();
-        }
-
-        private IEnumerable<Tag> GetTags(IEnumerable<Token> tokens)
-        {
-            throw new NotImplementedException();
-        }
-
-        private IEnumerable<Tuple<Tag, string>> GetTokensMapToTags(IEnumerable<Token> tokens, IEnumerable<Tag> tags)
-        {
-            throw new NotImplementedException();
-        }
-
-        private string GetHTMLCode(IEnumerable<Tuple<Tag, string>> tokensMapToTags)
-        {
-            throw new NotImplementedException();
+            return string.Join(" ", htmlTokens);
         }
     }
 }
