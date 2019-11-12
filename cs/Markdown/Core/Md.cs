11fd5bc0881f2a0c7c71c9533e96208c8f8eea93
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-12 16:41:00 +0500

    Добавлены обработчики тегов в поле класса Md

--- cs/Markdown/Core/Md.cs
+++ cs/Markdown/Core/Md.cs
@@ -1,11 +1,17 @@
-﻿namespace Markdown.Core
+﻿using Markdown.Core.Processors;
+
+namespace Markdown.Core
 {
     static class Md
     {
+        private static readonly BaseParser[] parsers = new BaseParser[]
+        {
+            new SingleUnderscoreProcessor(),
+            new DoubleUnderscoreProcessor()
+        };
+
         public static string Render(string markdown)
         {
-            var reader = new TokenReader(markdown);
-
             return null;
         }
     }
