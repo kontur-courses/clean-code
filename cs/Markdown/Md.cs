65f3d7b7137f5d4dc6d8d89dd56431ee8fca1cd2
balandin228 <55559705+balandin228@users.noreply.github.com>
2019-11-12 22:15:53 +0500

    make decomposition

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown
+{
+    public class Md
+    {
+        private Dictionary<Token, Token> TagsCollection { get; set; }
+
+        public Md()
+        {
+            TagsCollection = new Dictionary<Token, Token>();
+        }
+        public string Render(string paragraph)
+        {
+            var markdownTokens = TextToTokensParser.ParseTextToTokens(paragraph);
+            var htmlTokens = MarkdownToHtmlParser.ParseMarkdownToHtmlTokens(markdownTokens, TagsCollection);
+            var stringBuilder = new StringBuilder(paragraph);
+            foreach (var markdownToken in markdownTokens)
+                stringBuilder.Replace(markdownToken.Line, TagsCollection[markdownToken].Line, markdownToken
+                    .Start, markdownToken.Length);
+            return stringBuilder.ToString();
+        }
+    }
+}
