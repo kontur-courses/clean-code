fb1e3e4a47673a90467e15872e78c9e97540e8c5
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-12 16:38:59 +0500

    Добавлены обработчики тегов

---
+++ cs/Markdown/Core/Processors/SingleUnderscoreProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Markdown.Core.Tags;
+using Markdown.Core.Tags.HtmlTags;
+using Markdown.Core.Tags.MarkdownTags;
+
+namespace Markdown.Core.Processors
+{
+    class SingleUnderscoreProcessor : BaseParser
+    {
+        public override IHtmlTag HtmlTag { get; }
+        public override IMarkdownTag MarkdownTag { get; }
+
+        public SingleUnderscoreProcessor()
+        {
+            HtmlTag = new Em();
+            MarkdownTag = new SingleUnderscore();
+        }
+
+        protected override List<Token> FindMarkdownTags(string markdown)
+        {
+            return null;
+        }
+    }
+}
