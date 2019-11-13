82444c9c0690fbc50534f157bbed65d8d4fb98cb
Shcherbina99 <ej99@example.com>
2019-11-13 23:22:58 +0500

    Add Tag

---
+++ cs/Markdown/ITag.cs
@@ -0,0 +1,10 @@
+﻿namespace Markdown
+{
+    public interface ITag
+    {
+        string Name { get; }
+        string Start { get; }
+        string End { get; }
+        TagType TagType { get; }
+    }
+}
---
+++ cs/Markdown/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class Tag
+    {
+        public string Start { get; }
+        public string End { get; }
+        public IEnumerable<TagType> Children { get;}
+
+        public Tag(string start, string end, IEnumerable<TagType> children)
+        {
+            Start = start;
+            End = end;
+            Children = children;
+        }
+    }
+}
