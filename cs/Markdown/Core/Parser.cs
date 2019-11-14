8518fabef485af04391c05e9bb58050302b18f11
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-14 20:45:03 +0500

    Добавлен класс Parser
    Парсер принимает на вход текст и список правил, а на выходе выдает список токенов открыющихся\закрывающихся тегов

---
+++ cs/Markdown/Core/Parser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Markdown.Core.Rules;
+
+namespace Markdown.Core
+{
+    static class Parser
+    {
+        public static List<Token> Parse(string line, IEnumerable<IRule> rules)
+        {
+            rules = rules.OrderByDescending(r => r.SourceTag.Opening.Length);
+            var result = new List<Token>();
+            var tokenStack = new Stack<Token>();
+            for (var index = 0; index < line.Length; index++)
+            {
+                var currentRule = rules.FirstOrDefault(rule =>
+                    rule.SourceTag.Opening.Length + index <= line.Length &&
+                    rule.SourceTag.Opening == line.Substring(index, rule.SourceTag.Opening.Length));
+                if (currentRule == null) continue;
+
+                if (TagValidator.IsPossibleOpenningTag(line, index, currentRule.SourceTag))
+                {
+                    tokenStack.Push(new Token(index, currentRule.SourceTag, true));
+                    index += currentRule.SourceTag.Opening.Length - 1;
+                }
+                else if (TagValidator.IsPossibleClosingTag(line, index, currentRule.SourceTag))
+                {
+                    while (tokenStack.Count > 0 && tokenStack.Peek().Tag != currentRule.SourceTag)
+                        tokenStack.Pop();
+                    if (tokenStack.Count == 0) continue;
+
+                    result.Add(tokenStack.Pop());
+                    result.Add(new Token(index, currentRule.SourceTag, false));
+                    index += currentRule.SourceTag.Opening.Length - 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
