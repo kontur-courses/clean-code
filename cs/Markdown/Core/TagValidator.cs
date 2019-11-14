8ab880beabeef093e13a75dfac8926ca29b0f02a
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-14 20:42:06 +0500

    Добавлен класс TagValidator
    Методы класса выполняют функцию проверки по позиции в строке, может ли это быть началом открывающегося\закрывающегося тега

---
+++ cs/Markdown/Core/TagValidator.cs
@@ -0,0 +1,19 @@
+﻿using Markdown.Core.Tags;
+
+namespace Markdown.Core
+{
+    static class TagValidator
+    {
+        public static bool IsPossibleClosingTag(string line, int index, ITag tag)
+        {
+            return (index == line.Length - tag.Closing.Length || char.IsWhiteSpace(line[index + tag.Closing.Length])) &&
+                   (index == 0 || !char.IsWhiteSpace(line[index - 1]));
+        }
+
+        public static bool IsPossibleOpenningTag(string line, int index, ITag tag)
+        {
+            return (index == 0 || char.IsWhiteSpace(line[index - 1])) &&
+                   (index == line.Length - tag.Opening.Length || !char.IsWhiteSpace(line[index + tag.Opening.Length]));
+        }
+    }
+}
