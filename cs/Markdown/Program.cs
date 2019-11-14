eea594a6b08bdb2506053d31a22a6929f74e1956
EnergyNick <mr.clavsis@gmail.com>
2019-11-14 17:36:23 +0500

    Fix Program.cs disappearance

---
+++ cs/Markdown/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var cases = new [] {"__a__", "b_"};
+            var testing = new MarkdownTokenizer();
+            
+            var i = 1;
+            foreach (var str in cases)
+            {
+                Console.WriteLine($"Case {i}: ");
+                Console.WriteLine($"Current = \"{str}\", Length = {str.Length}");
+                foreach (var token in testing.SplitTextToTokens(str))
+                {
+                    Console.WriteLine(token);
+                }
+                Console.WriteLine();
+                i++;
+            }
+        }
+    }
+
+
+}
