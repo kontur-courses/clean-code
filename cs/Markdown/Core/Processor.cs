aa5432f92cdc369571fd69ec3c6f13a0ac6e4b3c
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-14 20:47:13 +0500

    Добавлен класс Processor
    Принимает на вход текст, список правил и токенов, а на выходе выдает текст, в котором токены в тексте заменены на тег установленный по правилу

---
+++ cs/Markdown/Core/Processor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Markdown.Core.Rules;
+
+namespace Markdown.Core
+{
+    static class Processor
+    {
+        public static string Process(string text, IEnumerable<IRule> rules, IEnumerable<Token> tokens)
+        {
+            tokens = tokens.OrderBy(token => token.StartPosition);
+            var renderedText = new StringBuilder(text);
+            var offsetAfterAdding = 0;
+
+            foreach (var token in tokens)
+            {
+                var currentRule = rules.FirstOrDefault(r => r.SourceTag == token.Tag);
+                if (currentRule == null)
+                    continue;
+                var resultTag = currentRule.ResultTag;
+                var tagValue = token.IsOpening ? resultTag.Opening : resultTag.Closing;
+
+                renderedText.Remove(token.StartPosition + offsetAfterAdding, token.Length);
+                renderedText.Insert(token.StartPosition + offsetAfterAdding, tagValue);
+
+                offsetAfterAdding += tagValue.Length - token.Length;
+            }
+
+            return renderedText.ToString();
+        }
+    }
+}
