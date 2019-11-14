0a471dc272627ba89f18306d9af8db9e08345ced
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-14 20:50:46 +0500

    Добавлены тесты на класс Md

--- cs/Markdown/Tests/MdTests.cs
+++ cs/Markdown/Tests/MdTests.cs
@@ -7,9 +7,17 @@ namespace Markdown.Tests
     class MdTests
     {
         [TestCase("_foo bar_", ExpectedResult = "<em>foo bar</em>", TestName = "When_AtBeginningAndEndText")]
-        [TestCase("fo_o b_ar", ExpectedResult = "fo<em>o b</em>ar", TestName = "When_InMiddleText")]
-        [TestCase("_fo_o b_ar_", ExpectedResult = "<em>fo</em>o b<em>ar</em>", TestName = "When_SeveralTimesInText")]
+        [TestCase("fo _o b_ ar", ExpectedResult = "fo <em>o b</em> ar", TestName = "When_InMiddleText")]
+        [TestCase("_fo _o b_ ar_", ExpectedResult = "<em>fo <em>o b</em> ar</em>", TestName = "When_SeveralTimesInText")]
         public string Render_ReplacedEm(string rawText)
+        {
+            return Md.Render(rawText);
+        }
+
+        [TestCase("__foo bar__", ExpectedResult = "<strong>foo bar</strong>", TestName = "When_AtBeginningAndEndText")]
+        [TestCase("fo __o b__ ar", ExpectedResult = "fo <strong>o b</strong> ar", TestName = "When_InMiddleText")]
+        [TestCase("__fo __o b__ ar__", ExpectedResult = "<strong>fo <strong>o b</strong> ar</strong>", TestName = "When_SeveralTimesInText")]
+        public string Render_ReplacedStrong(string rawText)
         {
             return Md.Render(rawText);
         }
