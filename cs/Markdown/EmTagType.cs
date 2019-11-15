9b5f455c4fbf61631e622b3adfbe364adb4ded83
Olga Bursina <bursina.olya@yandex.ru>
2019-11-15 21:55:14 +0500

    Updated TagTypes

--- cs/Markdown/EmTagType.cs
+++ cs/Markdown/EmTagType.cs
@@ -2,8 +2,26 @@
 {
     public class EmTagType : TagType
     {
-        private EmTagType() : base("<em>", "</em>", "_", "_")
+        public EmTagType() : base("<em>", "</em>", "_", "_")
         {
+        }
+
+        public static bool IsOpenedTag(string text, int position)
+        {
+            if (text[position] != '_' )
+                return false;
+            if (position == 0)
+                return char.IsLetter(text[position + 1]);
+            return position + 1 < text.Length && char.IsLetter(text[position + 1]) && text[position - 1] == ' ';
+        }
+
+        public static bool IsClosedTag(string text, int position)
+        {
+            if (text[position] != '_' || position < 1)
+                return false;
+            if (position == text.Length - 1)
+                return char.IsLetter(text[position - 1]);
+            return text[position + 1] == ' ' && char.IsLetter(text[position - 1]);
         }
     }
 }
--- cs/Markdown/StrongTagType.cs
+++ cs/Markdown/StrongTagType.cs
@@ -5,5 +5,23 @@
         public StrongTagType() : base("<strong>", "</strong>", "__", "__")
         {
         }
+
+        public static bool IsOpenedTag(string text, int position)
+        {
+            if (position + 2 >= text.Length || text[position] != '_' || text[position + 1] != '_')
+                return false;
+            if (position == 0)
+                return char.IsLetter(text[position + 2]);
+            return char.IsLetter(text[position + 2]) && text[position - 1] == ' ';
+        }
+
+        public static bool IsClosedTag(string text, int position)
+        {
+            if (text[position] != '_' || position < 1 || position + 1 >= text.Length || text[position + 1] != '_')
+                return false;
+            if (position + 1 == text.Length - 1)
+                return char.IsLetter(text[position - 1]);
+            return char.IsLetter(text[position - 1]) && text[position + 2] == ' ';
+        }
     }
 }
