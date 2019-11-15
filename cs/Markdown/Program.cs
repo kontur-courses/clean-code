550c22c09f6fa47b1c9836f97fd32d6f1b869bc3
Olga Bursina <bursina.olya@yandex.ru>
2019-11-15 21:58:24 +0500

    Added point of entry

---
+++ cs/Markdown/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var md = new Md();
+            Console.WriteLine(md.Render("dfdf _dfd_"));
+        }
+    }
+}
