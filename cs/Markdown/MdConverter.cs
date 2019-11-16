d6c71a3f745a777ca258f7dcd71bfadc7fb87981
Olga Bursina <bursina.olya@yandex.ru>
2019-11-17 00:57:54 +0500

    Updated MdConverter and tests

--- cs/Markdown/MdConverter.cs
+++ cs/Markdown/MdConverter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -21,29 +20,29 @@ namespace Markdown
             return tokens.Select(token => ConvertToHtml(token)).ToList();
         }
 
-        private string ConvertToHtml(Token token)
+        public string ConvertToHtml(Token token)
         {
             if (token.TagType is DefaultTagType || !HasPairTags(token))
                 return text.Substring(token.Position, token.Length);
             return GetHtmlEquivalent(token);
         }
 
-        private bool HasPairTags(Token token)
+        public bool HasPairTags(Token token)
         {
             var type = token.TagType;
-            var openingSymbol = type.OpeningSymbol;
-            var closingSymbol = type.ClosingSymbol;
-            return text.Substring(token.Position, openingSymbol.Length) == openingSymbol &&
-                   text.Substring(token.Position + token.Length - closingSymbol.Length + 1, closingSymbol.Length) ==
-                   closingSymbol;
+            var openingSymbol = type.MdOpeningTag;
+            var closingSymbol = type.MdClosingTag;
+            var tokenContent = text.Substring(token.Position, token.Length);
+            return tokenContent.StartsWith(openingSymbol) && tokenContent.EndsWith(closingSymbol);
         }
 
-        public string GetHtmlEquivalent(Token token)
+        private string GetHtmlEquivalent(Token token)
         {
             var type = token.TagType;
-            var openingSymbol = type.OpeningSymbol;
-            var closingSymbol = type.ClosingSymbol;
-            var content = text.Substring(token.Position + openingSymbol.Length, token.Length - closingSymbol.Length);
+            var openingSymbolLength = type.MdOpeningTag.Length;
+            var closingSymbolLength = type.MdClosingTag.Length;
+            var content = text.Substring(token.Position + openingSymbolLength,
+                token.Length - closingSymbolLength - openingSymbolLength);
             return new StringBuilder(type.HtmlOpeningTag).Append(content).Append(type.HtmlClosingTag).ToString();
         }
     }
---
+++ cs/MarkdownTests/MdConverter_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Markdown;
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    public class MdConverter_Tests
+    {
+        [TestCase("just text", 1)]
+        [TestCase("text _emTag_", 2)]
+        [TestCase("text _cutEmTag", 1)]
+        [TestCase("text __strongTag__ _cutEmTag", 2)]
+        [TestCase("_emStart __strongTagInEmTag__ emFinish_", 1)]
+        public void MdConverter_HasPairTags_RightDefinesPairTags(string text, int expectedPairTokens)
+        {
+            var tokens = new MdParser(text).GetTokens();
+            var converter = new MdConverter(tokens, text);
+            var pairTagsCount = tokens.Count(token => converter.HasPairTags(token));
+            pairTagsCount.Should().Be(pairTagsCount);
+        }
+        
+        [TestCase("just text", "just text")]
+        [TestCase("text _emTag_", "text ", "<em>emTag</em>")]
+        [TestCase("text _cutEmTag", "text ", "_cutEmTag")]
+        [TestCase("text __strongTag__ _cutEmTag", "text ", "<strong>strongTag</strong>", " ", "_cutEmTag")]
+        [TestCase("_emStart __strongTagInEmTag__ emFinish_", "<em>emStart __strongTagInEmTag__ emFinish</em>")]
+        public void MdConverter_ConvertToHtml_RightResult(string text, params string[] expectedHtmlEquivalents)
+        {
+            var tokens = new MdParser(text).GetTokens();
+            var converter = new MdConverter(tokens, text);
+            var htmlEquivalents = tokens.Select(token => converter.ConvertToHtml(token)).ToArray();
+            htmlEquivalents.Should().BeEquivalentTo(expectedHtmlEquivalents);
+        }
+    }
+}
