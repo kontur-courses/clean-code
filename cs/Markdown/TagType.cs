ed1ee2666c58d214114d7b36dbd154856ad0c2a8
Olga Bursina <bursina.olya@yandex.ru>
2019-11-17 00:56:38 +0500

    Updated

--- cs/Markdown/TagType.cs
+++ cs/Markdown/TagType.cs
@@ -4,15 +4,24 @@
     {
         public readonly string HtmlOpeningTag;
         public readonly string HtmlClosingTag;
-        public readonly string OpeningSymbol;
-        public readonly string ClosingSymbol;
+        public readonly string MdOpeningTag;
+        public readonly string MdClosingTag;
 
-        public TagType(string htmlOpeningTag, string htmlClosingTag, string openingSymbol, string closingSymbol)
+        public TagType(string htmlOpeningTag, string htmlClosingTag, string mdOpeningTag, string mdClosingTag)
         {
             HtmlOpeningTag = htmlOpeningTag;
             HtmlClosingTag = htmlClosingTag;
-            OpeningSymbol = openingSymbol;
-            ClosingSymbol = closingSymbol;
+            MdOpeningTag = mdOpeningTag;
+            MdClosingTag = mdClosingTag;
+        }
+        
+        public static TagType GetTagType(string text, int position)
+        {
+            if (EmTagType.IsOpenedTag(text, position) || EmTagType.IsClosedTag(text, position))
+                return new EmTagType();
+            if (StrongTagType.IsOpenedTag(text, position) || StrongTagType.IsClosedTag(text, position))
+                return new StrongTagType();
+            return new DefaultTagType();
         }
     }
 }
