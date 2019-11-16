3af2de06de52ab80af75e50b05ac93dc28ee4a21
Данил Панков <mistertimechip@mail.ru>
2019-11-16 19:48:52 +0300

    Symbol table to structure md to html processing

---
+++ cs/Markdown/MdProcessing/MdToHtmlSymbolTable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Markdown.MdProcessing
+{
+    public class MdToHtmlSymbolTable
+    {
+        private Dictionary<string, string> symbolToHtmlBeginning;
+        private Dictionary<string, string> symbolToHtmlEnding;
+
+        public MdToHtmlSymbolTable()
+        {
+            symbolToHtmlBeginning = new Dictionary<string, string>();
+            symbolToHtmlEnding = new Dictionary<string, string>();
+        }
+        
+        public void AddSymbol(string symbol, string htmlBeginning, string htmlEnding)
+        {
+            symbolToHtmlBeginning[symbol] = htmlBeginning;
+            symbolToHtmlEnding[symbol] = htmlEnding;
+        }
+
+        public string GetHtmlBeginning(string symbol)
+        {
+            return symbolToHtmlBeginning[symbol];
+        }
+
+        public string GetHtmlEnding(string symbol)
+        {
+            return symbolToHtmlEnding[symbol];
+        }
+    }
+}
