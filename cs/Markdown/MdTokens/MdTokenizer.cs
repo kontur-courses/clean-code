c7a42058a24c935b136ff4b7770b3fd5e3423b2b
Данил Панков <mistertimechip@mail.ru>
2019-11-16 17:59:21 +0300

    Wrote MdTokenizer using TDD

--- cs/Markdown/MdTokens/MdTokenizer.cs
+++ cs/Markdown/MdTokens/MdTokenizer.cs
@@ -1,15 +1,77 @@
 ﻿﻿using System;
 using System.Collections.Generic;
-using Markdown.Tokenizer;
+ using System.Linq;
+ using Markdown.Tokenizer;
 
 namespace Markdown.MdTokens
 {
     public class MdTokenizer : ITokenizer
     {
+        private HashSet<string> SpecialSymbols;
+
+        public MdTokenizer()
+        {
+            SpecialSymbols = new HashSet<string> {"_", "#", "__"};
+        }
         public IEnumerable<IToken> MakeTokens(string text)
         {
             if (text == null) throw new ArgumentNullException(nameof(text));
-            throw new NotImplementedException();
+            if (text == "") throw new ArgumentException("Text is empty");
+            return text.Split().Select(str => MakeToken(str));
+        }
+
+        private MdToken MakeToken(string text)
+        {
+            var beginningSymbol = GetBeginningSpecialSymbol(text);
+            var endingSymbol = GetEndingSpecialSymbol(text);
+            var content = GetContent(text, beginningSymbol, endingSymbol);
+            return new MdToken(content, beginningSymbol, endingSymbol);
+        }
+
+        private string GetBeginningSpecialSymbol(string text)
+        {
+            var specialSymbolBeginning = text[0].ToString();
+            var specialSymbolBeginningDoubles = "" + text[0] + text[1];
+            return GetSpecialSymbol(specialSymbolBeginning, specialSymbolBeginningDoubles);
+        }
+        
+        private string GetEndingSpecialSymbol(string text)
+        {
+            var specialSymbolEnding = text[text.Length - 1].ToString();
+            var specialSymbolEndingDoubles = "" + text[text.Length - 1] + text[text.Length - 2];
+            return GetSpecialSymbol(specialSymbolEnding, specialSymbolEndingDoubles);
+        }
+        
+        private string GetContent(string text, string beginningSymbol, string endingSymbol)
+        {
+            var content = text;
+            if (IsSymbolShielded(beginningSymbol))
+                content = content.Substring(1);
+            else if(beginningSymbol != "NONE")
+                content = content.Substring(beginningSymbol.Length);
+            if (IsSymbolShielded(endingSymbol))
+                content = content.Remove(content.Length - 2, 1);
+            else if(endingSymbol != "NONE")
+                content = content.Remove(content.Length - endingSymbol.Length);
+            return content;
+        }
+
+        private string GetSpecialSymbol(string singular, string doubled)
+        {
+            var specialSymbol = singular;
+            if (!SpecialSymbols.Contains(singular))
+                specialSymbol =  "NONE";
+            if (IsSymbolShielded(doubled))
+                specialSymbol = @"\";
+            else if (SpecialSymbols.Contains(doubled))
+                specialSymbol = doubled;
+            
+            return specialSymbol;
+        }
+
+        private bool IsSymbolShielded(string symbols)
+        {
+            return symbols.Contains(@"\");
         }
     }
 }
---
+++ cs/Markdown/Tests/MdTokenizer_should.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Markdown.MdTokens;
+using NUnit.Framework;
+
+namespace Markdown.Tests
+{
+    [TestFixture]
+    public class MdTokenizer_should
+    {
+        private MdTokenizer tokenizer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tokenizer = new MdTokenizer();
+        }
+
+        [Test]
+        public void MakeTokens_Should_ThrowArgumentNullException_When_ArgumentIsNull()
+        {
+            Following.Code(() => tokenizer.MakeTokens(null).First()).Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MakeTokens_Should_ThrowArgumentException_When_EmptyText()
+        {
+            Following.Code(() => tokenizer.MakeTokens("").First()).Should().Throw<ArgumentException>();
+        }
+
+        [TestCase("text", "text", "NONE", "NONE", TestName = "when no special symbols found")]
+        [TestCase("_text_", "text", "_", "_", TestName = "when text surrounded with emphasis")]
+        [TestCase("__text__", "text", "__", "__", TestName = "when text surrounded with bold symbols")]
+        [TestCase(@"\_text\_", @"_text_", @"\", @"\", TestName = "when text surrounded with shielded emphasis")]
+        [TestCase("ipsumdolor_sit_23_", "ipsumdolor_sit_23", "NONE", "_", TestName = "when text with emphasis and numbers")]
+        [TestCase("text_", "text", "NONE", "_", TestName = "when text with singular emphasis and the end")]
+        [TestCase("_text", "text", "_", "NONE", TestName = "when text with singular emphasis and the beginning")]
+        [TestCase("text__", "text", "NONE", "__", TestName = "when text with singular bold symbol and the end")]
+        [TestCase("__text", "text", "__", "NONE", TestName = "when text with singular bold symbol and the beginning")]
+        [TestCase("b_aa__a_c", "b_aa__a_c", "NONE", "NONE", TestName = "when text with incorrect special symbols")]
+        public void MakeTokens_Should_ParseToken(string text, 
+            string expectedContent, 
+            string expectedSpecialSymbolBeginning, 
+            string expectedSpecialSymbolEnding)
+        {
+            var actualToken = (MdToken)tokenizer.MakeTokens(text).ToList().First(); 
+            var expectedToken = new MdToken(expectedContent, expectedSpecialSymbolBeginning, expectedSpecialSymbolEnding);
+            actualToken.Should().BeEquivalentTo(expectedToken);
+        }
+        
+        [Test]
+        public void MakeTokens_Should_ReturnSeveralTokens_When_TextWithSpaces()
+        {
+            tokenizer.MakeTokens("abc abc").Count().Should().Be(2);
+        }
+        
+        [Test]
+        public void MakeTokens_Should_ReturnSeveralEmptyTokens_When_TextWithSpacesAndNoSpecialSymbols()
+        {
+            var tokens = tokenizer.MakeTokens("abc abc").ToList();
+            var firstToken = tokens.First();
+            var expectedToken = new MdToken("abc", "NONE", "NONE");
+            firstToken.Should().BeEquivalentTo(expectedToken);
+        }
+        
+        [Test]
+        public void MakeTokens_Should_ReturnSeveralTokensWithEmphasisToken_When_TextWithSpacesAndEmphasis()
+        {
+            var tokens = tokenizer.MakeTokens("_abc_ abc").ToList();
+            var firstToken = tokens.First();
+            var expectedToken = new MdToken("abc", "_", "_");
+            firstToken.Should().BeEquivalentTo(expectedToken);
+        }
+        
+        [Test]
+        public void MakeTokens_Should_ReturnSeveralTokensWithEmphasis_When_TextWithSeveralTokensWithEmphasis()
+        {
+            var tokens = tokenizer.MakeTokens("_abc_ _abc_").ToList();
+            var lastToken = tokens.Last();
+            var expectedToken = new MdToken("abc", "_", "_");
+            lastToken.Should().BeEquivalentTo(expectedToken);
+        }
+        
+        [Test]
+        public void MakeTokens_Should_ReturnSeveralDifferentTokens_When_TextWithSeveralTokensWithSymbols()
+        {
+            var tokens = tokenizer.MakeTokens("_abc_ _abc_ __ade__").ToList();
+            var lastToken = tokens.Last();
+            var expectedToken = new MdToken("ade", "__", "__");
+            lastToken.Should().BeEquivalentTo(expectedToken);
+        }
+        
+        [Test]
+        public void MakeTokens_Should_ParseNestedTokens_When_TextWithNestedTokens()
+        {
+            var tokens = tokenizer.MakeTokens("__Lorem _ipsum_ dolor sit amet__").ToList();
+            var firstToken = tokens[0];
+            var secondToken = tokens[1];
+            var lastToken = tokens.Last();
+            var expectedFirstToken = new MdToken("Lorem", "__", "NONE");
+            firstToken.Should().BeEquivalentTo(expectedFirstToken);
+            var expectedSecondToken = new MdToken("ipsum", "_", "_");
+            secondToken.Should().BeEquivalentTo(expectedSecondToken);
+            var expectedLastToken = new MdToken("amet", "NONE", "__");
+            lastToken.Should().BeEquivalentTo(expectedLastToken);
+        }
+    }
+
+    public static class Following
+    {
+        public static Action Code(Action action)
+        {
+            return action;
+        }
+    }
+}
