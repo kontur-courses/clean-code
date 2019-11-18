0263321bb11a98e5ea412a93cee08ef51f8a5a52
Арзыбек Казикулов <Arzybek>
2019-11-19 04:02:06 +0500

    Trying to clean-code it

---
+++ cs/Markdown/Tag.cs
@@ -0,0 +1,15 @@
+﻿namespace Markdown
+{
+    public abstract class Tag
+    {
+        public string StringRepr;
+        public int Length;
+        public static bool isOpen = true;
+        
+        public Tag(string str)
+        {
+            this.StringRepr = str;
+            this.Length = str.Length;
+        }
+    }
+}
