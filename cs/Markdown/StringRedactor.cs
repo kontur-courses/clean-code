9a86a2b75f2b5df07baa8f73183367b90f684e7d
JDreyka <belous860@gmail.com>
2019-11-20 18:17:10 +0500

    Вынес работы со строкой в отдельный класс

---
+++ cs/Markdown/StringRedactor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    internal static class StringRedactor
+    {
+        internal static string SwitchMarkdownTagsToHtml(string inputString, List<Tag> tagsForChange)
+        {
+            if (tagsForChange.Count == 0) return inputString;
+
+            var result = new StringBuilder();
+
+            var startedIndex = 0;
+            foreach (var tag in tagsForChange)
+            {
+                for (var i = startedIndex; i < tag.Index; i++)
+                    result.Append(inputString[i]);
+
+                result.Append(tag.Type == TagType.Opening
+                    ? $"<{Markdown.TagsInfo[tag.Designations].HtmlDesignation}>"
+                    : $"</{Markdown.TagsInfo[tag.Designations].HtmlDesignation}>");
+
+                startedIndex = tag.Index + tag.Designations.Length;
+            }
+
+            for (var i = tagsForChange.Last().Designations.Length + tagsForChange.Last().Index; i < inputString.Length; i++)
+                result.Append(inputString[i]);
+
+            return result.ToString();
+        }
+
+        internal static string RemoveEscapeSymbols(string input) => input.Replace(@"\", "");
+    }
+}
