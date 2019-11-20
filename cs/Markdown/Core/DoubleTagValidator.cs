e008b531df8d503547f13e2ef2ec04e732da775c
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-21 00:56:27 +0500

    Рефакторинг валидатора двойных тегов

---
+++ cs/Markdown/Core/DoubleTagValidator.cs
@@ -0,0 +1,35 @@
+﻿using Markdown.Core.Tags;
+
+namespace Markdown.Core
+{
+    static class DoubleTagValidator
+    {
+        private static bool IsFirstSymbol(int index) => index == 0;
+        private static bool SymbolBeforeCurrentIsSpace(string line, int index) => char.IsWhiteSpace(line[index - 1]);
+
+        private static bool SymbolAfterTagIsSpace(string line, int index, IDoubleTag tag) =>
+            char.IsWhiteSpace(line[index + tag.Opening.Length]);
+
+        private static bool IsLastIndexPossibleForTag(string line, int index, IDoubleTag tag) =>
+            index == line.Length - tag.Closing.Length;
+
+        private static bool SymbolBeforeCurrentIs(string line, int index, char symbol) => line[index - 1] == symbol;
+
+        public static bool TagStartsFromPosition(string line, int index, string tagValue) =>
+            tagValue.Length + index <= line.Length && tagValue == line.Substring(index, tagValue.Length);
+
+        public static bool IsPossibleClosingTag(string line, int index, IDoubleTag tag)
+        {
+            return (IsLastIndexPossibleForTag(line, index, tag) || SymbolAfterTagIsSpace(line, index, tag)) &&
+                   (IsFirstSymbol(index) || SymbolBeforeCurrentIs(line, index, '\\') ||
+                    !SymbolBeforeCurrentIsSpace(line, index));
+        }
+
+        public static bool IsPossibleOpeningTag(string line, int index, IDoubleTag tag)
+        {
+            return (IsFirstSymbol(index) || SymbolBeforeCurrentIs(line, index, '\\') ||
+                    SymbolBeforeCurrentIsSpace(line, index)) &&
+                   (IsLastIndexPossibleForTag(line, index, tag) || !SymbolAfterTagIsSpace(line, index, tag));
+        }
+    }
+}
