e0dce9b8d1ea443006807c7c136365df88e2be74
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-21 00:55:59 +0500

    Парсер разделен на парсеры single и double тегов

---
+++ cs/Markdown/Core/DoubleTagParser.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Markdown.Core.Rules;
+using Markdown.Core.Tags;
+
+namespace Markdown.Core
+{
+    class DoubleTagParser
+    {
+        private IEnumerable<IDoubleTag> tags;
+
+        public DoubleTagParser(IEnumerable<IRule> rules)
+        {
+            tags = rules.Where(rule => (rule.SourceTag as IDoubleTag) != null)
+                .Select(rule => rule.SourceTag as IDoubleTag)
+                .OrderByDescending(tag => tag.Opening.Length);
+        }
+
+        private bool IsSuitableTag(IDoubleTag tag, string line, int index) =>
+            DoubleTagValidator.TagStartsFromPosition(line, index, tag.Opening) ||
+            DoubleTagValidator.TagStartsFromPosition(line, index, tag.Closing);
+
+        private bool IsSkippedTag(string line, int index) => index != 0 && line[index - 1] == '\\';
+
+        public List<TagToken> Parse(string line)
+        {
+            var result = new List<TagToken>();
+            if (line == null)
+                return result;
+
+            var tagTokenStack = new Stack<TagToken>();
+            for (var index = 0; index < line.Length; index++)
+            {
+                var currentTag = tags.FirstOrDefault(tag => IsSuitableTag(tag, line, index));
+                if (currentTag == null) continue;
+
+                if (DoubleTagValidator.IsPossibleOpeningTag(line, index, currentTag))
+                {
+                    tagTokenStack.Push(new TagToken(index, currentTag, currentTag.Opening, true,
+                        IsSkippedTag(line, index)));
+                }
+                else if (DoubleTagValidator.IsPossibleClosingTag(line, index, currentTag))
+                {
+                    while (tagTokenStack.Count > 0 && tagTokenStack.Peek().Tag != currentTag)
+                    {
+                        var deletedTag = tagTokenStack.Pop();
+                        if (deletedTag.IsSkipped)
+                            result.Add(deletedTag);
+                    }
+
+                    if (tagTokenStack.Count == 0) continue;
+
+                    result.Add(tagTokenStack.Pop());
+                    result.Add(new TagToken(index, currentTag, currentTag.Closing, false, IsSkippedTag(line, index)));
+                }
+
+                index += currentTag.Opening.Length - 1;
+            }
+
+            return result.Concat(tagTokenStack.Where(token => token.IsSkipped)).ToList();
+        }
+    }
+}
---
+++ cs/Markdown/Core/SingleTagParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Markdown.Core.Rules;
+using Markdown.Core.Tags;
+
+namespace Markdown.Core
+{
+    class SingleTagParser
+    {
+        private readonly List<ISingleTag> tags;
+
+        public SingleTagParser(IEnumerable<IRule> rules)
+        {
+            tags = rules.Where(rule => rule.SourceTag is ISingleTag)
+                .Select(rule => rule.SourceTag as ISingleTag)
+                .OrderByDescending(tag => tag.Opening.Length)
+                .ToList();
+        }
+
+        public List<TagToken> Parse(string line)
+        {
+            return line == null
+                ? new List<TagToken>()
+                : tags.Where(tag => line.StartsWith(tag.Opening))
+                    .Select(tag => new TagToken(0, tag, tag.Opening, true, false))
+                    .ToList();
+        }
+    }
+}
