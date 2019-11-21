691fb16810deabf53d24273e5a45c3dc240c3287
Данил Панков <mistertimechip@mail.ru>
2019-11-21 11:38:14 +0300

    Class of certain token info

---
+++ cs/Markdown/MdTokens/MdTokenSpec.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown.MdTokens
+{
+    public class MdTokenSpec
+    {
+        public string Tag { get; }
+        public bool IsPaired { get; }
+        public bool CanBeNested { get; }
+        public HashSet<string> NestingExceptions { get; }
+
+        public MdTokenSpec(string tag, bool isPaired, bool canBeNested)
+        {
+            Tag = tag;
+            IsPaired = isPaired;
+            CanBeNested = canBeNested;
+            NestingExceptions = new HashSet<string>();
+        }
+
+        public void AddNestingExceptions(string tag)
+        {
+            NestingExceptions.Add(tag);
+        }
+    }
+}
