8bea212d88f93f516cbb2289c06d9174ac133c04
neoezop <49761942+neoezop@users.noreply.github.com>
2019-11-21 17:44:48 +0500

    Added and changed tests
    Accordingto new tags

--- cs/Markdown/Markdown/Tests/MdTests.cs
+++ cs/Markdown/Markdown/Tests/MdTests.cs
@@ -25,12 +25,12 @@ namespace Markdown
         [Test]
         public void AddElement_ShouldAddElementCorrectly()
         {
-            var mdElement = new MdElement('^', "<H1>", true);
+            var mdElement = new MdElement("^", "<H1>", true);
             md.AddElement(mdElement);
-            var expected = new MdElement('^', "<H1>", true);
+            var expected = new MdElement("^", "<H1>", true);
             expected.HtmlTagClose = "<H1/>";
-            md.elementSigns.ContainsKey('^').Should().BeTrue();
-            md.elementSigns['^'].Should().BeEquivalentTo(expected);
+            md.elementSigns.ContainsKey("^").Should().BeTrue();
+            md.elementSigns["^"].Should().BeEquivalentTo(expected);
         }
 
         [Test]
@@ -43,8 +43,12 @@ namespace Markdown
         [TestCase("", ExpectedResult = "")]
         [TestCase("_a_", ExpectedResult = "<em>a<em/>")]
         [TestCase("_a a a_", ExpectedResult = "<em>a a a<em/>")]
-        [TestCase("*a*", ExpectedResult = "<strong>a<strong/>")]
-        [TestCase("*a a a*", ExpectedResult = "<strong>a a a<strong/>")]
+        [TestCase("*a*", ExpectedResult = "<em>a<em/>")]
+        [TestCase("*a a a*", ExpectedResult = "<em>a a a<em/>")]
+        [TestCase("__a__", ExpectedResult = "<strong>a<strong/>")]
+        [TestCase("__a a a__", ExpectedResult = "<strong>a a a<strong/>")]
+        [TestCase("**a**", ExpectedResult = "<strong>a<strong/>")]
+        [TestCase("**a a a**", ExpectedResult = "<strong>a a a<strong/>")]
         [TestCase("a _a *a a", ExpectedResult = "a _a *a a")]
         [TestCase("a a_ a* a", ExpectedResult = "a a_ a* a")]
         [TestCase("_a_a_a_", ExpectedResult = "<em>a_a_a<em/>")]
@@ -54,8 +58,32 @@ namespace Markdown
         [TestCase("_a * a*_", ExpectedResult = "<em>a * a*<em/>")]
         [TestCase("*_ a _*", ExpectedResult = "*_ a _*")]
         [TestCase("\a _sad_", ExpectedResult = "\a <em>sad<em/>")]
+        [TestCase("_a a a*", ExpectedResult = "_a a a*")]
+        [TestCase("__a a a**", ExpectedResult = "__a a a**")]
+        [TestCase("___a a___", ExpectedResult = "<em><strong>a a<strong/><em/>")]
+        [TestCase("____a____", ExpectedResult = "<strong><strong>a<strong/><strong/>")]
+        [TestCase("_a__", ExpectedResult = "<em>a<em/>_")]
+        [TestCase("__a_", ExpectedResult = "_<em>a<em/>")]
+        [TestCase("_\\_a_", ExpectedResult = "<em>_a<em/>")]
         public string Render_ShouldReturnCorrectResult(string text)
         {
+            return md.Render(text);
+        }
+
+        [TestCase("# a", ExpectedResult = "<H1> a")]
+        [TestCase("## a", ExpectedResult = "<H2> a")]
+        [TestCase("### a", ExpectedResult = "<H3> a")]
+        [TestCase("#### a", ExpectedResult = "<H4> a")]
+        [TestCase("##### a", ExpectedResult = "<H5> a")]
+        [TestCase("###### a", ExpectedResult = "<H6> a")]
+        public string Render_ShouldReturnCorrectResult_OnAddedNonEnclosedElements(string text)
+        {
+            var tag = "#";
+            for (int i = 1; i <= 6; i++)
+            {
+                md.AddElement(new MdElement(tag, $"<H{i}>", false));
+                tag += '#';
+            }
             return md.Render(text);
         }
     }
