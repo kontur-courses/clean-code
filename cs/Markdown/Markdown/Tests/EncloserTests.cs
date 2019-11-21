dc42e98fde74c61ec5941ef8de215356af9404af
neoezop <49761942+neoezop@users.noreply.github.com>
2019-11-21 18:13:02 +0500

    got rid ofunused usings

---
+++ cs/Markdown/Markdown/Tests/EncloserTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    [TestFixture]
+    class EncloserTests
+    {
+        private Tokenizer tokenizer;
+        private Md md;
+
+        [SetUp]
+        public void BaseSetup()
+        {
+            md = new Md();
+            tokenizer = new Tokenizer(md.elementSigns);
+        }
+
+        [Test]
+        public void Tokenize_ShouldThrowArgumentNullException_OnNullText()
+        {
+            Action act = () => tokenizer.Tokenize(null);
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
