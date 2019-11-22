c05b98d704bbfdb7a22697c27595e9efba23be7c
Shcherbina99 <ej99@example.com>
2019-11-22 15:47:41 +0500

    test: replaced reflection with languageBuilder

--- cs/Markdown/Tests/SyntaxNodes_should.cs
+++ cs/Markdown/Tests/SyntaxNodes_should.cs
@@ -9,80 +9,80 @@ namespace Markdown.Tests
 {
     public class SyntaxNodes_should
     {
+        private static string value;
 
-        [TestCase(typeof(Html))]
-        [TestCase(typeof(MarkDown))]
-        public void TextNodeShould_ConvertTo_WhenAnyLanguage_ReturnValue(Type type)
+        [SetUp]
+        public void SetUp()
         {
-            const string value = "aba aba";
+            value = "aba aba";
+        }
+
+        [TestCase("Html")]
+        [TestCase("MarkDown")]
+        public void TextNodeShould_ConvertTo_WhenAnyLanguage_ReturnValue(string typeName)
+        {
             var textNode = new TextNode(value);
-            var language = (ILanguage) type.GetConstructor(new Type[0])?.Invoke(new object[0]);
+            var language = LanguageRegistry.BuildLanguage(typeName);
             textNode.ConvertTo(language.Tags)
                 .Should().Be(value);
         }
 
-        [TestCase(typeof(Html), TagType.Em)]
-        [TestCase(typeof(Html), TagType.Strong)]
-        [TestCase(typeof(MarkDown), TagType.Em)]
-        [TestCase(typeof(MarkDown), TagType.Strong)]
-        public void TagNodeShould_ConvertTo_WhenAnyLanguageAndTextNode_AddInStartAndEndTag(Type type, TagType tagType)
+        [TestCase("Html", TagType.Em)]
+        [TestCase("Html", TagType.Strong)]
+        [TestCase("MarkDown", TagType.Em)]
+        [TestCase("MarkDown", TagType.Strong)]
+        public void TagNodeShould_ConvertTo_WhenAnyLanguageAndTextNode_AddInStartAndEndTag(string typeName,
+            TagType tagType)
         {
-            const string value = "aba aba";
             var tagNode = new TagNode(tagType, new List<SyntaxNode>() {new TextNode(value)});
-            var language = (ILanguage) type.GetConstructor(new Type[0])?.Invoke(new object[0]);
+            var language = LanguageRegistry.BuildLanguage(typeName);
             var result = tagNode.ConvertTo(language.Tags);
             result.Should().StartWith(language.Tags[tagType].Start);
             result.Should().EndWith(language.Tags[tagType].End);
         }
 
-        [TestCase(typeof(Html))]
-        [TestCase(typeof(MarkDown))]
-        public void SyntaxTreeShould_ConvertTo_WhenAnyLanguageAndTwoTextNode_JoinText(Type type)
+        [TestCase("Html")]
+        [TestCase("MarkDown")]
+        public void SyntaxTreeShould_ConvertTo_WhenAnyLanguageAndTwoTextNode_JoinText(string typeName)
         {
-            const string value = "aba aba";
             var textNode = new TextNode(value);
             var syntaxTree = new SyntaxTree(new List<SyntaxNode>() {textNode, textNode});
-            var language = (ILanguage) type.GetConstructor(new Type[0])?.Invoke(new object[0]);
+            var language = LanguageRegistry.BuildLanguage(typeName);
             var result = syntaxTree.ConvertTo(language.Tags);
             result.Should().Be(value + value);
         }
 
-        [TestCase(typeof(Html), TagType.Em)]
-        [TestCase(typeof(Html), TagType.Strong)]
-        [TestCase(typeof(MarkDown), TagType.Em)]
-        [TestCase(typeof(MarkDown), TagType.Strong)]
-        public void SyntaxTreeShould_ConvertTo_WhenAnyLanguageAndTextNodeAndTagNode_JoinText(Type type, TagType tagType)
+        [TestCase("Html", TagType.Em)]
+        [TestCase("Html", TagType.Strong)]
+        [TestCase("MarkDown", TagType.Em)]
+        [TestCase("MarkDown", TagType.Strong)]
+        public void SyntaxTreeShould_ConvertTo_WhenAnyLanguageAndTextNodeAndTagNode_JoinText(string typeName,
+            TagType tagType)
         {
-            const string value = "aba aba";
             var textNode = new TextNode(value);
             var tagNode = new TagNode(tagType, new List<SyntaxNode>() {textNode});
             var syntaxTree = new SyntaxTree(new List<SyntaxNode>() {tagNode, textNode});
-            var language = (ILanguage) type.GetConstructor(new Type[0])?.Invoke(new object[0]);
+            var language = LanguageRegistry.BuildLanguage(typeName);
             var result = syntaxTree.ConvertTo(language.Tags);
             result.Should().Be(language.Tags[tagType].Start + value + language.Tags[tagType].End + value);
         }
 
-        [TestCase(typeof(Html), TagType.Em, null)]
-        [TestCase(typeof(Html), TagType.Strong, null)]
-        [TestCase(typeof(MarkDown), TagType.Em, null)]
-        [TestCase(typeof(MarkDown), TagType.Strong, null)]
-        [TestCase(typeof(Html), TagType.Em, "")]
-        [TestCase(typeof(Html), TagType.Strong, "")]
-        [TestCase(typeof(MarkDown), TagType.Em, "")]
-        [TestCase(typeof(MarkDown), TagType.Strong, "")]
-        public void TagNodeShould_ConvertTo_WhenAnyLanguageAndTagNodeWithNullOrEmpty_ReturnEmptyWithTag(Type type,
+        [TestCase("Html", TagType.Em, null)]
+        [TestCase("Html", TagType.Strong, null)]
+        [TestCase("MarkDown", TagType.Em, null)]
+        [TestCase("MarkDown", TagType.Strong, null)]
+        [TestCase("Html", TagType.Em, "")]
+        [TestCase("Html", TagType.Strong, "")]
+        [TestCase("MarkDown", TagType.Em, "")]
+        [TestCase("MarkDown", TagType.Strong, "")]
+        public void TagNodeShould_ConvertTo_WhenAnyLanguageAndTagNodeWithNullOrEmpty_ReturnEmptyWithTag(string typeName,
             TagType tagType, string str)
         {
             var textNode = new TextNode(null);
             var tagNode = new TagNode(tagType, new List<SyntaxNode>() {textNode});
-            var language = languageBuilder(type);
+            var language = LanguageRegistry.BuildLanguage(typeName);
             var result = tagNode.ConvertTo(language.Tags);
             result.Should().Be(language.Tags[tagType].Start + language.Tags[tagType].End);
-        }
-
-        private ILanguage languageBuilder(Type type)
-        {
-            return (ILanguage) type.GetConstructor(new Type[0])?.Invoke(new object[0]);
         }
     }
 }
