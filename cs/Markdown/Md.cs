9e49647623620a52f8fc20a494bdcaca987c53af
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2019-11-22 23:45:33 +0500

    Щур Всеволод

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework.Constraints;
+
+namespace Markdown
+{
+    class Md
+    {
+        public string Render(string mdString)
+        {
+            var elements = FindElements(mdString);
+            return CreateHtml(mdString, elements);
+        }
+
+        public string CreateHtml(string mdString, List<ElementData> elements)
+        {
+            var renderedString = new StringBuilder();
+            var elemStack = new Stack<ElementType>();
+            var textStartIndex = 0;
+            foreach (var element in elements)
+            {
+                var textEndIndex = element.StartIndex - 1;
+                var length = Math.Max(0, textEndIndex - textStartIndex + 1);
+                renderedString.Append(mdString.Substring(textStartIndex, length));
+                textStartIndex = element.EndIndex;
+
+                var tagData = TagInfo.TypeToHtmlTag[element.Type];
+                if (elemStack.TryPeek(out var type) && type == element.Type)
+                {
+                    elemStack.Pop();
+                    renderedString.Append(tagData.EndTag);
+                }
+                else
+                {
+                    elemStack.Push(element.Type);
+                    if (!CheckConditions(elemStack.ToList()))
+                    {
+                        elemStack.Pop();
+                        continue;
+                    }
+
+                    renderedString.Append(tagData.StartTag);
+                    elemStack.Push(element.Type);
+                }
+            }
+
+            return renderedString.ToString();
+        }
+
+        private bool CheckConditions(IReadOnlyList<ElementType> typeList)
+        {
+            if (typeList.Count > 1 && typeList[typeList.Count - 1] == ElementType.Em 
+                                   && typeList[typeList.Count - 2] == ElementType.Strong)
+                return false;
+
+            return true;
+        }
+
+        public List<ElementData> FindElements(string mdString)
+        {
+            var elements = new List<ElementData>();
+            var mdStarts = TagInfo.MdToType.Keys;
+            var maxBufferLength = mdStarts.Max(e => e.Length);
+            var i = 0;
+            while (i < mdString.Length)
+            {
+                var bufferLength = Math.Min(maxBufferLength, mdString.Length - i);
+                var buffer = mdString.Substring(i, bufferLength);
+
+                var isMdTagInBuf = new Dictionary<string, bool>();
+                foreach (var mdTagStart in mdStarts)
+                    isMdTagInBuf[mdTagStart] = buffer.StartsWith(mdTagStart);
+
+                var mdTag = isMdTagInBuf.Where(y => y.Value).Max(y => y.Key);
+                if (mdTag == null)
+                {
+                    i++;
+                    continue;
+                }
+
+                var (type, end) = TagInfo.MdToType[mdTag];
+                elements.Add(new ElementData(type, i, i + end.Length));
+                i += end.Length;
+            }
+
+            return elements;
+        }
+    }
+
+    static class TagInfo
+    {
+        public static readonly Dictionary<string, Tuple<ElementType, string>> MdToType = new Dictionary<string, Tuple<ElementType, string>>
+        {
+            {"_", Tuple.Create(ElementType.Em, "_")},
+            {"__", Tuple.Create(ElementType.Strong, "__")}
+        };
+
+        public static readonly Dictionary<ElementType, HtmlTagData> TypeToHtmlTag 
+            = new Dictionary<ElementType, HtmlTagData>
+            {
+                {ElementType.Em, new HtmlTagData("<em>", "</em>")},
+                {ElementType.Strong, new HtmlTagData("<strong>", "</strong>")}
+            };
+    }
+    public class ElementData
+    {
+        public readonly ElementType Type;
+        public readonly int StartIndex;
+        public readonly int EndIndex;
+
+        public ElementData(ElementType type, int startIndex, int endIndex)
+        {
+            Type = type;
+            StartIndex = startIndex;
+            EndIndex = endIndex;
+        }
+    }
+
+    public enum ElementType
+    {
+        Em,
+        Strong
+    }
+
+    class HtmlTagData
+    {
+        public readonly string StartTag;
+        public readonly string EndTag;
+
+        public HtmlTagData(string startTag, string endTag)
+        {
+            StartTag = startTag;
+            EndTag = endTag;
+        }
+    }
+}
---
+++ cs/Markdown/Md_Should.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Markdown
+{
+    [TestFixture]
+    public class Md_Should
+    {
+        private Md processor;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            processor = new Md();
+        }
+        
+        [TestCase("_a_", ExpectedResult = "<em>a</em>", TestName = "OneUnderlineToEm")]
+        [TestCase("__word__", ExpectedResult = "<strong>word</strong>", TestName = "TwoUnderlinesToStrong")]
+        [TestCase("a _a_ a __a__", ExpectedResult = "a <em>a</em> a <strong>a</strong>", TestName = "Mixed")]
+        [TestCase("_a__a__a_", ExpectedResult = "<em>__a__</em>", TestName = "TwoUnderlinesInsideOneDontRender")]
+        public string Processor_ChangesMdElementsToHtml(string mdString)
+        {
+            var a = processor.Render(mdString);
+            return a;
+        }
+        
+        [TestCase]
+        public void Processor_FindsElements()
+        {
+            var a =  processor.FindElements("_a_");
+            var expected = new List<ElementData>
+            {
+                new ElementData(ElementType.Em, 0, 1),
+                new ElementData(ElementType.Em, 2, 3)
+            };
+            Assert.AreEqual(expected,a);
+        }
+    }
+}
