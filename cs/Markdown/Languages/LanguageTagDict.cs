23de1037f50ebf9d2854d84452e72f613d1c9407
Shcherbina99 <ej99@example.com>
2019-11-22 15:09:11 +0500

    feat: add shell over dictionary

---
+++ cs/Markdown/Languages/LanguageTagDict.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Markdown.Languages
+{
+    public class LanguageTagDict
+    {
+        private readonly Dictionary<TagType, Tag> dictionary;
+        public Dictionary<TagType, Tag>.KeyCollection Keys => dictionary.Keys;
+        public Dictionary<TagType, Tag>.ValueCollection Values => dictionary.Values;
+        public int Count => dictionary.Count;
+
+        public LanguageTagDict(Dictionary<TagType, Tag> dictionary)
+        {
+            this.dictionary = dictionary;
+        }
+
+        public Tag this[TagType tagType]
+        {
+            get
+            {
+                if (dictionary.ContainsKey(tagType))
+                {
+                    return dictionary[tagType];
+                }
+                else
+                {
+                    var child = ((TagType[]) Enum.GetValues(typeof(TagType))).ToList();
+                    return new Tag("", "", child);
+                }
+            }
+        }
+    }
+}
---
+++ cs/Markdown/Tests/LanguageTagDict_should.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Markdown.Languages;
+using NUnit.Framework;
+
+namespace Markdown.Tests
+{
+    public class LanguageTagDict_should
+    {
+        [Test]
+        public void WhenUnregulatedTag_ReturnEmptyTag()
+        {
+            var dict = new LanguageTagDict(new Dictionary<TagType, Tag>());
+            dict[TagType.Em].Start.Should().Be("");
+            dict[TagType.Em].End.Should().Be("");
+            dict[TagType.Em].Children.Should().NotContainNulls();
+        }
+
+        [Test]
+        public void WhenRegulatedTag_ReturnEmptyTag()
+        {
+            var dict = new LanguageTagDict(new Dictionary<TagType, Tag>
+                {{TagType.Em, new Tag("start", "end", new TagType[0])}});
+            dict[TagType.Em].Start.Should().Be("start");
+            dict[TagType.Em].End.Should().Be("end");
+            dict[TagType.Em].Children.Should().HaveCount(0);
+        }
+    }
+}
