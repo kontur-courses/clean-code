cd5e60fffbea831ad5354b51f23684b17e933560
JDreyka <belous860@gmail.com>
2019-11-23 00:31:19 +0500

    Переименовал класс StringRedactor и перписать основной его метод

---
+++ cs/Markdown/HtmlConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    internal static class HtmlConverter
+    {
+        internal static string ConvertToHtml(string inputString, IEnumerable<TagToken> tagsForChange)
+        {
+            var tagIndices = tagsForChange
+                .ToDictionary(tag => tag.Index, tag => tag);
+            
+            var result = new StringBuilder();
+
+            for (int i = 0; i < inputString.Length;)
+            {
+                if (tagIndices.ContainsKey(i))
+                {
+                    result.Append(tagIndices[i].TokenType == TagTokenType.Opening
+                        ? $"<{tagIndices[i].MarkdownTag.HtmlDesignation}>"
+                        : $"</{tagIndices[i].MarkdownTag.HtmlDesignation}>");
+                    i += tagIndices[i].MarkdownTag.TagDesignation.Length;
+                }
+                else
+                {
+                    result.Append(inputString[i]);
+                    i += 1;
+                }
+            }
+            
+            return result.ToString();
+        }
+    }
+}
