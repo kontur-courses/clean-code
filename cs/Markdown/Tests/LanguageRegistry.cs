846b0409326ad32a7b0f0044f6e5a750e1b92662
Shcherbina99 <ej99@example.com>
2019-11-22 15:46:17 +0500

    feat: add languageBuilder

---
+++ cs/Markdown/Tests/LanguageRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Markdown.Languages;
+
+namespace Markdown.Tests
+{
+    public static class LanguageRegistry
+    {
+        public static ILanguage BuildLanguage(string typeName)
+        {
+            return (ILanguage) Activator.CreateInstance(Type.GetType($"Markdown.Languages.{typeName}") ??
+                                                        throw new ArgumentException());
+        }
+    }
+}
