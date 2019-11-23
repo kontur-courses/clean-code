8bf9a63c66155ccf4f7a7eb76e9f9a643cd8d6cf
TVIGes <khdv66@gmail.com>
2019-11-23 15:55:37 +0500

    Add MarkdownParser implementation

--- cs/Markdown/Parser/MarkdownParser.cs
+++ cs/Markdown/Parser/MarkdownParser.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Markdown.Lexer;
 
 namespace Markdown.Parser
@@ -16,11 +17,70 @@ namespace Markdown.Parser
                 [LexemeDefinitions.Italic] = (value) => new MarkdownItalicElement(value),
                 [LexemeDefinitions.Bold] = (value) => new MarkdownBoldElement(value)
             };
+            Parsers = new Dictionary<TokenType, Action<Token, Stack<Element>>>
+            {
+                [TokenType.Text] = ParseTextToken,
+                [TokenType.OpeningTag] = ParseOpeningTag,
+                [TokenType.ClosingTag] = ParseClosingTag
+            };
         }
 
         internal static Element Parse(IEnumerable<Token> tokens)
         {
-            throw new NotImplementedException();
+            var root = new Element(null);
+            var stack = new Stack<Element>();
+            stack.Push(root);
+            foreach (var token in tokens)
+                Parsers[token.Type](token, stack);
+            TransformSubtreeToText(root, TakeOutSubElements(stack, root));
+            return root;
+        }
+
+        private static void ParseTextToken(Token token, Stack<Element> elements)
+        {
+            elements.Peek().ChildNodes.Add(new Text(token.Value));
+        }
+
+        private static void ParseOpeningTag(Token token, Stack<Element> elements)
+        {
+            var element = Elements[token.Lexeme](token.Value);
+            if (elements.Peek().CanContain(element))
+                elements.Push(element);
+            else
+                ParseTextToken(token, elements);
+        }
+
+        private static void ParseClosingTag(Token token, Stack<Element> elements)
+        {
+            var element = Elements[token.Lexeme](token.Value);
+            if (elements.Any(opening => opening.GetType() == element.GetType()))
+            {
+                var subElements = TakeOutSubElements(elements, element);
+                element = elements.Pop();
+                TransformSubtreeToText(element, subElements);
+                elements.Peek().ChildNodes.Add(element);
+            }
+            else
+                ParseTextToken(token, elements);
+        }
+
+        private static void TransformSubtreeToText(Element parent, IEnumerable<INode> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                var text = node.Type == NodeType.Text ? node : new Text(node.Value);
+                parent.ChildNodes.Add(text);
+                if (node.Type == NodeType.Element)
+                    TransformSubtreeToText(parent, node.ChildNodes);
+            }
+        }
+
+        private static IEnumerable<Element> TakeOutSubElements(Stack<Element> elements, Element parent)
+        {
+            var result = new Stack<Element>();
+            while (elements.Peek().GetType() != parent.GetType())
+                result.Push(elements.Pop());
+            return result;
         }
     }
 }
---
+++ cs/Markdown/Parser/MarkdownParser_Should.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Markdown.Lexer;
+using NUnit.Framework;
+
+namespace Markdown.Parser
+{
+    [TestFixture]
+    public class MarkdownParser_Should
+    {
+        private static readonly Lexeme Italic = LexemeDefinitions.Italic;
+        private static readonly Lexeme Bold = LexemeDefinitions.Bold;
+
+        [Test]
+        public void Parse_BuildsCorrectTree_WithoutTags()
+        {
+            const string text = "foo bar";
+            var tokens = new[] {new Token(0, text, TokenType.Text)};
+            var expected = new Text(text);
+
+            TestParsing(tokens, expected);
+        }
+
+        [Test]
+        public void Parse_BuildCorrectTree_WithItalicTag()
+        {
+            const string text = "foo";
+            var tokens = new[]
+            {
+                new Token(0, Italic.Representation, TokenType.OpeningTag, Italic),
+                new Token(1, text, TokenType.Text),
+                new Token(4, Italic.Representation, TokenType.ClosingTag, Italic)
+            };
+            var expected = new MarkdownItalicElement(Italic.Representation);
+            expected.ChildNodes.Add(new Text(text));
+
+            TestParsing(tokens, expected);
+        }
+
+        [Test]
+        public void Parse_BuildCorrectTree_WithBoldTag()
+        {
+            const string text = "bar";
+            var tokens = new[]
+            {
+                new Token(0, Bold.Representation, TokenType.OpeningTag, Bold),
+                new Token(2, text, TokenType.Text),
+                new Token(5, Bold.Representation, TokenType.ClosingTag, Bold)
+            };
+            var expected = new MarkdownBoldElement(Bold.Representation);
+            expected.ChildNodes.Add(new Text(text));
+
+            TestParsing(tokens, expected);
+        }
+
+        [Test]
+        public void Parse_BuildCorrectTree_WhenItalicIsEmbeddedInBoldTag()
+        {
+            const string text = "baz";
+            var tokens = new[]
+            {
+                new Token(0, Bold.Representation, TokenType.OpeningTag, Bold),
+                new Token(2, Italic.Representation, TokenType.OpeningTag, Italic),
+                new Token(3, text, TokenType.Text),
+                new Token(6, Italic.Representation, TokenType.ClosingTag, Italic),
+                new Token(7, Bold.Representation, TokenType.ClosingTag, Bold)
+            };
+            var italic = new MarkdownItalicElement(Italic.Representation);
+            italic.ChildNodes.Add(new Text(text));
+            var expected = new MarkdownBoldElement(Bold.Representation);
+            expected.ChildNodes.Add(italic);
+
+            TestParsing(tokens, expected);
+        }
+
+        [Test]
+        public void Parse_BuildCorrectTree_WhenBoldIsEmbeddedInItalicTag()
+        {
+            const string text = "baz";
+            var tokens = new[]
+            {
+                new Token(0, Italic.Representation, TokenType.OpeningTag, Italic),
+                new Token(1, Bold.Representation, TokenType.OpeningTag, Bold),
+                new Token(3, text, TokenType.Text),
+                new Token(6, Bold.Representation, TokenType.ClosingTag, Bold),
+                new Token(8, Italic.Representation, TokenType.ClosingTag, Italic)
+            };
+            var expected = new MarkdownItalicElement(Italic.Representation);
+            expected.ChildNodes.Add(new Text(Bold.Representation));
+            expected.ChildNodes.Add(new Text(text));
+            expected.ChildNodes.Add(new Text(Bold.Representation));
+
+            TestParsing(tokens, expected);
+        }
+
+        [Test]
+        public void Parse_BuildCorrectTree_WhenTagsHaveNoPairs()
+        {
+            var tokens = new[]
+            {
+                new Token(0, Bold.Representation, TokenType.OpeningTag, Bold),
+                new Token(2, "foo", TokenType.Text),
+                new Token(5, Italic.Representation, TokenType.OpeningTag, Italic),
+                new Token(6, "bar ", TokenType.Text)
+            };
+            var expected = new INode[]
+            {
+                new Text(Bold.Representation), new Text("foo"), new Text(Italic.Representation), new Text("bar ")
+            };
+            
+            TestParsing(tokens, expected);
+        }
+
+        [Test]
+        public void Parse_BuildCorrectTree_WhenTagsIntersect()
+        {
+            var tokens = new[]
+            {
+                new Token(0, Bold.Representation, TokenType.OpeningTag, Bold),
+                new Token(2, "foo", TokenType.Text),
+                new Token(5, Italic.Representation, TokenType.OpeningTag, Italic),
+                new Token(6, "bar", TokenType.Text),
+                new Token(9, Bold.Representation, TokenType.ClosingTag, Bold),
+                new Token(11, "baz", TokenType.Text),
+                new Token(14, Italic.Representation, TokenType.ClosingTag, Italic)
+            };
+            var boldElement = new MarkdownBoldElement(Bold.Representation);
+            boldElement.ChildNodes.Add(new Text("foo"));
+            boldElement.ChildNodes.Add(new Text(Italic.Representation));
+            boldElement.ChildNodes.Add(new Text("bar"));
+            var expected = new INode[] {boldElement, new Text("baz"), new Text(Italic.Representation)};
+
+            TestParsing(tokens, expected);
+        }
+
+        private static void TestParsing<T>(IEnumerable<Token> tokens, params T[] nodes) where T : INode
+        {
+            var root = MarkdownParser.Parse(tokens);
+
+            root.ChildNodes.Should().BeEquivalentTo(nodes);
+        }
+    }
+}
