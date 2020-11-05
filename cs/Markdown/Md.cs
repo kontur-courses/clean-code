9f22d9fd0a37b63d1475d625a82905d13b765795
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2020-11-06 01:18:44 +0500

    Project created, tests created

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public string Render(string markdownText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
