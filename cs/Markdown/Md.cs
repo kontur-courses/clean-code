e62569a8e30c2e07f64b60618fc62af8f48640ab
klim0o0 <kirill.klimmmm@gmail.com>
2020-11-06 21:35:50 +0500

    Создал макет

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public static string Render(string markdown)
+        {
+            var renderedMarkdown = new StringBuilder();
+            foreach (var paragraph in GetParagraphs(markdown))
+            {
+                renderedMarkdown.Append(RenderParagraph(paragraph));
+            }
+
+            return renderedMarkdown.ToString();
+        }
+
+        private static string RenderParagraph(string paragraph)
+        {
+            var tags = GetTags(paragraph);
+            var textWithouttTags = DeleteTags(paragraph, tags);
+            return InsertHtmlTags(textWithouttTags, tags);
+        }
+
+        private static Tag[] GetTags(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static string DeleteTags(string text, Tag[] tags)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static string[] GetParagraphs(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static string InsertHtmlTags(string text, Tag[] tags)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ cs/Markdown/Tag.cs
@@ -0,0 +1,20 @@
+﻿namespace Markdown
+{
+    public class Tag
+    {
+        public readonly string TegName;
+        public readonly int Position;
+
+        public Tag(string tegName, int position)
+        {
+            TegName = tegName;
+            Position = position;
+        }
+
+
+        public string BuildHtmlTag(bool isClosingTeg)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
