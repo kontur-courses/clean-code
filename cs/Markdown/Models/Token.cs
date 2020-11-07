889a69afefaf3398e894db55531f77762092a47e
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2020-11-07 18:48:28 +0500

    create basic architecture

---
+++ cs/Markdown/Models/Token.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown.Models
+{
+    internal class Token : IToken
+    {
+        public int StartPosition { get; }
+        public int Length { get; }
+        public string Value { get; }
+
+        public Token(int startPosition, string value)
+        {
+            StartPosition = startPosition;
+            Value = value;
+            Length = value.Length;
+        }
+    }
+}
---
+++ cs/Markdown/Models/TokenReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown.Models
+{
+    internal class TokenReader
+    {
+        public IEnumerable<IToken> ReadTokens(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
