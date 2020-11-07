a177a640570a7c1f5404ffea061ba1f38502614e
Kisus228 <nik.babushkin.201408@gmail.com>
2020-11-08 01:18:55 +0500

    Сделал каскадную версию кода

---
+++ cs/Markdown/Token.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class Token
+    {
+        public readonly int Length;
+        public readonly int StartIndex;
+        public readonly TagOption TagOption;
+
+        public Token(int startIndex, int length, TagOption tagOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Token[] ParseStringToTokens(string sourceText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ApplyTokensToString(string sourceText, Token[] tokens)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string AddItalicTagToString(string sourceText, Token token)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string AddBoldTagToString(string sourceText, Token token)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string AddHeaderTagToString(string sourceText, Token token)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IncreaseTokensLength(Token[] tokens, Token lastAppliedToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/MarkdownTests/MdTests.cs
@@ -0,0 +1,8 @@
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    public class Tests
+    {
+    }
+}
