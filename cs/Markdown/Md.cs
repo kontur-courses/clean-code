672e848d2d8c336d20ed32bcc870a346060ed2f2
yourname <yourname@site.com>
2020-11-11 01:38:15 +0500

    создал макет решения

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Markdown
+{
+    public static class Md
+    {
+        public static string Render(string text)
+        {
+            
+            throw new NotImplementedException();
+        }    
+    }
+}
---
+++ cs/Markdown/Tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Tag
+    {
+        public string TagName { get; }
+        public int OpenPosition { get; }
+        public int ClosePosition { get; }
+        
+        public static Tag SpotTag(string text, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FindClosePosition(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
