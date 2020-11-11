4754ad78102ee5f2531bc35b811882ad6bdbc8ca
Ivan Grabovsky <raccoon.twitch@gmail.com>
2020-11-11 23:49:50 +0500

    a bit refactoring

--- cs/Markdown/TextParser.cs
+++ cs/Markdown/TextParser.cs
@@ -13,48 +13,35 @@ namespace Markdown
         {
             if (text == null)
                 throw new ArgumentException("string was null");
-            var specialSymbols = new List<char> { '_' };
             var splittedText = new List<TextToken>();
-            var openedTokens = new List<TextToken>();
             if (text.Length == 0)
                 return splittedText;
 
-            var sb = new StringBuilder();
+            var currentText = new StringBuilder();
             for (var index = 0; index < text.Length; index++)
             {
-                sb.Append(text[index]);
+                currentText.Append(text[index]);
                 var letterType = GetTokenType(index, text);
-                if (letterType == TokenType.Strong)
+                switch (letterType)
                 {
-                    sb.Append(text[index + 1]);
-                    index++;
-                    if (sb.Length > 2 && GetTokenType(0,sb.ToString()) == GetTokenType(sb.Length - 2, sb.ToString()))
+                    case TokenType.Strong:
                     {
-                        sb.Remove(0, 2);
-                        sb.Remove(sb.Length - 2, 2);
-                        var tokenToAdd = new TextToken(index - sb.Length - 1, sb.Length, letterType, sb.ToString());
-                        splittedText.Add(tokenToAdd);
-                        sb.Clear();
+                        currentText.Append(text[index + 1]);
+                        index++;
+                        WorkWithStrongText(currentText, splittedText, index, letterType);
+
+                        break;
                     }
-                }
-                else if (letterType == TokenType.Emphasized)
-                {
-                    if (sb.Length > 1 && GetTokenType(0, sb.ToString()) == GetTokenType(sb.Length - 1, sb.ToString()))
+                    case TokenType.Emphasized:
                     {
-                        sb.Remove(0, 1);
-                        sb.Remove(sb.Length - 1, 1);
-                        var tokenToAdd = new TextToken(index - sb.Length, sb.Length, letterType, sb.ToString());
-                        splittedText.Add(tokenToAdd);
-                        sb.Clear();
+                        WorkWithEmphasizedText(currentText, splittedText, index, letterType);
+
+                        break;
                     }
-                }
-                else
-                {
-                    if (index + 1 == text.Length || (GetTokenType(index + 1,text) != TokenType.Text && GetTokenType(0,sb.ToString()) == TokenType.Text))
+                    default:
                     {
-                        var tokenToAdd = new TextToken(index - sb.Length + 1, sb.Length, TokenType.Text, sb.ToString());
-                        splittedText.Add(tokenToAdd);
-                        sb.Clear();
+                        WorkWithText(currentText, splittedText, text, index, letterType);
+                        break;
                     }
                 }
 
@@ -62,21 +49,49 @@ namespace Markdown
             return splittedText;
         }
 
-        public TokenType GetTokenType(int index, string text)
+        private void WorkWithStrongText(StringBuilder sb,List<TextToken> splittedText, int index, TokenType letterType)
+        {
+            if (sb.Length <= 2 || GetTokenType(0, sb.ToString()) != GetTokenType(sb.Length - 2, sb.ToString())) return;
+            sb.Remove(0, 2);
+            sb.Remove(sb.Length - 2, 2);
+            var tokenToAdd = new TextToken(index - sb.Length - 1, sb.Length, letterType, sb.ToString());
+            splittedText.Add(tokenToAdd);
+            sb.Clear();
+        }
+
+        private void WorkWithEmphasizedText(StringBuilder currentText, List<TextToken> splittedText, int index, TokenType letterType)
+        {
+            if (currentText.Length <= 1 || GetTokenType(0, currentText.ToString()) !=
+                GetTokenType(currentText.Length - 1, currentText.ToString())) return;
+            currentText.Remove(0, 1);
+            currentText.Remove(currentText.Length - 1, 1);
+            var tokenToAdd = new TextToken(index - currentText.Length, currentText.Length, letterType, currentText.ToString());
+            splittedText.Add(tokenToAdd);
+            currentText.Clear();
+        }
+
+        private void WorkWithText(StringBuilder currentText, List<TextToken> splittedText, string text, int index, TokenType letterType)
+        {
+            if (index + 1 != text.Length && (GetTokenType(index + 1, text) == TokenType.Text ||
+                                             GetTokenType(0, currentText.ToString()) != TokenType.Text)) return;
+            var tokenToAdd = new TextToken(index - currentText.Length + 1, currentText.Length, TokenType.Text, currentText.ToString());
+            splittedText.Add(tokenToAdd);
+            currentText.Clear();
+        }
+
+        private static TokenType GetTokenType(int index, string text)
         {
             if (index + 1 < text.Length && text[index] == '_' && text[index + 1] == '_')
                 return TokenType.Strong;
-            if (text[index] == '_')
-                return TokenType.Emphasized;
-            if (text[index] == '#')
-                return TokenType.Heading;
-            return TokenType.Text;
-        }
-
-
-        private TextToken FindOpenedToken(TokenType type, List<TextToken> openedTokens)
-        {
-            return openedTokens.FirstOrDefault(token => token.Type == type);
+            switch (text[index])
+            {
+                case '_':
+                    return TokenType.Emphasized;
+                case '#':
+                    return TokenType.Heading;
+                default:
+                    return TokenType.Text;
+            }
         }
     }
 }
