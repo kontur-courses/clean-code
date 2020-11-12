36d4226d66a1ee796af235fa3b8d6e34180cce16
PShenica <polebota@gmail.com>
2020-11-12 21:38:34 +0500

    implemented HtmlGenerator, HtmlConverterTests and HtmlConverter class

--- cs/Markdown/HtmlConverter.cs
+++ cs/Markdown/HtmlConverter.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Markdown
 {
-    public class HtmlConverter
+    public class HtmlConverter : IConverter
     {
-        public List<Token> CovertTokensToHtml(List<Token> tokens)
+        public string CovertTokensToHtml(List<Token> tokens)
         {
-            throw new NotImplementedException();
+            var sortedTokens = tokens.OrderBy(x => x.Position);
+            var result = new StringBuilder();
+
+            foreach (var token in sortedTokens)
+            {
+                switch (token.Type)
+                {
+                    case TokenType.Heading:
+                        result.Append($"<h1>{token.ValueWithoutTags()}</h1>");
+                        break;
+                    case TokenType.Strong:
+                        result.Append($"<strong>{token.ValueWithoutTags()}</strong>");
+                        break;
+                    case TokenType.Emphasized:
+                        result.Append($"<em>{token.ValueWithoutTags()}</em>");
+                        break;
+                    case TokenType.PlainText:
+                        result.Append(token.ValueWithoutTags());
+                        break;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
---
+++ cs/Markdown/HtmlGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class HtmlGenerator
+    {
+        private IConverter Converter { get; }
+
+        public HtmlGenerator(IConverter converter)
+        {
+            Converter = converter;
+        }
+
+        public string CovertTokensToHtml(List<Token> tokens)
+        {
+            return Converter.CovertTokensToHtml(tokens);
+        }
+    }
+}
