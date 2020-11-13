cffb265683f2123a788ee6f4b758f678a748d5a9
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-14 01:11:04 +0500

    Added tests for single markup tokens

--- cs/Markdown/MarkdownTest/MdTests.cs
+++ cs/Markdown/MarkdownTest/MdTests.cs
@@ -21,5 +21,31 @@ namespace MarkdownTest
             var text = "123 dsa 542 asfaasfg";
             md.GetHtmlMarkup(text).Should().Be(text);
         }
+
+        [Test]
+        public void SingleHeaderTest_ShouldWork()
+        {
+            var text =
+                @"# Header
+other text";
+            md.GetHtmlMarkup(text).Should().Be(@"<h1> Header</h1>
+other text");
+        }
+
+        [Test]
+        public void SingleItalicTag_ShouldWork()
+        {
+            var text = @"some text _italic text_ not italic text";
+
+            md.GetHtmlMarkup(text).Should().Be(@"some text <em>italic text</em> not italic text");
+        }
+
+        [Test]
+        public void SingleBoldTag_ShouldWork()
+        {
+            var text = @"some text __bold text__ not bold text";
+            
+            md.GetHtmlMarkup(text).Should().Be(@"some text <strong>bold text</strong> not bold text");
+        }
     }
 }
