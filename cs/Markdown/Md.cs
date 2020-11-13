d3fb901c47b92fe7f63bad18454cbc3b3fedb222
Scindapsus345 <zavnikita345@gmail.com>
2020-11-13 13:36:11 +0500

    Decomposition

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    class Md
+    {
+        public string Render(string input)
+        {
+            //SplitToTokens(string line)
+            //ConvertTokensToString(List<Token> tokens)
+            throw new NotImplementedException();
+        }
+
+        private List<Token> SplitToTokens(string line)
+        {
+            //SkipWhiteSpaces(string line, int startPos)
+            //ReadToken(string line, int startPos)
+            throw new NotImplementedException();
+        }
+
+        private int SkipWhiteSpaces(string line, int startPos)
+        {
+            //ReadToken(string line, int startPos)
+            throw new NotImplementedException();
+        }
+
+        private Token ReadToken(string line, int startPos)
+        {
+            //ReadSimpleToken(string line, int startPos)
+            //ReadStrongToken(string line, int startPos)
+            //ReadEmToken(string line, int startPos)
+            //ReadHeaderToken(string line, int startPos)
+            throw new NotImplementedException();
+        }
+
+        private Token ReadSimpleToken(string line, int startPos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Token ReadStrongToken(string line, int startPos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Token ReadEmToken(string line, int startPos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Token ReadHeaderToken(string line, int startPos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ConvertTokensToString(List<Token> tokens)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs/Markdown/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    class Token
+    {
+        public string Value { get; set; }
+        public int Position { get; set; }
+        public int Length { get; set; }
+        public List<TokenType> TokenTypes { get; set; }
+    }
+}
