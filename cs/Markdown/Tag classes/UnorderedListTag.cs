6ad293ae08c88e7792c80bdc31e3478d18ba8dad
Denis Bautin <denis020301@mail.ru>
2020-11-13 14:19:32 +0500

    Create UnorderedListTag class

---
+++ cs/Markdown/Tag classes/UnorderedListTag.cs
@@ -0,0 +1,17 @@
+﻿namespace Markdown
+{
+    public class UnorderedListTag : Tag
+    {
+        public UnorderedListTag(int startOfOpeningTag, int startOfClosingTag)
+            : base(startOfOpeningTag, startOfClosingTag)
+        {
+        }
+
+        public UnorderedListTag()
+        {
+        }
+
+        public override string MdTag => "*";
+        public override string HtmlTagName => "li";
+    }
+}
