4325e4ff9bc00299355ccaf64858eac199a28e3c
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-13 12:40:17 +0500

    Implemented method 'Convert'

--- cs/Markdown/MarkdownToHtmlConverter.cs
+++ cs/Markdown/MarkdownToHtmlConverter.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Markdown
 {
@@ -7,7 +8,17 @@ namespace Markdown
     {
         public static string Convert(string text, IEnumerable<Tag> tags)
         {
-            throw new NotImplementedException();
+            var position = 0;
+            var html = new StringBuilder();
+            foreach (var tag in tags.OrderBy(x => x.Position))
+            {
+                html.Append(text.Substring(position, tag.Position - position));
+                html.Append(tag.GetHtmlTag());
+                position = tag.Position + tag.MdTag.Length;
+            }
+
+            html.Append(text.Substring(position, text.Length - position));
+            return html.ToString();
         }
     }
 }
