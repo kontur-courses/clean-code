fe9e74a401149d29883446591d25363684751295
FoolLikeYou <Lambda2712@gmail.com>
2020-11-13 15:27:30 +0500

    created Markdown helper classes

---
+++ cs/Markdown/Markdown/Markdown/Mark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Markdown
+{
+    /// <summary>
+    /// класс содержит символ индетифицирующий тэг, все символы тэга и функцию для преобразования тэга
+    /// </summary>
+    public class Mark
+    {
+        /// <summary>
+        /// строка определяющая начало символа 
+        /// </summary>
+        public string DefiningSymbol { get; }
+        /// <summary>
+        /// все символы по порядку их использования требуемые для преобразования тэга
+        /// </summary>
+        public string[] AllSymbols { get; }
+
+        /// <summary>
+        /// функция для правильного преобразования тэга 
+        /// </summary>
+        public Func<string, string> TransformMark { get; }
+
+        public Mark(string definingSymbol, string[] allSymbols, Func<string, string> transformMark)
+        {
+            DefiningSymbol = definingSymbol;
+            AllSymbols = allSymbols;
+            TransformMark = transformMark;
+        }
+        
+        public Mark(string definingSymbol)
+        {
+            DefiningSymbol = definingSymbol;
+        }
+    }
+}
---
+++ cs/Markdown/Markdown/Markdown/MarkdownProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class MarkdownProcessor
+    {
+        public List<Mark> Marks { get; }
+        public MarkdownProcessor()
+        {
+            ///заполняется тэгами для форматирования токенов
+        }
+        public List<TokenMd> FormatTokens(List<TokenMd> tokens)
+        {
+            ///токены форматируются с использованием функций тэгов
+            throw new NotImplementedException();
+        }
+        private TokenMd FormatToken(TokenMd token)
+        {
+            ///токены форматируются с использованием функций тэгов
+            throw new NotImplementedException();
+        }
+        
+    }
+}
