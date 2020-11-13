2cece58717831abe46d314012f9994d1796ceec5
FoolLikeYou <Lambda2712@gmail.com>
2020-11-13 15:29:07 +0500

    created Md.cs and Programm.cs

---
+++ cs/Markdown/Markdown/Markdown/Md.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace Markdown
+{
+    public class Md
+    {
+        private Parser parser; 
+        private MarkdownProcessor markdownProcessor;
+
+        public Md()
+        {
+            markdownProcessor = new MarkdownProcessor();
+            parser = new Parser(markdownProcessor.Marks);
+        }
+        public string Render(string text)
+        {
+            var tokens = parser.GetTokens(text);
+            var formattedToken = markdownProcessor.FormatTokens(tokens);
+            return GetHTML(formattedToken);
+            throw new NotImplementedException();
+        }
+
+        private string GetHTML(List<TokenMd> formattedTokens)
+        {
+            // склеевает текст назад
+            throw new NotImplementedException();
+        }
+    }
+}
