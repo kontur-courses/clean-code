5e46e5092c2385b5bce3229b839666b8ef8436a6
FoolLikeYou <Lambda2712@gmail.com>
2020-11-13 15:28:13 +0500

    created Token class for Markdown

---
+++ cs/Markdown/Markdown/Markdown/TokenMd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class TokenMd
+    {
+        private string Token;
+        private string textWithoutMark;
+
+        private (string First, string Last) markSymbols;
+        
+        private List<TokenMd> InnerTokens;
+    }
+}
