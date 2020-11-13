84ae0abe420154b240a334059e85f32d78cee311
Danil <danil7953@mail.ru>
2020-11-13 19:20:28 +0500

    Create Markdown.csproj. Create Md.cs.

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public Md()
+        {
+        }
+
+        public string Render(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
