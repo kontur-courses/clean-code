7c88d85633080c9ea3b0de55051fca112336e39a
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-14 00:10:53 +0500

    Added Token

---
+++ cs/Markdown/Markdown/Token.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown
+{
+    public class Token
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public int Length => End - Start + 1;
+        public List<MarkupType> MarkupTypes { get; }
+
+        public Token(int start, int end)
+        {
+            Start = start;
+            End = end;
+            MarkupTypes = new List<MarkupType>();
+        }
+    }
+}
