1621d19a52c4ebf2c4dda06a7fff4c1dd83a68b3
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-11-14 17:06:58 +0500

    Start new realization
    Remove all unnedeed

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
 using System.Text;
 
 namespace Markdown
@@ -8,62 +10,24 @@ namespace Markdown
     {
         public string Render(string[] text)
         {
-            var stringAccumulator = new StringBuilder();
-            foreach (var line in text)
-            {
-                var token = MarkdownParser.ReadHeaderToken(line);
-                if (token.Length > 0)
-                {
-                    stringAccumulator.Append(token.Value);
-                }
-                else
-                {
-                    stringAccumulator.Append(MarkdownParser.GetHtmlValue(line));
-                }
-            }
-            return stringAccumulator.ToString();
+            throw new NotImplementedException();
         }
     }
 
     internal static class MarkdownParser
     {
-        public static string GetHtmlValue(string line)
+        public static Tag[] ReadAllItalicTokens(string line)
         {
-            var stringAccumulator = new StringBuilder();
-            for (var i = 0; i < line.Length; i++)
-            {
-                var insertedToken = ReadBoldToken(line, i);
-                insertedToken = (insertedToken.Length > 0) ? insertedToken :MarkdownParser.ReadItalicToken(line, i);
-                if (insertedToken.Length > 0)
-                {
-                    stringAccumulator.Append(insertedToken.Value);
-                    i += insertedToken.Length;
-                }
-                else
-                {
-                    var count = MarkdownParser.SkipNotStyleWords(line, i);
-                    stringAccumulator.Append(line.Substring(i, count));
-                    i += count;
-                }
-            }
-
-            return stringAccumulator.ToString();
+            throw new NotImplementedException();
         }
-        public static Token ReadItalicToken(string line, int startIndex)
+        public static Tag[] ReadAllBoldTokens(string line)
         {
-            if (!IsItalicTokenStarting(line, startIndex))
-            {
-                return Token.Empty();
-            }
-            for (var i = startIndex + 1; i < line.Length; i++)
-            {
-                if (line[i] == '_' && line[i - 1] != ' ')
-                {
-                    var lengthWithoutBorders = i - startIndex - 1;
-                    return new Token(lengthWithoutBorders + 2, startIndex, TokenType.Italic, line);
-                }
-            }
-            return Token.Empty();
+            throw new NotImplementedException();
+        }
+
+        private static bool IsItalicTokenEnded(string line, int index)
+        {
+            return line[index] == '_' && line[index - 1] != ' ';
         }
 
         private static bool IsItalicTokenStarting(string line, int startIndex)
@@ -74,36 +38,6 @@ namespace Markdown
                    && !Char.IsDigit(line[startIndex + 1])
                    && line[startIndex + 1] != ' ';
         }
-        
-        public static Token ReadBoldToken(string line, int startIndex)
-        {
-            if (!IsBoldTokenStart(line, startIndex))
-            {
-                return Token.Empty();
-            }
-
-            for (var i = startIndex + 2; i < line.Length - 1; i++)
-            {
-                if (IsBoldTokenEnd(line, i + 1))
-                {
-                    return new Token(i + 2 - startIndex, startIndex, TokenType.Bold, line);
-                }
-            }
-
-            return Token.Empty();
-        }
-
-        public static Token ReadHeaderToken(string line)
-        {
-            if (line[0] != '#')
-            {
-                return Token.Empty();
-            }
-            var token = new Token(line.Length, 0, TokenType.Header, line);
-            
-            return token;
-        }
-
         private static bool IsBoldTokenStart(string line, int startIndex)
         {
             return line[startIndex] == '_'
---
+++ cs/Markdown/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace Markdown
+{
+    public class Tag
+    {
+        public readonly string Value;
+        public readonly int Index;
+
+        public Tag(string value, int index)
+        {
+            Value = value;
+            Index = index;
+        }
+    }
+}
