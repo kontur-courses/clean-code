166c3da83c80e1c7f6fa5e0f2f078b84970d7484
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-14 11:28:21 +0500

    Added tokenizer to Md

--- cs/Markdown/Markdown/Md.cs
+++ cs/Markdown/Markdown/Md.cs
@@ -4,8 +4,17 @@ namespace Markdown
 {
     public class Md : IMarkupProcessor
     {
+        private Tokenizer tokenizer;
+        private string tags
+        
+        public Md()
+        {
+            tokenizer = new Tokenizer();
+        }
+
         public string GetHtmlMarkup(string text)
         {
+            var words = tokenizer.GetWords(text);
             return text;
         }
     }
