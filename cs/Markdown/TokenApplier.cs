c1eac5195046b9a5f54fa90a49538675b0891d78
Kisus228 <nik.babushkin.201408@gmail.com>
2020-11-15 14:00:44 +0500

    Распределена ответственность в классе Token.
    Сделана адаптация под новую сигнатуру класса TagInfo

---
+++ cs/Markdown/TokenApplier.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    public static class TokenApplier
+    {
+        public static string ApplyTokensToString(string sourceText, Token[] tokens)
+        {
+            var resultText = new StringBuilder(sourceText);
+            var influence = 0;
+            foreach (var token in tokens.OrderByDescending(token => token.StartIndex))
+            {
+                var tagInMdLength = token.TagInfo.TagInMd.Length;
+                resultText = resultText.Replace(token.TagInfo.TagInMd, $"<{token.TagInfo.TagForConverting}>", token.StartIndex + influence, 1);
+                influence += token.TagInfo.TagForConverting.Length + 2 - tagInMdLength;
+                resultText = resultText.Replace(token.TagInfo.TagInMd, $"</{token.TagInfo.TagForConverting}>", token.StartIndex + token.Length + influence - 1,
+                    1);
+                influence += token.TagInfo.TagForConverting.Length + 3 - tagInMdLength;
+            }
+
+            return resultText.ToString();
+        }
+    }
+}
---
+++ cs/Markdown/TokenParser.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Markdown
+{
+    public static class TokenParser
+    {
+        public static Token[] ParseStringToMdTokens(string sourceText, params TagInfo[] tagInfos)
+        {
+            var tokens = new List<Token>();
+            var tagsList = FindTagsIndexes(sourceText, tagInfos);
+            var tagsStack = new Stack<(TagInfo tagInfo, int startIndex)>();
+            foreach (var tagAndIndex in tagsList.OrderBy(tagAndIndex => tagAndIndex.startIndex))
+            {
+                if (tagsStack.Count == 0)
+                {
+                    tagsStack.Push(tagAndIndex);
+                    continue;
+                }
+
+                var tagInfo = tagAndIndex.tagInfo;
+                var tagInMd = tagInfo.TagInMd;
+                var peekTag = tagsStack.Peek().tagInfo.TagInMd;
+                if (tagInMd == "__" && peekTag == "_")
+                    continue;
+                if (tagInMd == peekTag)
+                {
+                    var length = tagAndIndex.startIndex + tagInMd.Length - tagsStack.Peek().startIndex;
+                    tokens.Add(new Token(tagsStack.Peek().startIndex,
+                        length,
+                        tagInfo));
+                    tagsStack.Pop();
+                    continue;
+                }
+
+                tagsStack.Push((tagInfo, tagAndIndex.startIndex));
+            }
+
+            return tokens.ToArray();
+        }
+
+        private static List<(TagInfo tagInfo, int startIndex)> FindTagsIndexes(string sourceText, TagInfo[] tagInfos)
+        {
+            var tagsList = new List<(TagInfo tagInfo, int startIndex)>();
+            var groupedTagInfos = tagInfos
+                .GroupBy(tag => tag.TagForConverting.Length);
+            foreach (var group in groupedTagInfos)
+            {
+                var tagLength = group.First().TagInMd.Length;
+                var index = 0;
+                while (index < sourceText.Length - tagLength + 1)
+                {
+                    var substring = sourceText.Substring(index, tagLength);
+                    foreach (var tagInfo in group)
+                    {
+                        if (tagInfo.TagInMd != substring || tagsList.Any(tagAndIndex =>
+                            index >= tagAndIndex.startIndex &&
+                            index <= tagAndIndex.startIndex + tagAndIndex.tagInfo?.TagInMd.Length))
+                            continue;
+                        tagsList.Add((tagInfo, index));
+                    }
+
+                    index++;
+                }
+            }
+
+            return tagsList;
+        }
+    }
+}
