3932f1d242fe1270f540bfc1398a4faa24d35791
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2020-11-15 16:06:14 +0500

    Create classes for tags

---
+++ cs/Markdown/BoldTag.cs
@@ -0,0 +1,23 @@
+﻿namespace Markdown
+{
+    public class BoldTag : Tag
+    {
+        public BoldTag(string value, int index) : base(value, index, 2)
+        {
+            
+        }
+    }
+    
+    public class OpenBoldTag : BoldTag
+    {
+        public OpenBoldTag( int index) : base("<strong>", index)
+        {
+        }
+    }
+    public class CloseBoldTag : BoldTag
+    {
+        public CloseBoldTag(int index) : base("</strong>", index)
+        {
+        }
+    }
+}
---
+++ cs/Markdown/ItalicTag.cs
@@ -0,0 +1,23 @@
+﻿namespace Markdown
+{
+    public class ItalicTag : Tag
+    {
+        public ItalicTag(string value, int index) : base(value, index, 1)
+        {
+        }
+    }
+
+    public class OpenItalicTag : ItalicTag
+    {
+        public OpenItalicTag(int index) : base("<em>", index)
+        {
+        }
+    }
+
+    public class CloseItalicTag : ItalicTag
+    {
+        public CloseItalicTag(int index) : base("</em>", index)
+        {
+        }
+    }
+}
