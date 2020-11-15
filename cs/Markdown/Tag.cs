9669f7e1c77aa82a12a0a62dcea447859b4b7054
yourname <yourname@site.com>
2020-11-16 03:50:16 +0500

    переместил все "создающие" методы в TagBuilder

--- cs/Markdown/Tag.cs
+++ cs/Markdown/Tag.cs
@@ -1,59 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace Markdown
 {
     public class Tag
     {
-        public string TagName { get; }
-        private string Mark { get; }
-        public int OpenPosition { get; }
-        public string Content => GetContent();
-        public int Lenght => Content.Length;
-        public int ClosePosition { get; }
-        private readonly string text;
-
-        private bool Closed => IsClosed();
+        public bool Closed;
         private readonly bool Screened;
 
-        public Tag(string tagName, int openPosition, int closePosition, bool isScreened,  string text)
+        public Tag(string tagName, bool isScreened)
         {
-            this.text = text;
-            
             TagName = tagName;
-            Mark = TagBuilder.marks[tagName];
-            
-            OpenPosition = openPosition;
-            ClosePosition = closePosition;
-            
             Screened = isScreened;
+            Mark = TagBuilder.GetMarkByHtmlTag(tagName);
         }
+
+        public string TagName { get; }
+        private string Mark { get; }
+        public int OpenPosition { get; set; }
+        public int ClosePosition { get; set; }
+        public string Content { get; set; }
 
         public override string ToString()
         {
-            if(!Closed || Screened)
+            if (!Closed || Screened)
                 return $"{Mark}{Content}{Mark}";
-            
+
             return $"<{TagName}>{Content}</{TagName}>";
-        }
-
-        private bool IsClosed()
-        {
-            if (Mark == "#")
-                return true;
-            
-            return text.Substring(OpenPosition, Mark.Length) == text.Substring(ClosePosition - Mark.Length + 1, Mark.Length) && !Content.Any(char.IsDigit);
-        }
-
-        private string GetContent()
-        {
-            var len = Mark != "#" ? ClosePosition - OpenPosition - 2 * Mark.Length + 1 : ClosePosition - OpenPosition;
-            if (Screened)
-                len--;
-            
-            return text.Substring(OpenPosition + Mark.Length, len);
         }
     }
 }
--- cs/Markdown/TagBuilder.cs
+++ cs/Markdown/TagBuilder.cs
@@ -1,43 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Markdown
 {
     public static class TagBuilder
     {
-        public static readonly Dictionary<string, string> tags = new Dictionary<string, string>
-        {
-            {"_", "em"},
-            {"__", "strong"},
-            {"#", "h1"}
-        };
-        
-        public static readonly Dictionary<string, string> marks = new Dictionary<string, string>
-        {
-            {"em", "_"},
-            {"strong", "__"},
-            {"h1", "#"}
-        };
-        
         public static readonly Dictionary<string, string> markPairs = new Dictionary<string, string>
         {
             {"_", "_"},
             {"__", "__"},
             {"#", "\n"}
         };
-        
+
         public static Tag BuildTag(string text, int startIndex, bool isScreened)
         {
-            var tagName = tags[$"{text[startIndex]}"];;
-            if (text[startIndex] == '_' && startIndex < text.Length - 1 && text[startIndex + 1] == '_')
-                tagName = "strong";
-
-            var mark = marks[tagName];
+            var mark = GetMark(text, startIndex);
+            var tagName = GetHtmlTagByMark(mark);
             var closePosition = FindClosePosition(text, startIndex, mark);
-            
-            return new Tag(tagName, startIndex, closePosition, isScreened, text);;
+            var content = GetContent(startIndex, closePosition, text);
+            if (isScreened)
+                content = RemoveScreeningSymbols(content);
+
+            var tag = new Tag(tagName, isScreened)
+            {
+                OpenPosition = startIndex,
+                ClosePosition = FindClosePosition(text, startIndex, mark),
+                Content = content,
+            };
+            tag.Closed = IsTagClosed(tag, text);
+
+            return tag;
         }
-        
+
         private static int FindClosePosition(string text, int startIndex, string openedMark)
         {
             var i = startIndex + 1;
@@ -49,12 +44,99 @@ namespace Markdown
                 i++;
             }
 
+            if (openedMark == "#" && i == text.Length)
+                i++;
+
             return i - 1 + mark.Length - 1;
         }
 
         public static bool ExpectedToBeMark(char c)
         {
-            return tags.Keys.Select(mark => mark[0]).Contains(c);
+            var marksFirstLetters = new HashSet<char> {'_', '#'};
+            return marksFirstLetters.Contains(c);
+        }
+
+        private static string GetContent(int startIndex, int endIndex, string text)
+        {
+            var contentBuilder = new StringBuilder();
+            var mark = GetMark(text, startIndex);
+            for (var i = startIndex + mark.Length; i <= endIndex - mark.Length; i++)
+                contentBuilder.Append(text[i]);
+
+            return contentBuilder.ToString();
+        }
+
+        private static string RemoveScreeningSymbols(string text)
+        {
+            var i = text.Length - 1;
+            var slashCount = 0;
+            while (text[text.Length-1] == text[i])
+            {
+                slashCount++;
+                i--;
+            }
+            return text.Remove(text.Length - 1, 1);
+        }
+
+        private static string GetMark(string text, int start)
+        {
+            var i = start;
+            var markBuilder = new StringBuilder();
+            while (text[i] == text[start])
+            {
+                markBuilder.Append(text[i]);
+                i++;
+            }
+
+            return markBuilder.ToString();
+        }
+
+        private static bool IsTagClosed(Tag tag, string text)
+        {
+            var mark = GetMarkByHtmlTag(tag.TagName);
+
+            if (mark == "#")
+                return true;
+
+            return text.Substring(tag.OpenPosition, mark.Length) ==
+                   text.Substring(tag.ClosePosition - mark.Length + 1, mark.Length)
+                   && !tag.Content.Any(char.IsDigit);
+        }
+
+        public static string GetMarkByHtmlTag(string htmlTag)
+        {
+            var marks = new Dictionary<string, string>
+            {
+                {"em", "_"},
+                {"strong", "__"},
+                {"h1", "#"}
+            };
+
+            return marks[htmlTag];
+        }
+
+        public static string GetHtmlTagByMark(string mark)
+        {
+            var tags = new Dictionary<string, string>
+            {
+                {"_", "em"},
+                {"__", "strong"},
+                {"#", "h1"}
+            };
+
+            return tags[mark];
+        }
+
+        public static string GetMarkPair(string mark)
+        {
+            var pairs = new Dictionary<string, string>
+            {
+                {"_", "_"},
+                {"__", "__"},
+                {"#", "\n"}
+            };
+
+            return pairs[mark];
         }
     }
 }
