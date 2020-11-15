e2ba2812357c67fe10c5ed995d886f924a078bac
Ivan Grabovsky <raccoon.twitch@gmail.com>
2020-11-15 16:47:15 +0500

    added tests that checking correct work of em tag and strong tag

--- cs/MarkdownShould/HTMLConverter_Should.cs
+++ cs/MarkdownShould/HTMLConverter_Should.cs
@@ -35,5 +35,55 @@ namespace Markdown.Tests
 
             actualString.Should().BeEquivalentTo(expectedString);
         }
+
+        [Test]
+        public void GetHTMLString_CorrectStringLine_TextTokensAndEmTokens()
+        {
+            var textTokens = new List<TextToken>
+            {
+                new TextToken(0,2,TokenType.Text, "aa"),
+                new TextToken(2,4,TokenType.Emphasized,"cccc", 
+                    new List<TextToken>
+                    {
+                        new TextToken(0, 4, TokenType.Text, "cccc")
+                    }),
+                new TextToken(6,2,TokenType.Text, "bb"),
+                new TextToken(9,4,TokenType.Emphasized,"dddd", 
+                    new List<TextToken>
+                    {
+                        new TextToken(0, 4, TokenType.Text, "dddd")
+                    }),
+            };
+            var htmlConverter = new HTMLConverter();
+            var expectedString = "aa<em>cccc</em>bb<em>dddd</em>";
+            
+            var actualString = htmlConverter.GetHTMLString(textTokens, tokenConverters);
+
+            actualString.Should().BeEquivalentTo(expectedString);
+        }
+
+        [Test]
+        public void GetHTMLString_CorrectStringLine_EmTokenInsideStrong()
+        {
+            var textTokens = new List<TextToken>
+            {
+                new TextToken(0,2,TokenType.Strong, "aa", new List<TextToken>
+                {
+                    new TextToken(0,4,TokenType.Text,"cccc"),
+                    new TextToken(4,2,TokenType.Emphasized,"ab", new List<TextToken>
+                    {
+                        new TextToken(0,2,TokenType.Text, "ab")
+                    }),
+                    new TextToken(6,4,TokenType.Text,"bbbb"),
+                }),
+                
+            };
+            var htmlConverter = new HTMLConverter();
+            var expectedString = "<strong>cccc<em>ab</em>bbbb</strong>";
+            
+            var actualString = htmlConverter.GetHTMLString(textTokens, tokenConverters);
+
+            actualString.Should().BeEquivalentTo(expectedString);
+        }
     }
 }
