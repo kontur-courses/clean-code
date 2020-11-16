aa375e509c271f8c72104213589f73cbee955929
PShenica <polebota@gmail.com>
2020-11-16 16:35:28 +0500

    refactor TextParserTests

--- cs/Markdown.Tests/TextParserTests.cs
+++ cs/Markdown.Tests/TextParserTests.cs
@@ -16,47 +16,102 @@ namespace MarkdownTests
             Parser = new TextParser();
         }
 
-        static object[] TestCases =
+        private static object[] EmphasizedTestCases =
         {
-            new TestCaseData("", new List<Token>() {}).SetName("Empty string"),
-
-            new TestCaseData("# one", new List<Token>()
-            {
-                new Token(0, "# one", TokenType.Heading)
-            }).SetName("One heading"),
-
             new TestCaseData("_word_", new List<Token>()
             {
                 new Token(0, "_word_", TokenType.Emphasized)
             }).SetName("One token"),
-
-            new TestCaseData("__let__ _me_ __in__", new List<Token>()
-            {
-                new Token(0, "__let__", TokenType.Strong),
-                new Token(7, " ", TokenType.PlainText),
-                new Token(8, "_me_", TokenType.Emphasized),
-                new Token(12, " ", TokenType.PlainText),
-                new Token(13, "__in__", TokenType.Strong),
-            }).SetName("More than one token"),
 
             new TestCaseData("_long text_", new List<Token>()
             {
                 new Token(0, "_long text_", TokenType.Emphasized),
             }).SetName("Two words in one tag"),
 
-            new TestCaseData("_long text with spaces_", new List<Token>()
-            {
-                new Token(0, "_long text with spaces_", TokenType.Emphasized),
-            }).SetName("More than two words in one tag"),
-
-            new TestCaseData("_sta_rt mi__dd__le en_d._", new List<Token>()
+            new TestCaseData("_sta_rt mi_dd_le en_d._", new List<Token>()
             {
                 new Token(0, "_sta_", TokenType.Emphasized),
                 new Token(5, "rt mi", TokenType.PlainText),
-                new Token(10, "__dd__", TokenType.Strong),
-                new Token(16, "le en", TokenType.PlainText),
-                new Token(21, "_d._", TokenType.Emphasized),
+                new Token(10, "_dd_", TokenType.Emphasized),
+                new Token(14, "le en", TokenType.PlainText),
+                new Token(19, "_d._", TokenType.Emphasized),
             }).SetName("Token in words"),
+
+            new TestCaseData("_e __s__ e_", new List<Token>()
+            {
+                new Token(0, "_e __s__ e_", TokenType.Emphasized),
+            }).SetName("Strong tag in emphasized"),
+
+            new TestCaseData("_in tag_ text", new List<Token>()
+            {
+                new Token(0, "_in tag_", TokenType.Emphasized),
+                new Token(8, " text", TokenType.PlainText),
+            }).SetName("Plain text at the end"),
+        };
+
+        [TestCaseSource("EmphasizedTestCases")]
+        public void GetTokens_ReturnEmphasizedTokens_When(string text, List<Token> expectedTokens)
+        {
+            var result = new TextParser().GetTokens(text);
+
+            result.Should().BeEquivalentTo(expectedTokens);
+        }
+
+        private static object[] StrongTestCases =
+        {
+            new TestCaseData("__let__ __me__ __in__", new List<Token>()
+            {
+                new Token(0, "__let__", TokenType.Strong),
+                new Token(7, " ", TokenType.PlainText),
+                new Token(8, "__me__", TokenType.Strong),
+                new Token(14, " ", TokenType.PlainText),
+                new Token(15, "__in__", TokenType.Strong),
+            }).SetName("More than one token"),
+
+            new TestCaseData("__long text with spaces__", new List<Token>()
+            {
+                new Token(0, "__long text with spaces__", TokenType.Strong),
+            }).SetName("More than two words in one tag"),
+
+            new TestCaseData("__s s _e_ s__", new List<Token>()
+            {
+                new Token(0, "__s s _e_ s__", TokenType.Strong),
+            }).SetName("Emphasized tag in strong"),
+
+            new TestCaseData("text __in tag__", new List<Token>()
+            {
+                new Token(0, "text ", TokenType.PlainText),
+                new Token(5, "__in tag__", TokenType.Strong),
+            }).SetName("Plain text at start"),
+        };
+
+        [TestCaseSource("StrongTestCases")]
+        public void GetTokens_ReturnStrongTokens_When(string text, List<Token> expectedTokens)
+        {
+            var result = new TextParser().GetTokens(text);
+
+            result.Should().BeEquivalentTo(expectedTokens);
+        }
+
+        private static object[] HeadingTestCases =
+        {
+            new TestCaseData("# one", new List<Token>()
+            {
+                new Token(0, "# one", TokenType.Heading)
+            }).SetName("One heading"),
+        };
+
+        [TestCaseSource("HeadingTestCases")]
+        public void GetTokens_ReturnHeadingTokens_When(string text, List<Token> expectedTokens)
+        {
+            var result = new TextParser().GetTokens(text);
+
+            result.Should().BeEquivalentTo(expectedTokens);
+        }
+
+        private static object[] PlainTextTestCases =
+        {
+            new TestCaseData("", new List<Token>() {}).SetName("Empty string"),
 
             new TestCaseData("_ empty_", new List<Token>()
             {
@@ -73,18 +128,6 @@ namespace MarkdownTests
                 new Token(0, "__12__3", TokenType.PlainText),
             }).SetName("Digits in tags"),
 
-            new TestCaseData("text _in tag_", new List<Token>()
-            {
-                new Token(0, "text ", TokenType.PlainText),
-                new Token(5, "_in tag_", TokenType.Emphasized),
-            }).SetName("Plain text at start"),
-
-            new TestCaseData("_in tag_ text", new List<Token>()
-            {
-                new Token(0, "_in tag_", TokenType.Emphasized),
-                new Token(8, " text", TokenType.PlainText),
-            }).SetName("Plain text at the end"),
-
             new TestCaseData("__No_ paired", new List<Token>()
             {
                 new Token(0, "__No_ paired", TokenType.PlainText),
@@ -94,17 +137,18 @@ namespace MarkdownTests
             {
                 new Token(0, "____", TokenType.PlainText),
             }).SetName("Empty strong tag"),
+        };
 
-            new TestCaseData("__s s _e_ s__", new List<Token>()
-            {
-                new Token(0, "__s s _e_ s__", TokenType.Strong),
-            }).SetName("Emphasized tag in strong"),
+        [TestCaseSource("PlainTextTestCases")]
+        public void GetTokens_ReturnPlainTextTokens_When(string text, List<Token> expectedTokens)
+        {
+            var result = new TextParser().GetTokens(text);
 
-            new TestCaseData("_e __s__ e_", new List<Token>()
-            {
-                new Token(0, "_e __s__ e_", TokenType.Emphasized),
-            }).SetName("Strong tag in emphasized"),
+            result.Should().BeEquivalentTo(expectedTokens);
+        }
 
+        private static object[] BackslashTestCases =
+        {
             new TestCaseData(@"\_", new List<Token>()
             {
                 new Token(0, @"\_", TokenType.PlainText),
@@ -121,8 +165,8 @@ namespace MarkdownTests
             }).SetName("Backslash before ending tag"),
         };
 
-        [TestCaseSource("TestCases")]
-        public void GetTokens_ReturnExpectedTokens_When(string text, List<Token> expectedTokens)
+        [TestCaseSource("BackslashTestCases")]
+        public void GetTokens_ReturnExpectedResult_When(string text, List<Token> expectedTokens)
         {
             var result = new TextParser().GetTokens(text);
 
