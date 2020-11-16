1493a88a6f337b5f4876c7013a586fbf7a4d0f17
VladSavitskiy <cyberl1fe@yandex.ru>
2020-11-16 19:06:50 +0500

    Created Markdown.csproj and MarkdownTests.csproj projects

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Markdown
+{
+    public class Md
+    {
+        public static string Render(string markupText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
