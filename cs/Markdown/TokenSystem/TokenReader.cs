15d03d705b46d18723109ea95b8055c7ba290844
VladSavitskiy <cyberl1fe@yandex.ru>
2020-11-16 22:48:07 +0500

    Implemented TokenReader.cs with its tests

---
+++ cs/Markdown/TokenSystem/TokenReader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace Markdown.TokenSystem
+{
+    public class TokenReader
+    {
+        public string Text { get; }
+        public int Position { get; private set; }
+            
+        public TokenReader(string text, int position)
+        {
+            Text = text;
+            Position = position;
+        }
+
+        public Token ReadUntil(Func<string, bool> isStopStatement)
+        {
+            var stringBuilder = new StringBuilder();
+            var startPosition = Position;
+
+            for (var i = startPosition; i < Text.Length; i++)
+            {
+                var sign = Text[i].ToString();
+                if (isStopStatement(sign) || (i + 1 != Text.Length && isStopStatement(sign + Text[i + 1])))
+                    return new Token(startPosition, stringBuilder.Length, stringBuilder.ToString());
+
+                stringBuilder.Append(Text[i]);
+                Position++;
+            }
+
+            return new Token(startPosition, Text.Length, stringBuilder.ToString());
+        }
+
+        public Token ReadWhile(Func<string, bool> accept)
+        {
+            return ReadUntil(str => !accept(str));
+        }
+
+        public Token TakeSigns(int count)
+        {
+            if (Position + count >= Text.Length)
+                throw new ArgumentException(
+                    "Sum of signs count and current position must be less than text length");
+            
+            var stringBuilder = new StringBuilder(count);
+            var startPosition = Position;
+            for (int i = startPosition; i < count; i++)
+            {
+                stringBuilder.Append(Text[i]);
+                Position++;
+            }
+            
+            return new Token(startPosition, count, stringBuilder.ToString());
+        }
+
+        public void StartReadingOver()
+        {
+            Position = 0;
+        }
+
+        public TokenReader SkipSigns(int count)
+        {
+            if (Position + count >= Text.Length)
+                throw new ArgumentException(
+                    "Sum of signs count and current position must be less than text length");
+            
+            Position += count;
+            return this;
+        }
+    }
+}
---
+++ cs/MarkdownTests/TokenSystemTests/TokenReaderTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Markdown.TokenSystem;
+using NUnit.Framework;
+
+namespace MarkdownTests.TokenSystemTests
+{
+    public class TokenReaderTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new TokenReader("123_some text!", 0);
+        }
+
+        private TokenReader sut;
+        
+        [TestCase("", "123_some text!")] 
+        [TestCase("1", "")] 
+        [TestCase(" ", "123_some")] 
+        [TestCase("xt", "123_some te")] 
+        public void ReadUntil_ReturnSequenceUntilCondition(string stopStr, string expected)
+        {
+            var result = sut.ReadUntil(str => str == stopStr);
+
+            result.Value.Should().Be(expected);
+        }
+        
+        [TestCase("", "123_some text!")] 
+        [TestCase("1", "")] 
+        [TestCase(" ", "123_some")] 
+        [TestCase("xt", "123_some te")] 
+        public void ReadWhile_ReturnSequenceUntilCondition(string stopStr, string expected)
+        {
+            var result = sut.ReadWhile(str => str != stopStr);
+
+            result.Value.Should().Be(expected);
+        }
+    }
+}
