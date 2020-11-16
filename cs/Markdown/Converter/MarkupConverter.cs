209d5c91a7cc2b32055dc9c53bb38016202428e7
VladSavitskiy <cyberl1fe@yandex.ru>
2020-11-16 22:50:02 +0500

    Implemented MarkupConverter.cs with its tests

---
+++ cs/Markdown/Converter/MarkupConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown.Converter
+{
+    public static class MarkupConverter
+    {
+        public static string ConvertToHtmlString(IEnumerable<Markup.Markup> markupParts)
+        {
+            var stringBuilder = new StringBuilder();
+            
+            foreach (var markup in markupParts)
+            {
+                stringBuilder.Append(markup.Tag != null
+                    ? $"<{markup.Tag.HtmlValue}>{markup.Value}</{markup.Tag.HtmlValue}>"
+                    : markup.Value);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ cs/MarkdownTests/ConverterTests/MarkupConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Markdown.Converter;
+using Markdown.Markup;
+using Markdown.Tags;
+using NUnit.Framework;
+
+namespace MarkdownTests.ConverterTests
+{
+    public class ConverterTests
+    {
+        private readonly Dictionary<string, Tag> supportedTags =
+            new Dictionary<string, Tag>
+            {
+                {"_", new Tag("_", "em", "_")},
+                {"__", new Tag("__", "strong", "__")},
+                {"#", new Tag("#", "h1", "\n")}
+            };
+        
+        [Test]
+        public void ConvertToHtmlString_ReturnHtmlString()
+        {
+            var expected =
+                "Обычный текст<em>курсивный текст</em><strong>полужирный текст</strong><h1>заголовок</h1>";
+            
+            var converted = MarkupConverter.ConvertToHtmlString(new[]
+            {
+                new Markup("Обычный текст", null),
+                new Markup("курсивный текст", supportedTags["_"]),
+                new Markup("полужирный текст", supportedTags["__"]),
+                new Markup("заголовок", supportedTags["#"])
+            });
+
+            converted.Should().Be(expected);
+        }
+    }
+}
