4988486750f0e74bed90dcba75d85a00d4bd8356
TerribleVoice <terriblevoice@ya.ru>
2020-11-17 22:20:20 +0500

    разделил Marks на htmlTags и MarkdownTags

---
+++ cs/Markdown/HtmlTags.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown
+{
+    public static class HtmlTags
+    {
+        private static readonly HashSet<string> openTags = new HashSet<string> {"<h1>", "<em>", "<strong>"};
+        private static readonly HashSet<string> closeTags = new HashSet<string> {"</h1>", "</em>", "</strong>"};
+
+        private static readonly Dictionary<string, string> marksByTag = new Dictionary<string, string>
+        {
+            {"em", "_"},
+            {"strong", "__"},
+            {"h1", "#"},
+
+            {"<em>", "_"},
+            {"<strong>", "__"},
+            {"<h1>", "#"},
+
+            {"</em>", "_"},
+            {"</strong>", "__"},
+            {"</h1>", "#"}
+        };
+
+        public static string GetMarkByHtmlTag(string mark)
+        {
+            return marksByTag[mark];
+        }
+
+        public static bool IsClosedHtmlTag(string tag)
+        {
+            return closeTags.Contains(tag);
+        }
+
+        public static TagType GetTagType(string tagName)
+        {
+            switch (tagName)
+            {
+                case "em":
+                case "<em>":
+                case "</em>":
+                    return TagType.Italic;
+
+                case "strong":
+                case "<strong>":
+                case "</strong>":
+                    return TagType.Bold;
+
+                case "h1":
+                case "<h1>":
+                case "</h1>":
+                    return TagType.Heading;
+
+                default:
+                    return TagType.Incorrect;
+            }
+        }
+
+        public static bool IsHtmlTag(string tag)
+        {
+            return openTags.Contains(tag) || closeTags.Contains(tag);
+        }
+    }
+}
---
+++ cs/Markdown/MarkdownTags.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    public static class MarkdownTags
+    {
+        private static readonly HashSet<string> marks = new HashSet<string> {"_", "__", "#"};
+
+        private static readonly Dictionary<string, string> tagsByMark = new Dictionary<string, string>
+        {
+            {"_", "em"},
+            {"__", "strong"},
+            {"#", "h1"}
+        };
+
+        private static readonly Dictionary<string, string> pairs = new Dictionary<string, string>
+        {
+            {"_", "_"},
+            {"__", "__"},
+            {"#", "\n"}
+        };
+
+        public static string GetHtmlTagByMark(string mark)
+        {
+            return tagsByMark[mark];
+        }
+
+        public static string GetMarkPair(string mark)
+        {
+            return pairs[mark];
+        }
+
+        public static bool IsMark(string mark)
+        {
+            return marks.Contains(mark);
+        }
+
+        public static string GetMarkFromText(int start, string text)
+        {
+            var i = start;
+            var markBuilder = new StringBuilder();
+            while (i < text.Length && text[i] == text[start])
+            {
+                markBuilder.Append(text[i]);
+                i++;
+            }
+
+            return markBuilder.ToString();
+        }
+
+        public static bool ExpectedToBeMark(int index, string text)
+        {
+            var marksFirstLetters = marks.Select(mark => mark[0]).ToHashSet();
+
+            return index + 1 < text.Length && marksFirstLetters.Contains(text[index]) &&
+                   !char.IsWhiteSpace(text[index + 1]) ||
+                   index + 1 == text.Length && marksFirstLetters.Contains(text[index]);
+        }
+    }
+}
