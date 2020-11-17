467dbacffc1959e88c2c27b3d8a8cc32998d5e33
MaxCross <dan25001@gmail.com>
2020-11-17 16:22:27 +0500

    Вынес MdTokenWithSubTokens в отдельный файл

---
+++ cs/Markdown/MdTokenWithSubTokens.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown
+{
+    public abstract class MdTokenWithSubTokens : MdToken
+    {
+        protected MdTokenWithSubTokens(int startPosition, int length = 0) : base(startPosition, length) { }
+
+        public List<MdToken> SubTokens = new List<MdToken>();
+
+        public void AddSubtoken(MdToken subToken)
+        {
+            SubTokens.Add(subToken);
+            subToken.ParentToken = this;
+            Length += subToken.Length;
+        }
+
+        public void RemoveSubtokenAt(int index)
+        {
+            Length -= SubTokens[index].Length;
+            SubTokens.RemoveAt(index);
+        }
+
+        public void RemoveLastSubtoken() => RemoveSubtokenAt(SubTokens.Count - 1);
+
+        public void SetSubtokenCount(int count)
+        {
+            while (SubTokens.Count > count) RemoveLastSubtoken();
+        }
+    }
+}
