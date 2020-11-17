ca55e666fa3640d6d3041dee706724a7feb5d70b
yourname <yourname@site.com>
2020-11-17 15:37:03 +0500

    добавил тестов на базовую реализацию

--- cs/MarkDownTests/Tests.cs
+++ cs/MarkDownTests/Tests.cs
@@ -12,7 +12,9 @@ namespace MarkDownTests
         public void Render_EmTag_OnlyTag()
         {
             var text = "_abc_";
+            
             var result = Md.Render(text);
+            
             result.Should().Be("<em>abc</em>");
         }
 
@@ -20,6 +22,7 @@ namespace MarkDownTests
         public void Render_PlainText__OnlyText()
         {
             var text = "abc";
+            
             Md.Render(text).Should().Be(text);
         }
 
@@ -27,7 +30,9 @@ namespace MarkDownTests
         public void Render_StrongTag_OnlyTag()
         {
             var text = "__abc__";
+            
             var result = Md.Render(text);
+            
             result.Should().Be("<strong>abc</strong>");
         }
         
@@ -35,7 +40,9 @@ namespace MarkDownTests
         public void Render_H1Tag_OnlyTag()
         {
             var text = "#abc";
+            
             var result = Md.Render(text);
+            
             result.Should().Be("<h1>abc</h1>");
         }
 
@@ -44,7 +51,9 @@ namespace MarkDownTests
         public void Render_TagAndText_TextAfterTag(string mark)
         {
             var text = $"{mark}abc{mark} abc";
-            var tagName = TagBuilder.tags[mark];
+            
+            var tagName = Marks.GetHtmlTagByMark(mark);
+            
             Md.Render(text).Should().Be($"<{tagName}>abc</{tagName}> abc");
         }
         
@@ -53,7 +62,9 @@ namespace MarkDownTests
         public void Render_TextAndTag_TextBeforeTag(string mark)
         {
             var text = $"abc {mark}abc{mark}";
-            var tagName = TagBuilder.tags[mark];
+            
+            var tagName = Marks.GetHtmlTagByMark(mark);
+            
             Md.Render(text).Should().Be($"abc <{tagName}>abc</{tagName}>");
         }
         
@@ -62,7 +73,9 @@ namespace MarkDownTests
         public void Render_TagAndText_TextAfterTagWithoutSpace(string mark)
         {
             var text = $"{mark}abc{mark}abc";
-            var tagName = TagBuilder.tags[mark];
+            
+            var tagName = Marks.GetHtmlTagByMark(mark);
+            
             Md.Render(text).Should().Be($"<{tagName}>abc</{tagName}>abc");
         }
         
@@ -71,7 +84,8 @@ namespace MarkDownTests
         public void Render_TextAndTag_TextBeforeTagWithoutSpace(string mark)
         {
             var text = $"abc{mark}abc{mark}";
-            var tagName = TagBuilder.tags[mark];
+            var tagName = Marks.GetHtmlTagByMark(mark);
+            
             Md.Render(text).Should().Be($"abc<{tagName}>abc</{tagName}>");
         }
 
@@ -80,6 +94,7 @@ namespace MarkDownTests
         public void Render_PlainText_MarksIntoTextWithDigits(string mark)
         {
             var text = $"abc{mark}12{mark}3";
+            
             Md.Render(text).Should().Be(text);
         }
         
@@ -88,8 +103,119 @@ namespace MarkDownTests
         public void Render_PlainText_ScreenedMarks(string mark)
         {
             var text = $"\\{mark}abc\\{mark}";
+            
             Md.Render(text).Should().Be($"{mark}abc{mark}");
         }
+
+        [TestCase("__")]
+        [TestCase("_")]
+        public void Render_PlainText_ScreenedMarksWith3Slashes(string mark)
+        {
+            var text = $"\\\\\\{mark}abc\\\\\\{mark}";
+            
+            Md.Render(text).Should().Be($"\\\\{mark}abc\\\\{mark}");
+        }
+
+        [Test]
+        public void Render_PlainText_SlashInsidePlainText()
+        {
+            var text = "ab\\c";
+
+            Md.Render(text).Should().Be(text);
+        }
+
+        [Test]
+        public void Render_PlainText_SlashInsideTag()
+        {
+            var text = "_ab\\c_";
+
+            Md.Render(text).Should().Be("<em>ab\\c</em>");
+        }
+
+        [TestCase("__")]
+        [TestCase("_")]
+        public void Render_TagInsideWord_MdTagAtBeginningOfWord(string mark)
+        {
+            var text = $"{mark}ab{mark}c";
+            var tagName = Marks.GetHtmlTagByMark(mark);
+
+            Md.Render(text).Should().Be($"<{tagName}>ab</{tagName}>c");
+        }
         
+        [TestCase("__")]
+        [TestCase("_")]
+        public void Render_TagInsideWord_MdTagAtMiddleOfWord(string mark)
+        {
+            var text = $"a{mark}b{mark}c";
+            var tagName = Marks.GetHtmlTagByMark(mark);
+
+            Md.Render(text).Should().Be($"a<{tagName}>b</{tagName}>c");
+        }
+        
+        [TestCase("__")]
+        [TestCase("_")]
+        public void Render_TagInsideWord_MdTagAtEndOfWord(string mark)
+        {
+            var text = $"ab{mark}c{mark}";
+            var tagName = Marks.GetHtmlTagByMark(mark);
+
+            Md.Render(text).Should().Be($"ab<{tagName}>c</{tagName}>");
+        }
+
+        [Test]
+        public void Render_PlainText_EmptyBetweenMarks()
+        {
+            const string text = "______";
+
+            Md.Render(text).Should().Be(text);
+        }
+
+        [Test]
+        public void Render_PlainText_SpaceAfterOpenedMark()
+        {
+            var text = "_ abc_";
+
+            Md.Render(text).Should().Be(text);
+        }
+        
+        [Test]
+        public void Render_PlainText_SpaceBeforeClosedMark()
+        {
+            var text = "_abc _";
+
+            Md.Render(text).Should().Be(text);
+        }
+
+        [Test]
+        public void Render_EmTagWithMarkInside_MarkSurroundedBySpacesInsideTag()
+        {
+            var text = "_abc _ abc_";
+
+            Md.Render(text).Should().Be("<em>abc _ abc</em>");
+        }
+
+        [Test]
+        public void Render_PlainTextWithMarksInside_MarksInsideDifferentWords()
+        {
+            var text = "a_bc ab_c";
+
+            Md.Render(text).Should().Be(text);
+        }
+
+        [Test]
+        public void Render_EmTagInsideStrongTag_EmTagInsideStrongTag()
+        {
+            var text = "__abc _abc_ abc__";
+
+            Md.Render(text).Should().Be("<strong>abc <em>abc</em> abc</strong>");
+        }
+
+        [Test]
+        public void Render_EmTagWithDoubleMarkInside_StrongTagInsideEmTag()
+        {
+            var text = "_abc __abc__ abc_";
+
+            Md.Render(text).Should().Be("<em>abc __abc__ abc</em>");
+        }
     }
 }
