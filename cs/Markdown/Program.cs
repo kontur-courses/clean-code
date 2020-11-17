7f7e9470cd028fc0ea22274afa45ddbf6b03b9d1
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-11-17 10:06:45 +0500

    Added usge rules in README.md

--- cs/Markdown/Program.cs
+++ cs/Markdown/Program.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace Markdown
 {
     public static class Program
     {
-        private static void Main(string inputFile = "MarkdownSpec.md", string outputFile = "MarkdownSpec.html")
+        /// <summary>
+        /// Implementation of an algorithm for converting md-document into html-document.
+        /// </summary>
+        /// <param name="inputFile">Path to md-document</param>
+        /// <param name="outputFile">Path to result html-document. It will be created if necessary.</param>
+        private static void Main(string inputFile = "./MarkdownSpec.md", string outputFile = "./MarkdownSpec.html")
         {
             var source = File.ReadAllText(inputFile);
             var result = MarkdownEngine.Render(source);
---
+++ cs/Markdown/README.md
@@ -0,0 +1,10 @@
+﻿## Usage rules
+The rules are simple
+
+  1) Build project.
+  
+  2) Go to the build folder (default: .\bin\Debug\netcoreapp3.1) using the console.
+  
+  3) Launch Markdown.exe.
+  
+  4) If you need additional settings, use Markdown.exe --help or -h for more information.
