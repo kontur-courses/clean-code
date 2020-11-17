93060c079ecea8b04256ea2012a43aa1948e8a4b
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-17 06:20:22 +0500

    Moved functionality 'Tag' to 'TagHelper'

---
+++ cs/Markdown/TagHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace Markdown
+{
+    public abstract class TagHelper
+    {
+        private readonly string htmlTag;
+
+        protected TagHelper(string mdTag, string htmlTag)
+        {
+            MdTag = mdTag;
+            this.htmlTag = htmlTag;
+        }
+
+        protected string MdTag { get; }
+
+
+        public abstract bool TryParse(int position, string text, out Tag tag, bool inWord = false);
+
+        public virtual bool ParseForEscapeTag(int position, string text)
+        {
+            return IsTag(position + 1, text);
+        }
+
+        public string GetHtmlTag(bool isOpening)
+        {
+            return isOpening ? htmlTag : htmlTag.Insert(1, "/");
+        }
+
+        public virtual int GetSymbolsCountToSkipForParsing()
+        {
+            return MdTag.Length;
+        }
+
+        protected bool IsTag(int position, string text)
+        {
+            return position + MdTag.Length <= text.Length
+                   && text.Substring(position, MdTag.Length) == MdTag;
+        }
+    }
+}
