9bba295757f599cf43c443af8c34b6410def8fe0
MaxCross <dan25001@gmail.com>
2020-11-18 17:57:53 +0500

    Использовал метод добавления обычных токенов из TokenReader в MdTokenReader

--- cs/Markdown/MdTokenReader.cs
+++ cs/Markdown/MdTokenReader.cs
@@ -6,22 +6,9 @@ namespace Markdown
     {
         public MdTokenReader(string text) : base(text)
         {
-            AddToken((reader, parent)
-                => ReadFullLineToken<MdHeaderToken>(reader, parent, "# ", ""));
-            AddToken((reader, parent)
-                => ReadHighlightToken<MdBoldToken>(reader, parent, "__", "__"));
-            AddToken((reader, parent)
-                => ReadHighlightToken<MdItalicToken>(reader, parent, "_", "_"));
-        }
-        
-        public TToken ReadFullLineToken<TToken>(TokenReader reader, Token parent, string startWith, string endWith)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TToken ReadHighlightToken<TToken>(TokenReader reader, Token parent, string startWith, string endWith)
-        {
-            throw new NotImplementedException();
+            AddBasicToken<MdHeaderToken>("\n# ", "");
+            AddBasicToken<MdBoldToken>(" __", "__ ");
+            AddBasicToken<MdItalicToken>(" _", "_ ");
         }
     }
 }
