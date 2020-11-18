7ea2f0da3f360add22d26092b224ef401042fb8c
MaxCross <dan25001@gmail.com>
2020-11-18 20:45:01 +0500

    Тесты на рендеринг заголовка

---
+++ cs/Markdown/Tests/Md_Should.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Markdown
+{
+    [TestFixture]
+    public static class Md_Should
+    {
+        [TestCase("# Заголовок", ExpectedResult = "<h1>Заголовок</h1>", TestName = "RenderHeader")]
+        [TestCase("# Заголовок\n", ExpectedResult = "<h1>Заголовок</h1>\n", TestName = "RenderHeader_WhenEndsWithNewLine")]
+        [TestCase("\n# Заголовок", ExpectedResult = "\n<h1>Заголовок</h1>", TestName = "RenderHeader_WhenBeginsWithNewLine")]
+        public static string RenderHeader(string markdown) => Md.Render(markdown);
+    }
+}
