d3aeb335f7dc6d4eed5b5c09a972150bedbabe56
MaxCross <dan25001@gmail.com>
2020-11-18 21:13:38 +0500

    Тесты на полужирный текст

--- cs/Markdown/Tests/Md_Should.cs
+++ cs/Markdown/Tests/Md_Should.cs
@@ -5,9 +5,23 @@ namespace Markdown
     [TestFixture]
     public static class Md_Should
     {
-        [TestCase("# Заголовок", ExpectedResult = "<h1>Заголовок</h1>", TestName = "RenderHeader")]
-        [TestCase("# Заголовок\n", ExpectedResult = "<h1>Заголовок</h1>\n", TestName = "RenderHeader_WhenEndsWithNewLine")]
-        [TestCase("\n# Заголовок", ExpectedResult = "\n<h1>Заголовок</h1>", TestName = "RenderHeader_WhenBeginsWithNewLine")]
+        [TestCase("# Заголовок",
+            ExpectedResult = "<h1>Заголовок</h1>",
+            TestName = "RenderHeader")]
+        [TestCase("# Заголовок\n",
+            ExpectedResult = "<h1>Заголовок</h1>\n",
+            TestName = "RenderHeader_WhenEndsWithNewLine")]
+        [TestCase("\n# Заголовок",
+            ExpectedResult = "\n<h1>Заголовок</h1>",
+            TestName = "RenderHeader_WhenBeginsWithNewLine")]
         public static string RenderHeader(string markdown) => Md.Render(markdown);
+        
+        [TestCase("__Выделение__",
+            ExpectedResult = "<strong>Выделение</strong>",
+            TestName = "RenderBold")]
+        [TestCase("Некоторый текст __выделение__ ещё текст",
+            ExpectedResult = "Некоторый текст <strong>выделение</strong> ещё текст",
+            TestName = "RenderBold_InMiddleOfLine")]
+        public static string RenderBold(string markdown) => Md.Render(markdown);
     }
 }
