10546b4fec2e6223f046a00321b8fea008ba0c79
MaxCross <dan25001@gmail.com>
2020-11-19 03:31:01 +0500

    Добавил MdDigitToken

--- cs/Markdown/MdTokenReader.cs
+++ cs/Markdown/MdTokenReader.cs
@@ -6,9 +6,26 @@ namespace Markdown
     {
         public MdTokenReader(string text) : base(text)
         {
+            AddToken(ReadDigitToken);
+            
             AddBasicToken<MdHeaderToken>("\n# ", "\n");
             AddBasicToken<MdBoldToken>("__", "__", typeof(MdItalicToken));
             AddBasicToken<MdItalicToken>("_", "_");
+        }
+
+        public static MdDigitToken ReadDigitToken(TokenReader reader, Token parent)
+        {
+            if (!reader.IsAfterSpace()) return null;
+            var hasDigits = false;
+            var position = reader.CurrentPosition;
+            var offset = 0;
+            
+            for (; !reader.IsAtSpace(offset); offset++)
+                hasDigits |= char.IsDigit(reader.Text[position + offset]);
+            
+            if (!hasDigits) return null;
+            reader.CurrentPosition += offset;
+            return new MdDigitToken {StartPosition = position, Length = offset};
         }
     }
 }
---
+++ cs/Markdown/MdTokens/MdDigitToken.cs
@@ -0,0 +1,14 @@
+﻿namespace Markdown
+{
+    public class MdDigitToken : Token
+    {
+        public MdDigitToken() : this(0, 0, null)
+        {
+        }
+
+        public MdDigitToken(int startPosition, int length = 0, Token parent = null)
+            : base(startPosition, length, parent)
+        {
+        }
+    }
+}
