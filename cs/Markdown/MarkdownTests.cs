9fd92bd7a81020d7bc0c10a0e5e3b1482e51cb38
Rinsz <zer0zaur@mail.ru>
2020-11-19 15:57:05 +0500

    Added MarkdownSpec parsing test
    -Changed efficiency delta
    -expected parameter replaced with ExpectedResult

--- cs/Markdown/MarkdownTests.cs
+++ cs/Markdown/MarkdownTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Text;
 using FluentAssertions;
 using NUnit.Framework;
@@ -7,67 +8,60 @@ namespace Markdown
 {
     public class MarkdownTests
     {
-        [TestCase("I'm a _cursive_ :3", "I'm a <em>cursive</em> :3")]
-        [TestCase("_I'm a cursive :3_", "<em>I'm a cursive :3</em>")]
-        [TestCase("_I'm a_ _cursive :3_", "<em>I'm a</em> <em>cursive :3</em>")]
-        public void Should_SupportCursiveTag(string input, string expected)
+        [TestCase("I'm a _cursive_ :3", ExpectedResult = "I'm a <em>cursive</em> :3")]
+        [TestCase("_I'm a cursive :3_", ExpectedResult = "<em>I'm a cursive :3</em>")]
+        [TestCase("_I'm a_ _cursive :3_", ExpectedResult = "<em>I'm a</em> <em>cursive :3</em>")]
+        public string Should_SupportCursiveTag(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("I'm a __bold__ :3", "I'm a <strong>bold</strong> :3")]
-        [TestCase("__I'm a bold :3__", "<strong>I'm a bold :3</strong>")]
-        [TestCase("__I'm a__ __bold :3__", "<strong>I'm a</strong> <strong>bold :3</strong>")]
-        public void Should_SupportBoldTag(string input, string expected)
+        [TestCase("I'm a __bold__ :3", ExpectedResult = "I'm a <strong>bold</strong> :3")]
+        [TestCase("__I'm a bold :3__", ExpectedResult = "<strong>I'm a bold :3</strong>")]
+        [TestCase("__I'm a__ __bold :3__", ExpectedResult = "<strong>I'm a</strong> <strong>bold :3</strong>")]
+        public string Should_SupportBoldTag(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("# Header", "<h1>Header</h1>")]
-        [TestCase("# Header with many words", "<h1>Header with many words</h1>")]
-        [TestCase("This is # not header", "This is # not header")]
-        public void Should_SupportHeaderTag(string input, string expected)
+        [TestCase("# Header", ExpectedResult = "<h1>Header</h1>")]
+        [TestCase("# Header with many words", ExpectedResult = "<h1>Header with many words</h1>")]
+        [TestCase("This is # not header", ExpectedResult = "This is # not header")]
+        public string Should_SupportHeaderTag(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("\\# Cat\\s \\\\__are__ \\_cute_", "# Cat\\s \\<strong>are</strong> _cute_")]
-        [TestCase(@"\\\\", @"\\\\")] //Так как последний экран ничего не экранирует, то он считается как обычный символ, аналогично остальные
-        public void Should_SupportShielding(string input, string expected)
+        [TestCase("\\# Cat\\s \\\\__are__ \\_cute_", ExpectedResult = "# Cat\\s \\<strong>are</strong> _cute_")]
+        [TestCase(@"\\\\", ExpectedResult = @"\\\\")] //Так как последний экран ничего не экранирует, то он считается как обычный символ, аналогично остальные
+        public string Should_SupportShielding(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("Hey i'm covered _part_ial", "Hey i'm covered <em>part</em>ial")]
-        [TestCase("Hey i'm covered part_ial_", "Hey i'm covered part<em>ial</em>")]
-        [TestCase("Hey i'm covered __part__ial", "Hey i'm covered <strong>part</strong>ial")]
-        [TestCase("Hey i'm covered part__ial__", "Hey i'm covered part<strong>ial</strong>")]
-        public void Should_SupportPartialCovering_ForOneWord(string input, string expected)
+        [TestCase("Hey i'm covered _part_ial", ExpectedResult = "Hey i'm covered <em>part</em>ial")]
+        [TestCase("Hey i'm covered part_ial_", ExpectedResult = "Hey i'm covered part<em>ial</em>")]
+        [TestCase("Hey i'm covered __part__ial", ExpectedResult = "Hey i'm covered <strong>part</strong>ial")]
+        [TestCase("Hey i'm covered part__ial__", ExpectedResult = "Hey i'm covered part<strong>ial</strong>")]
+        public string Should_SupportPartialCovering_ForOneWord(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("__ ignored__", "__ ignored__")]
-        [TestCase("_ ignored_", "_ ignored_")]
-        [TestCase("# Ha-ha _ we're_ __ ignored__", "<h1>Ha-ha _ we're_ __ ignored__</h1>")]
-        public void Should_IgnoreTag_WhenWhiteSpace_AfterOpener(string input, string expected)
+        [TestCase("__ ignored__", ExpectedResult = "__ ignored__")]
+        [TestCase("_ ignored_", ExpectedResult = "_ ignored_")]
+        [TestCase("# Ha-ha _ we're_ __ ignored__", ExpectedResult = "<h1>Ha-ha _ we're_ __ ignored__</h1>")]
+        public string Should_IgnoreTag_WhenWhiteSpace_AfterOpener(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("_ignored _", "_ignored _")]
-        [TestCase("__ignored __", "__ignored __")]
-        [TestCase("# Ha _gotcha _ :3_ __And __ u2__", "<h1>Ha <em>gotcha _ :3</em> <strong>And __ u2</strong></h1>")]
-        public void Should_IgnoreTag_WhenWhiteSpace_BeforeCloser(string input, string expected)
+        [TestCase("_ignored _", ExpectedResult = "_ignored _")]
+        [TestCase("__ignored __", ExpectedResult = "__ignored __")]
+        [TestCase("# Ha _gotcha _ :3_ __And __ u2__", ExpectedResult = "<h1>Ha <em>gotcha _ :3</em> <strong>And __ u2</strong></h1>")]
+        public string Should_IgnoreTag_WhenWhiteSpace_BeforeCloser(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
         [Test]
@@ -80,8 +74,8 @@ namespace Markdown
         [Test]
         public void Should_IgnoreDifferentTagIntersections()
         {
-            MdRenderer.Render("_Uhm... what __the _f**k__ __is_ ___going_ on__ here?")
-                .Should().Be("_Uhm... what __the _f**k__ <strong>is_ __<em>going</em> on</strong> here?");
+            MdRenderer.Render("__Many _intersected __tags_ here__ :)__")
+                .Should().Be("<strong>Many _intersected __tags_ here__ :)</strong>");
         }
 
         [Test]
@@ -91,53 +85,60 @@ namespace Markdown
                 .Should().Be("<em>Ugh :( __I'm ignored again__ :(</em>");
         }
 
-        [TestCase("part_ial cover_", "part_ial cover_")]
-        [TestCase("part_ial cov_er", "part_ial cov_er")]
-        [TestCase("_partial c_over", "_partial c_over")]
-        [TestCase("# I'm a pa_rt an_d I'm ignored :(", "<h1>I'm a pa_rt an_d I'm ignored :(</h1>")]
-        public void Should_IgnoreTags_WhichCovering_DifferentWordsPartially(string input, string expected)
+        [TestCase("part_ial cover_", ExpectedResult = "part_ial cover_")]
+        [TestCase("part_ial cov_er", ExpectedResult = "part_ial cov_er")]
+        [TestCase("_partial c_over", ExpectedResult = "_partial c_over")]
+        [TestCase("# I'm a pa_rt an_d I'm ignored :(", ExpectedResult = "<h1>I'm a pa_rt an_d I'm ignored :(</h1>")]
+        public string Should_IgnoreTags_WhichCovering_DifferentWordsPartially(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("_12_3", "_12_3")]
-        [TestCase("__12__3", "__12__3")]
-        [TestCase("1_2_3", "1_2_3")]
-        [TestCase("1__2__3", "1__2__3")]
-        [TestCase("12_3_", "12_3_")]
-        [TestCase("12__3__", "12__3__")]
-        [TestCase("123_qwe_", "123_qwe_")]
-        [TestCase("123__qwe__", "123__qwe__")]
-        [TestCase("_123_", "<em>123</em>")]
-        [TestCase("_123qwe_", "<em>123qwe</em>")]
-        [TestCase("__123__", "<strong>123</strong>")]
-        [TestCase("__123qwe__", "<strong>123qwe</strong>")]
-        public void Should_IgnoreTags_WhichIntersectedWordWithNumber(string input, string expected)
+        [TestCase("_12_3", ExpectedResult = "_12_3")]
+        [TestCase("__12__3", ExpectedResult = "__12__3")]
+        [TestCase("1_2_3", ExpectedResult = "1_2_3")]
+        [TestCase("1__2__3", ExpectedResult = "1__2__3")]
+        [TestCase("12_3_", ExpectedResult = "12_3_")]
+        [TestCase("12__3__", ExpectedResult = "12__3__")]
+        [TestCase("123_qwe_", ExpectedResult = "123_qwe_")]
+        [TestCase("123__qwe__", ExpectedResult = "123__qwe__")]
+        [TestCase("_123_", ExpectedResult = "<em>123</em>")]
+        [TestCase("_123qwe_", ExpectedResult = "<em>123qwe</em>")]
+        [TestCase("__123__", ExpectedResult = "<strong>123</strong>")]
+        [TestCase("__123qwe__", ExpectedResult = "<strong>123qwe</strong>")]
+        public string Should_IgnoreTags_WhichIntersectedWordWithNumber(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("___word___", "<strong><em>word</em></strong>")]
-        [TestCase("__Here is a _word_ and _another_ :3__", "<strong>Here is a <em>word</em> and <em>another</em> :3</strong>")]
-        [TestCase("# ___word___", "<h1><strong><em>word</em></strong></h1>")]
-        [TestCase("# ___[Link](https://google.com)___", "<h1><strong><em><a href=https://google.com>Link</a></em></strong></h1>")]
-        public void Should_SupportCorrectIntersections(string input, string expected)
+        [TestCase("___word___", ExpectedResult = "<strong><em>word</em></strong>")]
+        [TestCase("__Here is a _word_ and _another_ :3__", ExpectedResult = "<strong>Here is a <em>word</em> and <em>another</em> :3</strong>")]
+        [TestCase("# ___word___", ExpectedResult = "<h1><strong><em>word</em></strong></h1>")]
+        [TestCase("# ___[Link](https://google.com)___", ExpectedResult = "<h1><strong><em><a href=https://google.com>Link</a></em></strong></h1>")]
+        public string Should_SupportCorrectIntersections(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
         }
 
-        [TestCase("[Link](https://google.com/)", "<a href=https://google.com/>Link</a>")]
-        [TestCase("[Link](https://go_og_le.com/)", "<a href=https://go_og_le.com/>Link</a>")]
-        [TestCase("[Link](https://go__og__le.com/)", "<a href=https://go__og__le.com/>Link</a>")]
-        [TestCase("\\[Link](https://w_oo_f.com)", "[Link](https://w<em>oo</em>f.com)")]
-        [TestCase("___Here is a [Link](https://google.com)___", "<strong><em>Here is a <a href=https://google.com>Link</a></em></strong>")]
-        public void Should_SupportLinks(string input, string expected)
+        [TestCase("[Link](https://google.com/)", ExpectedResult = "<a href=https://google.com/>Link</a>")]
+        [TestCase("[Link](https://go_og_le.com/)", ExpectedResult = "<a href=https://go_og_le.com/>Link</a>")]
+        [TestCase("[Link](https://go__og__le.com/)", ExpectedResult = "<a href=https://go__og__le.com/>Link</a>")]
+        [TestCase("\\[Link](https://w_oo_f.com)", ExpectedResult = "[Link](https://w<em>oo</em>f.com)")]
+        [TestCase("___Here is a [Link](https://google.com)___", ExpectedResult = "<strong><em>Here is a <a href=https://google.com>Link</a></em></strong>")]
+        public string Should_SupportLinks(string input)
         {
-            MdRenderer.Render(input)
-                .Should().Be(expected);
+            return MdRenderer.Render(input);
+        }
+
+        [Test]
+        public void Should_ParseMarkdownSpecCorrectly()
+        {
+            var projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.Parent?.Parent?.FullName;
+            var mdSpec = File.ReadAllLines($"{projectDirectory}/MarkdownSpec.md");
+            var expectedParseResult = File.ReadAllLines($"{projectDirectory}/MarkdownSpecParsedCorrectly.md");
+            for (var i = 0; i < mdSpec.Length; i++)
+                MdRenderer.Render(mdSpec[i])
+                    .Should().Be(expectedParseResult[i]);
         }
 
         [Test]
@@ -163,8 +164,9 @@ namespace Markdown
                 sw.Stop();
                 var currentTime = sw.ElapsedTicks;
                 var expectedTime = firstTime * (i + 1);
+                var delta = 2;
                 currentTime
-                    .Should().BeLessOrEqualTo(expectedTime + expectedTime / 2);
+                    .Should().BeLessOrEqualTo(expectedTime * delta);
             }
         }
     }
