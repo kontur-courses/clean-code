a50f0fe3d794588dd8e06b0654bf2957596a47e7
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-21 01:23:41 +0500

    Reworked Md

--- cs/Markdown/Markdown/Md.cs
+++ cs/Markdown/Markdown/Md.cs
@@ -1,21 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Markdown
 {
-    public class Md : IMarkupProcessor
+    public class Md
     {
-        private Tokenizer tokenizer;
-        private string tags
-        
-        public Md()
+        private string[] tags = {"__", "_", "# "};
+
+        private Dictionary<MarkupType, string> markupToTag = new Dictionary<MarkupType, string>
         {
-            tokenizer = new Tokenizer();
+            {MarkupType.Bold, "strong"},
+            {MarkupType.Italic, "em"},
+            {MarkupType.Default, ""},
+            {MarkupType.Header, "h1"}
+        };
+
+        private MarkupProcessor markupProcessor;
+
+        public Md(Dictionary<MarkupType, string> markupToTag)
+        {
+            markupProcessor = new MarkupProcessor(markupToTag);
         }
 
-        public string GetHtmlMarkup(string text)
+        public Md()
         {
-            var words = tokenizer.GetWords(text);
-            return text;
+        }
+
+        //Вынести эти методы из Md
+        public string MarkdownToHtml(string text)
+        {
+            var htmlText = new StringBuilder();
+            var tokenizer = new Tokenizer(text, tags);
+            var currentMarkup = new Stack<MarkupType>();
+
+            foreach (var token in tokenizer.GetTokens())
+            {
+                if (token.IsMarkup)
+                {
+                    if (currentMarkup.Any() &&
+                        currentMarkup.Peek() == markupProcessor.GetMarkupType(text, token))
+                    {
+                        htmlText.Append(markupProcessor.GetClosingTag(text, token));
+                        currentMarkup.Pop();
+                    }
+                    else
+                    {
+                        htmlText.Append(markupProcessor.GetOpeningTag(text, token));
+                        currentMarkup.Push(markupProcessor.GetMarkupType(text, token));
+                    }
+                }
+                else
+                    htmlText.Append(text.Substring(token));
+            }
+
+            return htmlText.ToString();
         }
     }
 }
