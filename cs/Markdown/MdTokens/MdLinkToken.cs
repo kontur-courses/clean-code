ee861bd807926918a2a1c030362ebdd5ef6aa922
MaxCross <dan25001@gmail.com>
2020-11-20 20:15:06 +0500

    Переместил MdLinkToken в отдельный файл

---
+++ cs/Markdown/MdTokens/MdLinkToken.cs
@@ -0,0 +1,26 @@
+﻿namespace Markdown
+{
+    public class MdLinkToken : Token
+    {
+        public MdLinkTextToken TextToken;
+        public MdLinkUrlToken UrlToken;
+
+        public MdLinkToken(int position, int length = 0, Token parent = null) : base(position, length, parent)
+        {
+        }
+        
+        public class MdLinkTextToken : BasicToken
+        {
+            public MdLinkTextToken() : base(0, 0, null)
+            {
+            }
+        }
+        
+        public class MdLinkUrlToken : BasicToken
+        {
+            public MdLinkUrlToken() : base(0, 0, null)
+            {
+            }
+        }
+    }
+}
