233743c59202806b6cbd8e287c288454817758e6
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-21 01:23:11 +0500

    Added string extensions

---
+++ cs/Markdown/Markdown/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Markdown
+{
+    public static class StringExtension
+    {
+        public static string Substring(this string s, Token token)
+        {
+            return s.Substring(token.Start, token.Length);
+        }
+    }
+}
