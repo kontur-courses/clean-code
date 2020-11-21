980ff31eed8f7bbd5b969c79e989ac4eaa545562
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-21 20:35:03 +0500

    Simplified Md

--- cs/Markdown/Markdown/Md.cs
+++ cs/Markdown/Markdown/Md.cs
@@ -9,27 +9,88 @@ namespace Markdown
     {
         private HashSet<string> tags = new HashSet<string> {"__", "_", "# "};
 
-        private Dictionary<MarkupType, string> markupToHtmlTag = new Dictionary<MarkupType, string>
+        private readonly Dictionary<MarkupType, string> markupToHtmlTag = new Dictionary<MarkupType, string>
         {
             {MarkupType.Bold, "strong"},
             {MarkupType.Italic, "em"},
             {MarkupType.Header, "h1"}
         };
 
-        private Dictionary<MarkupType, string> markupToMdTag = new Dictionary<MarkupType, string>
+        private readonly Dictionary<MarkupType, string> markupToMdTag = new Dictionary<MarkupType, string>
         {
             {MarkupType.Bold, "__"},
             {MarkupType.Italic, "_"},
             {MarkupType.Header, "# "}
         };
 
-        private HashSet<string> singleTags = new HashSet<string> {"# "};
+        private readonly HashSet<string> singleTags = new HashSet<string> {"# "};
 
-        private MarkupProcessor markupProcessor;
+        private readonly MarkupProcessor markupProcessor;
 
         public Md()
         {
             markupProcessor = new MarkupProcessor(markupToHtmlTag, markupToMdTag, singleTags);
+        }
+
+        private StringBuilder CloseSingleTags(Stack<MarkupType> markup)
+        {
+            var sb = new StringBuilder();
+            while (markup.Any())
+            {
+                var lastMarkup = markup.Peek();
+                if (!markupProcessor.IsSingleTag(lastMarkup))
+                    break;
+                sb.Append(markupProcessor.GetClosingTag(lastMarkup));
+                markup.Pop();
+            }
+
+            return sb;
+        }
+
+        private StringBuilder CloseAllTags(Stack<MarkupType> markup)
+        {
+            var sb = new StringBuilder();
+            while (markup.Any())
+            {
+                sb.Append(markupProcessor.GetClosingTag(markup.Pop()));
+            }
+
+            return sb;
+        }
+
+        private StringBuilder AddMarkup(string text, Stack<MarkupType> markup, Token token)
+        {
+            var sb = new StringBuilder();
+            if (markup.Any() &&
+                markup.Peek() == markupProcessor.GetMarkupType(text, token))
+            {
+                sb.Append(markupProcessor.GetClosingTag(text, token));
+                markup.Pop();
+            }
+            else
+            {
+                sb.Append(markupProcessor.GetOpeningTag(text, token));
+                markup.Push(markupProcessor.GetMarkupType(text, token));
+            }
+
+            return sb;
+        }
+
+        private StringBuilder AddRawText(string text, Stack<MarkupType> markup, Token token)
+        {
+            var sb = new StringBuilder();
+            var tokenText = text.Substring(token);
+            if (tokenText.EndsWith(Environment.NewLine))
+            {
+                sb.Append(tokenText.Substring(0,
+                    tokenText.Length - Environment.NewLine.Length));
+                sb.Append(CloseSingleTags(markup));
+                sb.Append(Environment.NewLine);
+            }
+            else
+                sb.Append(tokenText);
+
+            return sb;
         }
 
         public string MarkdownToHtml(string text)
@@ -40,50 +101,12 @@ namespace Markdown
             var tokens = tokenizer.GetTokens();
             foreach (var token in tokens)
             {
-                if (token.IsMarkup)
-                {
-                    if (currentMarkup.Any() &&
-                        currentMarkup.Peek() == markupProcessor.GetMarkupType(text, token))
-                    {
-                        htmlText.Append(markupProcessor.GetClosingTag(text, token));
-                        currentMarkup.Pop();
-                    }
-                    else
-                    {
-                        htmlText.Append(markupProcessor.GetOpeningTag(text, token));
-                        currentMarkup.Push(markupProcessor.GetMarkupType(text, token));
-                    }
-                }
-                else
-                {
-                    var tokenText = text.Substring(token);
-                    if (tokenText.EndsWith(Environment.NewLine))
-                    {
-                        htmlText.Append(tokenText.Substring(0,
-                            tokenText.Length - Environment.NewLine.Length));
-                        while (currentMarkup.Any())
-                        {
-                            var lastMarkup = currentMarkup.Peek();
-                            if (!markupProcessor.IsSingleTag(lastMarkup))
-                                break;
-                            htmlText.Append(markupProcessor.GetClosingTag(lastMarkup));
-                            currentMarkup.Pop();
-                        }
-
-                        htmlText.Append(Environment.NewLine);
-                    }
-                    else
-                    {
-                        htmlText.Append(tokenText);
-                    }
-                }
+                htmlText.Append(token.IsMarkup
+                    ? AddMarkup(text, currentMarkup, token)
+                    : AddRawText(text, currentMarkup, token));
             }
 
-            while (currentMarkup.Any())
-            {
-                htmlText.Append(markupProcessor.GetClosingTag(currentMarkup.Pop()));
-            }
-
+            htmlText.Append(CloseAllTags(currentMarkup));
             return htmlText.ToString();
         }
     }
