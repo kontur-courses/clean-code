45fa5ac321b80c5935daf2af53a6ce4bb58b8f2f
Scindapsus345 <zavnikita345@gmail.com>
2020-11-22 23:01:27 +0500

    References tests

--- cs/Md_Tests/Md_Tests.cs
+++ cs/Md_Tests/Md_Tests.cs
@@ -139,7 +139,7 @@ namespace Md_Tests
         }
 
         [TestCase("__Hello _my_ world__", "<strong>Hello <em>my</em> world</strong>")]
-        [TestCase("_Hello __my__ world_", "_Hello __my__ world_")]
+        [TestCase("_Hello __my__ world_", "<em>Hello __my__ world</em>")]
         [TestCase("___Hello my_ world__", "<strong><em>Hello my</em> world</strong>")]
         [TestCase("__Hello _my world___", "<strong>Hello <em>my world</em></strong>")]
         [TestCase("___Hello my world___", "<strong><em>Hello my world</em></strong>")]
@@ -151,14 +151,14 @@ namespace Md_Tests
             Assert.AreEqual(expected, result);
         }
 
-        [TestCase("# Hello", "<h1>Hello</h1>\n")]
-        [TestCase("# _Hello_", "<h1><em>Hello</em></h1>\n")]
-        [TestCase("# __Hello__", "<h1><strong>Hello</strong></h1>\n")]
-        [TestCase("# __Hello _my_ world__", "<h1><strong>Hello <em>my</em> world</strong></h1>\n")]
-        [TestCase("# m__Hello__m", "<h1>m<strong>Hello</strong>m</h1>\n")]
+        [TestCase("# Hello", "<h1>Hello</h1>")]
+        [TestCase("# _Hello_", "<h1><em>Hello</em></h1>")]
+        [TestCase("# __Hello__", "<h1><strong>Hello</strong></h1>")]
+        [TestCase("# __Hello _my_ world__", "<h1><strong>Hello <em>my</em> world</strong></h1>")]
+        [TestCase("# m__Hello__m", "<h1>m<strong>Hello</strong>m</h1>")]
         [TestCase("# Hello\n world", "<h1>Hello</h1>\n world")]
-        [TestCase("_# Hello_", "_# Hello_")]
-        [TestCase("__# Hello__", "__# Hello__")]
+        [TestCase("_# Hello_", "<em># Hello</em>")]
+        [TestCase("__# Hello__", "<strong># Hello</strong>")]
         public void Render_SupportsHeaderTag(string input, string expected)
         {
             var result = md.Render(input);
@@ -181,6 +181,17 @@ namespace Md_Tests
         [TestCase("_a__a_a__a_", "<em>a__a</em>a__a_")]
         [TestCase("__a_a__a_a__", "__a_a__a_a__")]
         public void Render_ReturnsCorrectResult_OnComplexCases(string input, string expected)
+        {
+            var result = md.Render(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("$ref!word$", "<a href=ref>word</a>")]
+        [TestCase("$!word$", "$!word$")]
+        [TestCase("$ref!_word_$", "<a href=ref><em>word</em></a>")]
+        [TestCase("# $ref!___word___$", "<h1><a href=ref><strong><em>word</em></strong></a></h1>")]
+        public void Render_SupportsReferences(string input, string expected)
         {
             var result = md.Render(input);
 
