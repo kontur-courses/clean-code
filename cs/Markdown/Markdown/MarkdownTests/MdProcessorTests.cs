22d27e77626d992b659589958d266df84e28534b
FoolLikeYou <Lambda2712@gmail.com>
2020-11-25 12:17:01 +0500

    refactored tests

--- cs/Markdown/Markdown/MarkdownTests/MdProcessorTests.cs
+++ cs/Markdown/Markdown/MarkdownTests/MdProcessorTests.cs
@@ -24,7 +24,6 @@ namespace MarkdownTests
             var mark = new StrongMark();
             var expectedToken = new TokenMd("\\<strong>12345\\</strong>", mark);
             var tags = new Dictionary<Mark, IMarkProcessor>();
-            //tags.Add(mark, MP);
             var tokens = new List<TokenMd>();
             tokens.Add(new TokenMd("__12345__", mark));
             var processor = new MarkdownProcessor(tags);
@@ -34,24 +33,6 @@ namespace MarkdownTests
             formattedTokens.First().Token.Should().BeEquivalentTo(expectedToken.Token);
         }
         
-        /*
-        [Test]
-        public void FormatToken_ReturnLinkFormattedToken_WhenTokenContainsOneLinkTag()
-        {
-            //\<h1><a href="bb"> \<strong>aa \<em>b\</em>b\<em> \</em>aa\</strong></a>\</h1>
-            var mark = new LinkMark();
-            var expectedToken = new TokenMd("<a href=\"bb\">aa</strong></a>", mark);
-            var tags = new Dictionary<Mark, IMarkProcessor>();
-            //tags.Add(mark, MP);
-            var tokens = new List<TokenMd>();
-            tokens.Add(new TokenMd("[aa](bb)", mark));
-            var processor = new MarkdownProcessor("[aa](bb)",tags);
-            
-            var formattedTokens = processor.FormatTokens(tokens);
-
-            formattedTokens.First().Token.Should().BeEquivalentTo(expectedToken.Token);
-        }
-        */
         
         [Test]
         public void FormatToken_ReturnItalicFormattedToken_WhenTokenContainsOneItalicTag()
@@ -60,7 +41,6 @@ namespace MarkdownTests
             var mark = new ItalicMark();
             var expectedToken = new TokenMd("\\<em>12345\\</em>", mark);
             var tags = new Dictionary<Mark, IMarkProcessor>();
-            //tags.Add(mark, MP);
             var tokens = new List<TokenMd>();
             tokens.Add(new TokenMd("_12345_", mark));
             var processor = new MarkdownProcessor(tags);
@@ -104,45 +84,5 @@ namespace MarkdownTests
 
             formattedTokens.First().Token.Should().BeEquivalentTo(expectedToken.Token);
         }
-        
-        /*
-        [Test]
-        public void FormatToken_ReturnTokensWithoutMark_WhenItalicAndStrongIntersect()
-        {
-            var strongProcessor = new StrongProcessor();
-            var strongMark = new StrongMark();
-            var italicProcessor = new ItalicProcessor();
-            var italicMark = new ItalicMark();
-            var headProcessor = new HeadProcessor();
-            var headMark = new HeadMark();
-            
-            var strongExpectedToken = new TokenMd("__123 _45__", null);
-            var italicExpectedToken = new TokenMd("67_", null);
-            var expectedTokens = new List<TokenMd>{strongExpectedToken,italicExpectedToken};
-            
-            var Tags = new Dictionary<Mark, IMarkProcessor>();
-            //Tags.Add(strongMark, strongProcessor);
-            //Tags.Add(italicMark, italicProcessor);
-            //Tags.Add(headMark, headProcessor);
-            
-            var tokens = new List<TokenMd>();
-            tokens.Add(new TokenMd("__123 _45__", strongMark));
-            tokens.Add(new TokenMd(" ", null));
-            tokens.Add(new TokenMd("67_", null));
-
-            var text = "__123 _45__ 67_";
-            var processor = new MarkdownProcessor(text,Tags);
-
-            var formattedTokens = processor.FormatTokens(tokens);
-
-            var res = new List<TokenMd>();
-            foreach (var e in formattedTokens)
-            {
-                res.Add(new TokenMd(e.Token, null));
-                Console.WriteLine("res=  "+e.Token);
-            }
-            res.Should().BeEquivalentTo(expectedTokens);
-        }
-        */
     }
 }
--- cs/Markdown/Markdown/MarkdownTests/MdTests.cs
+++ cs/Markdown/Markdown/MarkdownTests/MdTests.cs
@@ -32,27 +32,38 @@ namespace MarkdownTests
         [TestCase("italic_1_2_3", "italic_1_2_3", TestName = "underscore with numbers is not formatted")]
         [TestCase("italic_1_2_3", "italic_1_2_3", TestName = "underscore with numbers is not formatted")]
         
-        [TestCase("_it_alic", "\\<em>it\\</em>alic", TestName = "italic tag at the begin word works")]//------
-        [TestCase("it_al_ic", "it\\<em>al\\</em>ic", TestName = "italic tag in the middle word works")]//------
-        [TestCase("ital_ic_", "ital\\<em>ic\\</em>", TestName = "italic tag in the end word works")]//------
+        [TestCase("_it_alic", "\\<em>it\\</em>alic", TestName = "italic tag at the begin word works")]
+        [TestCase("it_al_ic", "it\\<em>al\\</em>ic", TestName = "italic tag in the middle word works")]
+        [TestCase("ital_ic_", "ital\\<em>ic\\</em>", TestName = "italic tag in the end word works")]
         
         [TestCase("diff_erent wor_ds", "diff_erent wor_ds", TestName = "italic tag in different words word does not works")]
         
-        [TestCase("__not paired strong italic_", "__not paired strong italic_", TestName = "not paired(strong-italic) tags does not works")]//------
-        [TestCase("_not paired italic strong__", "_not paired italic strong__", TestName = "not paired(italic-strong) tags does not works")]//------
+        [TestCase("__not paired strong italic_", "__not paired strong italic_", TestName = "not paired(strong-italic) tags does not works")]
+        [TestCase("_not paired italic strong__", "_not paired italic strong__", TestName = "not paired(italic-strong) tags does not works")]
         
         [TestCase("it_ wrong_", "it_ wrong_", TestName = "formatting without whitespace before beginning italic tag does not work")]
         [TestCase("_underline _notWorks", "_underline _notWorks", TestName = "formatting with whitespace before end italic tag does not work")]
         [TestCase("it__ wrong__", "it__ wrong__", TestName = "formatting without whitespace before beginning strong tag does not work")]
         [TestCase("__underline __notWorks", "__underline __notWorks", TestName = "formatting with whitespace before end strong tag does not work")]
         
-        [TestCase("__strong _italic__ word_", "__strong _italic__ word_", TestName = "intersect strong-italic does not works")]//------
-        [TestCase("_italic __strong_ word__", "_italic __strong_ word__", TestName = "intersect italic-strong does not works")]//------
+        [TestCase("__strong _italic__ word_", "__strong _italic__ word_", TestName = "intersect strong-italic does not works")]
+        [TestCase("_italic __strong_ word__", "_italic __strong_ word__", TestName = "intersect italic-strong does not works")]
         
         [TestCase("____", "____", TestName = "empty strong tag is not formatted")]
         [TestCase("__", "__", TestName = "empty italic tag is not formatted")]
         [TestCase("", "", TestName = "empty text return empty")]
         public void Render_ReturnCorrectHtml_TagsInteraction(string text, string expectedHtml)
+        {
+            var html = md.Render(text);
+
+            html.Should().BeEquivalentTo(expectedHtml);
+        }
+        
+        [TestCase("\\[text](link)", "text](link)", TestName = "Text with escaped link tag")]
+        [TestCase("\\_italic_", "italic_", TestName = "Text with escaped italic tag")]
+        [TestCase("\\__strong__", "strong__", TestName = "Text with escaped strong tag")]
+        [TestCase("\\#head", "head", TestName = "Text with escaped head tag")]
+        public void Render_ReturnCorrectHtml_WhenTextContainsEscapingSymbol(string text, string expectedHtml)
         {
             var html = md.Render(text);
 
