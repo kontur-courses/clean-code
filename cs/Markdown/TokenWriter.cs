d9a26fa01eedfd7c2ee2dde6e8982794f8ed1c72
Scindapsus345 <zavnikita345@gmail.com>
2020-12-20 03:47:18 +0500

    Fixes for Writer

--- cs/Markdown/TokenWriter.cs
+++ cs/Markdown/TokenWriter.cs
@@ -16,18 +16,39 @@ namespace Markdown
             newLine = new StringBuilder();
         }
 
-        public void Write(IToken token)
+        public string Write(IToken token)
         {
-            token.OpenTag(newLine, inputLine);
+            WriteToken(token);
+            return newLine.ToString();
+        }
 
+        public void WriteToken(IToken token)
+        {
+            token.OpeningTag.ReplaceMdTagToHtmlTag(newLine, token);
+            FillStringBeforeSubTokens(token);
 
             for (var i = 0; i < token.SubTokens.Count; i++)
             {
-                Write(token.SubTokens[i]);
+                WriteToken(token.SubTokens[i]);
                 FillStringBetweenSubTokens(token, i);
             }
 
-            token.CloseTag(newLine, inputLine);
+            FillStringAfterSubTokens(token);
+            token.ClosingTag.ReplaceMdTagToHtmlTag(newLine, token);
+        }
+
+        private void FillStringBeforeSubTokens(IToken token)
+        {
+            if (token.SubTokens.Any())
+                newLine.Append(inputLine.Substring(token.StartPosition + token.OpeningTag.MdTag.Length,
+                    token.SubTokens[0].StartPosition - token.OpeningTag.MdTag.Length - token.StartPosition));
+            // Здесь сложная обработка, потому что заголовок может заканчиваться как на \n, так и на конце всей строки
+            else if (token.Type == TokenType.Header && token.EndPosition == inputLine.Length)
+                newLine.Append(inputLine.Substring(token.StartPosition + token.OpeningTag.MdTag.Length,
+                    token.Length - token.OpeningTag.MdTag.Length - token.ClosingTag.MdTag.Length + 1));
+            else
+                newLine.Append(inputLine.Substring(token.StartPosition + token.OpeningTag.MdTag.Length,
+                    token.Length - token.OpeningTag.MdTag.Length - token.ClosingTag.MdTag.Length));
         }
 
         private void FillStringBetweenSubTokens(IToken token, int i)
@@ -37,6 +58,20 @@ namespace Markdown
                     token.SubTokens[i + 1].StartPosition - token.SubTokens[i].EndPosition));
         }
 
-        public string GetString() => newLine.ToString();
+        private void FillStringAfterSubTokens(IToken token)
+        {
+            if (token.SubTokens.Any())
+            {
+                var endOfLastSubToken = token.SubTokens.Last().EndPosition;
+                if (endOfLastSubToken != inputLine.Length)
+                    // То же самое, два типа окончания заголовка, один из которых нужно обрабатывать отдельно
+                    if (token.Type == TokenType.Header && inputLine[token.EndPosition - 1] != '\n')
+                        newLine.Append(inputLine.Substring(endOfLastSubToken,
+                            token.EndPosition - endOfLastSubToken));
+                    else
+                        newLine.Append(inputLine.Substring(endOfLastSubToken,
+                            token.EndPosition - token.ClosingTag.MdTag.Length - endOfLastSubToken));
+            }
+        }
     }
 }
