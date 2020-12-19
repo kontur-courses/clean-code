514dd15b67426da7404e6b3b1862bc8d2fa62e65
Scindapsus345 <zavnikita345@gmail.com>
2020-12-20 03:44:49 +0500

    Rewrote Token

--- cs/Markdown/Tokens/IToken.cs
+++ cs/Markdown/Tokens/IToken.cs
@@ -12,61 +12,12 @@ namespace Markdown
         public TokenType Type { get; set; }
         public List<IToken> SubTokens { get; set; }
         public IToken Parent { get; set; }
-        public int NestingLevel { get; set; }
-        public string OpeningTag { get; set; }
-        public string ClosingTag { get; set; }
-        public string HtmlTag { get; set; }
-        public bool IsClosed { get; set; }
+        public Tag OpeningTag { get; set; }
+        public Tag ClosingTag { get; set; }
         public bool ContainsOnlyDigits { get; set; }
-        public string HtmlClosingTag();
 
-        public void OpenTag(StringBuilder newLine, string inputLine);
+        public void Open(Stack<IToken> openedTokens);
 
-        public void CloseTag(StringBuilder newLine, string inputLine);
-        public void CloseToken(Stack<IToken> openedTokens, int position);
-
-        public static void Close(Stack<IToken> openedTokens, int startOfClosingTagPosition)
-        {
-            var closedToken = openedTokens.Pop();
-            closedToken.IsClosed = true;
-            closedToken.Length = startOfClosingTagPosition - closedToken.StartPosition + closedToken.ClosingTag.Length;
-            if (closedToken.Length != closedToken.ClosingTag.Length + closedToken.OpeningTag.Length)
-                openedTokens.Peek().SubTokens.Add(closedToken);
-        }
-
-        public static void ClosePreviousOpenedToken(Stack<IToken> openedTokens, int position)
-        {
-            var italicToken = openedTokens.Pop();
-            Close(openedTokens, position);
-            openedTokens.Push(italicToken);
-        }
-
-        public static void Open(IToken token, Stack<IToken> openedTokens)
-        {
-            token.Parent = openedTokens.Peek();
-            openedTokens.Push(token);
-        }
-        public static void Remove(IToken token)
-        {
-            var parent = token.Parent;
-            foreach (var closedSubToken in token.SubTokens.Where(t => t.IsClosed))
-            {
-                parent.SubTokens.Add(closedSubToken);
-                closedSubToken.Parent = parent;
-            }
-            parent.SubTokens.Remove(token);
-        }
-
-        public static bool CheckIntersectionOfTokens(Stack<IToken> openedTokens)
-        {
-            if (openedTokens.Peek().Parent.IsClosed)
-            {
-                Remove(openedTokens.Peek().Parent);
-                openedTokens.Pop();
-                return true;
-            }
-
-            return false;
-        }
+        public void Close(int position);
     }
 }
--- cs/Markdown/Tokens/Token.cs
+++ cs/Markdown/Tokens/Token.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Markdown
 {
-    public abstract class Token : IToken
+    public class Token : IToken
     {
         public int StartPosition { get; set; }
         public int EndPosition => StartPosition + Length;
@@ -15,44 +15,31 @@ namespace Markdown
         public TokenType Type { get; set; }
         public List<IToken> SubTokens { get; set; }
         public IToken Parent { get; set; }
-        public int NestingLevel { get; set; }
-        public string OpeningTag { get; set; }
-        public string ClosingTag { get; set; }
-        public string HtmlTag { get; set; }
-        public bool IsClosed { get; set; }
+        public Tag OpeningTag { get; set; }
+        public Tag ClosingTag { get; set; }
         public bool ContainsOnlyDigits { get; set; }
-        public virtual string HtmlClosingTag() => HtmlTag == "" ? "" : HtmlTag.Insert(1, "/");
 
-        public Token(int startPos, int length, IToken parent)
+        public Token(int startPos, int length, IToken parent, TokenType type)
         {
+            Type = type;
+            OpeningTag = Tag.GetTagByTokenType(type, true);
+            ClosingTag = Tag.GetTagByTokenType(type, false);
             StartPosition = startPos;
             Length = length;
             SubTokens = new List<IToken>();
             Parent = parent;
         }
 
-        public virtual void OpenTag(StringBuilder newLine, string inputLine)
+        public void Open(Stack<IToken> openedTokens)
         {
-            if (Type != TokenType.Simple)
-                newLine.Append(HtmlTag);
-            newLine.Append(SubTokens.Any()
-                ? inputLine.Substring(StartPosition + OpeningTag.Length,
-                    SubTokens[0].StartPosition - OpeningTag.Length - StartPosition)
-                : inputLine.Substring(StartPosition + OpeningTag.Length,
-                    Length - OpeningTag.Length - ClosingTag.Length));
+            Parent = openedTokens.Peek();
+            openedTokens.Peek().SubTokens.Add(this);
+            openedTokens.Push(this);
         }
 
-        public virtual void CloseTag(StringBuilder newLine, string inputLine)
+        public void Close(int position)
         {
-            if (SubTokens.Any())
-                newLine.Append(inputLine.Substring(SubTokens.Last().EndPosition,
-                    EndPosition - ClosingTag.Length - SubTokens.Last().EndPosition));
-            if (Type != TokenType.Simple)
-                newLine.Append(HtmlClosingTag());
-        }
-
-        public virtual void CloseToken(Stack<IToken> openedTokens, int position)
-        {
+            Length = position - StartPosition + 1;
         }
     }
 }
