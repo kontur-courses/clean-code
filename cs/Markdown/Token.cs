b87dee2bb472d4c2a3e6ed1bf1f90e12ad9300fb
Scindapsus345 <zavnikita345@gmail.com>
2020-12-21 22:28:14 +0500

    Delete IToken

--- cs/Markdown/Token.cs
+++ cs/Markdown/Token.cs
@@ -7,30 +7,30 @@ using System.Threading.Tasks;
 
 namespace Markdown
 {
-    public class Token : IToken
+    public class Token
     {
         public int StartPosition { get; set; }
         public int EndPosition => StartPosition + Length;
         public int Length { get; set; }
         public TokenType Type { get; set; }
-        public List<IToken> SubTokens { get; set; }
-        public IToken Parent { get; set; }
+        public List<Token> SubTokens { get; set; }
+        public Token Parent { get; set; }
         public Tag OpeningTag { get; set; }
         public Tag ClosingTag { get; set; }
         public bool ContainsOnlyDigits { get; set; }
 
-        public Token(int startPos, int length, IToken parent, TokenType type)
+        public Token(int startPos, int length, Token parent, TokenType type)
         {
             Type = type;
             OpeningTag = Tag.GetTagByTokenType(type, true);
             ClosingTag = Tag.GetTagByTokenType(type, false);
             StartPosition = startPos;
             Length = length;
-            SubTokens = new List<IToken>();
+            SubTokens = new List<Token>();
             Parent = parent;
         }
 
-        public void Open(Stack<IToken> openedTokens)
+        public void Open(Stack<Token> openedTokens)
         {
             Parent = openedTokens.Peek();
             openedTokens.Peek().SubTokens.Add(this);
@@ -39,7 +39,7 @@ namespace Markdown
 
         public void Close(int position)
         {
-            Length = position - StartPosition + 1;
+            Length = position + ClosingTag.MdTag.Length - StartPosition;
         }
     }
 }
