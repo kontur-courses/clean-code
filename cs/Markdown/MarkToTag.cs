0dbe0c2ac29d15482b67dfc32239fd33a91a06e1
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-18 02:04:11 +0500

    Add Markdown skeleton

---
+++ cs/Markdown/MarkToTag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    public static class MarkToTag
+    {
+        // Вынесено в отдельный класс, чтобы проще было изменять, если
+        // вдруг понадобится расширить Mark или Tag. Вероятно, будет использоваться в 
+        // Md.GetRenderedInput().
+        public static string GetTag(Mark mark)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
