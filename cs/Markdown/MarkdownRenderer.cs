2c0a37cf8b33a85bf62b91204c50663bf5666fab
Tripletri <Tripletri@yandex.ru>
2021-11-17 19:05:26 +0500

    Реализован MarkdownRenderer.cs

--- cs/Markdown/MarkdownRenderer.cs
+++ cs/Markdown/MarkdownRenderer.cs
@@ -1,18 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Markdown.Models;
 
 namespace Markdown
 {
     public class MarkdownRenderer
     {
-        private string text;
+        private readonly string text;
+        private Dictionary<int, TokenMatch> matchStartAtPosition = new();
+        private Dictionary<int, TokenMatch> matchEndAtPosition = new();
 
         public MarkdownRenderer(string text)
         {
             this.text = text;
         }
 
-        public string RenderMatches(IEnumerable<TokenMatch> matches) => throw new NotImplementedException();
+        public string RenderMatches(IEnumerable<TokenMatch> matches)
+        {
+            MarkPositions(matches);
+            return ConvertText();
+        }
+
+        private string ConvertText()
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (TryAppendStartTag(builder, ref i))
+                    continue;
+
+                if (TryAppendEndTag(builder, ref i))
+                    continue;
+
+                builder.Append(text[i]);
+            }
+
+            return builder.ToString();
+        }
+
+        private bool TryAppendEndTag(StringBuilder builder, ref int i)
+        {
+            if (!matchEndAtPosition.TryGetValue(i, out var matchAtEnd))
+                return false;
+
+            builder.Append(matchAtEnd.Token.TagConverter.CloseTag);
+            i += matchAtEnd.Token.Pattern.TagLength - 1;
+            return true;
+        }
+
+        private bool TryAppendStartTag(StringBuilder builder, ref int i)
+        {
+            if (!matchStartAtPosition.TryGetValue(i, out var matchAtStart))
+                return false;
+
+            builder.Append(matchAtStart.Token.TagConverter.OpenTag);
+            i += matchAtStart.Token.Pattern.TagLength - 1;
+            return true;
+        }
+
+        private void MarkPositions(IEnumerable<TokenMatch> matches)
+        {
+            var matchesList = matches.ToList();
+            matchStartAtPosition = matchesList.ToDictionary(match => match.Start, match => match);
+            matchEndAtPosition = matchesList.ToDictionary(
+                match => match.Start + match.Length - match.Token.Pattern.TagLength,
+                match => match);
+        }
     }
 }
---
+++ cs/MarkdownTests/MarkdownRendererTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Markdown;
+using Markdown.Models;
+using Markdown.Tokens;
+using NUnit.Framework;
+
+namespace MarkdownTests
+{
+    public class MarkdownRendererTests
+    {
+        [Test]
+        public void RenderMatches_SingleMatch()
+        {
+            var renderer = new MarkdownRenderer("_italic_");
+            var matches = new List<TokenMatch> {new() {Start = 0, Length = 8, Token = MarkdownTokensFactory.Italic()}};
+
+            var actual = renderer.RenderMatches(matches);
+
+            actual.Should().Be("<em>italic</em>");
+        }
+
+        [Test]
+        public void RenderMatches_TwoMatches()
+        {
+            var renderer = new MarkdownRenderer("_italic_ __bold__");
+            var matches = new List<TokenMatch>
+            {
+                new() {Start = 0, Length = 8, Token = MarkdownTokensFactory.Italic()},
+                new() {Start = 9, Length = 8, Token = MarkdownTokensFactory.Bold()}
+            };
+
+            var actual = renderer.RenderMatches(matches);
+
+            actual.Should().Be("<em>italic</em> <strong>bold</strong>");
+        }
+
+        [Test]
+        public void RenderMatches_NestedMatches()
+        {
+            var renderer = new MarkdownRenderer("__bo _italic_ ld__");
+            var matches = new List<TokenMatch>
+            {
+                new() {Start = 0, Length = 18, Token = MarkdownTokensFactory.Bold()},
+                new() {Start = 5, Length = 8, Token = MarkdownTokensFactory.Italic()}
+            };
+
+            var actual = renderer.RenderMatches(matches);
+
+            actual.Should().Be("<strong>bo <em>italic</em> ld</strong>");
+        }
+    }
+}
