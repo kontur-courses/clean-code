78261b230eec60df59cc5df3a636ab3a89cce59d
Georgy Grantsev <mrxenonmr@gmail.com>
2021-11-18 20:00:30 +0500

    Почистил юзинги

--- cs/Markdown/Tokens/HeaderToken.cs
+++ cs/Markdown/Tokens/HeaderToken.cs
@@ -3,7 +3,6 @@
 namespace Markdown.Tokens
 {
     internal class HeaderToken : NonPairedToken
-
     {
         public HeaderToken(int openIndex) : base(openIndex) { }
         internal HeaderToken(int openIndex, int closeIndex) : base(openIndex, closeIndex) { }
