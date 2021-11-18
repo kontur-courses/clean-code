2c58715ac97b5bb64a272e70afa4dd9a3890b427
makspanshin <makspanshin97@gmail.com>
2021-11-18 21:40:54 +0500

    Add UnitTest project

---
+++ cs/MarkdownUnitTests/HtmlConvectorTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MarkdownUnitTests
+{
+    public class HtmlConvectorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+    }
+}
