f00f046a731766d84fbaec6f5043f77d1c0c9d41
gODealOAple <maksim.math@gmail.com>
2021-11-20 23:21:15 +0500

    Refactor token parser tests

--- cs/Markdown.Tests/TokenParserTests.cs
+++ cs/Markdown.Tests/TokenParserTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
@@ -34,141 +33,69 @@ namespace Markdown.Tests
             parsed.Should().BeEmpty();
         }
 
-        [Test]
-        public void Parse_ShouldNotBeApplied_WhenOnlyTextTokens()
-        {
-            var tokens = new[] { Token.Text("text") };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(new TokenNode(tokens[0]));
-        }
-
-        [Test]
-        public void Parse_ShouldConcatenateTextTokens()
-        {
-            var tokens = new[]
-            {
-                Token.Text("A"),
-                Token.Text("B")
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(new TokenNode(Token.Text("AB")));
-        }
-        
-        [Test]
-        public void Parse_ShouldTransformEscapeSymbolToText()
-        {
-            var tokens = new[]
-            {
-                Token.Escape
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(new TokenNode(Token.Text("\\")));
-        }
-
-        [Test]
-        public void Parse_ShouldConcatenateEscapeSymbolAndText()
-        {
-            var tokens = new[]
-            {
-                Token.Escape,
-                Token.Text("Text")
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(new TokenNode(Token.Text("\\Text")));
-        }
-
-        [Test]
-        public void Parse_ShouldEscapeCursive_WhenSingleCursive()
-        {
-            var tokens = new[]
-            {
-                Token.Escape,
-                Token.Cursive
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(new TokenNode(Token.Text("_")));
-        }
-
-        [Test]
-        public void Parse_ShouldEscapeCursive_WhenBold()
-        {
-            var tokens = new[]
-            {
-                Token.Escape,
-                Token.Bold
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(Token.Text("_").ToNode(), Token.Text("_").ToNode());
-        }
-
-        [Test]
-        public void Parse_ShouldPlaceTextToken_WhenSurroundedByCursiveIntoOneTokenNode()
-        {
-            var tokens = new[]
-            {
-                Token.Cursive,
-                Token.Text("Text"),
-                Token.Cursive
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(new TokenNode(Token.Cursive, Token.Text("Text").ToNode()));
-        }
-        
-        [Test]
-        public void Parse_ShouldMakeToText_WhenStartsWithCursiveButNotEnds()
-        {
-            var tokens = new[]
-            {
-                Token.Cursive,
-                Token.Text("Text"),
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(Token.Text(Token.Cursive.Value).ToNode(), Token.Text("Text").ToNode());
-        }
-        
-        [Test]
-        public void Parse_ShouldMakeToTextCursive_WhenEndsWithCursive()
-        {
-            var tokens = new[]
-            {
-                Token.Text("Text"),
-                Token.Cursive,
-            };
-
-            var parsed = sut.Parse(tokens);
-
-            parsed.Should().Equal(Token.Text("Text").ToNode(), Token.Text(Token.Cursive.Value).ToNode());
-        }
-        
-        [TestCaseSource(typeof(TokenParserTests), nameof(GenerateCorrectnessCases))]
-        public void Parse_ShouldParseCorrectly(IEnumerable<Token> tokens, IEnumerable<TokenNode> nodes)
+        [TestCaseSource(nameof(GetCorrectnessTestCases))]
+        public void Parse_ShouldReturnCorrectNodes(IEnumerable<Token> tokens, IEnumerable<TokenNode> expected)
         {
             var parsed = sut.Parse(tokens);
 
-            parsed.Should().Equal(nodes);
+            parsed.Should().Equal(expected);
         }
 
-        public static IEnumerable<TestCaseData> GenerateCorrectnessCases()
+        public static IEnumerable<TestCaseData> GetCorrectnessTestCases()
         {
             yield return new TestCaseData(
+                Array.Empty<Token>(),
+                Array.Empty<TokenNode>()
+            ).SetName("Parse_ShouldNotBeApplied_WhenNoToken");
+            
+            yield return new TestCaseData(
+                new[] { Token.Text("text") },
+                new[] { Token.Text("text").ToNode() }
+            ).SetName("Only text token");
+
+            yield return new TestCaseData(
+                new[] { Token.Text("A"), Token.Text("B") },
+                new[] { Token.Text("AB").ToNode() }
+            ).SetName("Concatenate text tokens");
+
+            yield return new TestCaseData(
+                new[] { Token.Escape },
+                new[] { Token.Escape.ToText().ToNode() }
+            ).SetName("Should transform escape to text");
+
+            yield return new TestCaseData(
+                new[] { Token.Escape, Token.Text("Text") },
+                new[] { Token.Text("\\Text").ToNode() }
+            ).SetName("Should concatenate escape and text");
+
+            yield return new TestCaseData(
+                new[] { Token.Escape, Token.Cursive },
+                new[] { Token.Cursive.ToText().ToNode() }
+            ).SetName("Should escape cursive character when single character");
+
+            yield return new TestCaseData(
+                new[] { Token.Escape, Token.Bold },
+                new[] { Token.Cursive.ToText().ToNode(), Token.Cursive.ToText().ToNode() }
+            ).SetName("Should escape cursive character when single bold character");
+
+            yield return new TestCaseData(
+                new[] { Token.Cursive, Token.Text("Text"), Token.Cursive },
+                new[] { new TokenNode(Token.Cursive, Token.Text("Text").ToNode()) }
+            ).SetName("Should apply cursive format when surrounds text");
+
+            yield return new TestCaseData(
+                new[] { Token.Cursive, Token.Text("Text") },
+                new[] { Token.Cursive.ToText().ToNode(), Token.Text("Text").ToNode() }
+            ).SetName("Should make cursive as text when just before text");
+
+            yield return new TestCaseData(
+                new[] { Token.Text("Text"), Token.Cursive },
+                new[] { Token.Text("Text").ToNode(), Token.Cursive.ToText().ToNode() }
+            ).SetName("Should make cursive as text when just before text");
+
+            yield return new TestCaseData(
                 new[] { Token.Text("A"), Token.Cursive, Token.Text("B"), Token.Cursive },
-                new[] { Token.Text("A").ToNode(), new TokenNode(Token.Cursive, Token.Text("B").ToNode())}
+                new[] { Token.Text("A").ToNode(), new TokenNode(Token.Cursive, Token.Text("B").ToNode()) }
             ).SetName("Should apply cursive when formatting text after some text");
         }
     }
