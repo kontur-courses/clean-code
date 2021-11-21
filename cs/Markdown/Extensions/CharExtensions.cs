803c009e956860a300980c7797d63ac0b31b97e9
Titov Stepan <titovstepan@mail.ru>
2021-11-21 10:26:28 +0500

    add extensions

---
+++ cs/Markdown/Extensions/CharExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown.Extensions
+{
+    public static class CharExtensions
+    {
+        private static readonly HashSet<char> charset = new HashSet<char>() {'#', '_', '\\'};
+
+        public static bool IsTagSymbol(this char s)
+            => charset.Contains(s);
+    }
+}
