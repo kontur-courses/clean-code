eb2331eee5678d8f2043c39e2938c498a683e229
DaGnYMilleR <danil.lunyow@mail.ru>
2021-11-21 22:21:30 +0500

    rename Md.cs to Markdown.cs

--- cs/Homework/Markdown.Converter/Markdown.cs
+++ cs/Homework/Markdown.Converter/Markdown.cs
@@ -3,12 +3,12 @@ using Markdown.TokenFormatter;
 
 namespace Markdown
 {
-    public class Md
+    public class Markdown
     {
         private readonly IParser parser;
         private readonly ITokensFormatter tokensFormatter;
 
-        public Md(IParser parser, ITokensFormatter tokensFormatter)
+        public Markdown(IParser parser, ITokensFormatter tokensFormatter)
         {
             this.parser = parser;
             this.tokensFormatter = tokensFormatter;
