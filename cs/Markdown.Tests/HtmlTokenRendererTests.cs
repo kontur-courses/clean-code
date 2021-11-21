de68dfd63f64bb02c11f0bd7c3b4119eae9b438a
gODealOAple <maksim.math@gmail.com>
2021-11-22 03:03:22 +0500

    Add html token renderer and tested it

---
+++ cs/Markdown.Tests/HtmlTokenRendererTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Markdown.Extensions;
+using Markdown.TokenRenderer;
+using Markdown.Tokens;
+using NUnit.Framework;
+
+namespace Markdown.Tests
+{
+    public class HtmlTokenRendererTests
+    {
+        private HtmlTokenRenderer sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new HtmlTokenRenderer();
+        }
+        
+        [Test]
+        public void Render_ShouldThrowException_WhenTokensIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => sut.Render(null));
+        }
+        
+        [Test]
+        public void Render_ShouldReturnEmptyString_WhenTokensEmpty()
+        {
+            var html = sut.Render(Array.Empty<TokenNode>());
+
+            html.Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void Render_ShouldReturnText_WhenTextNode()
+        {
+            var node = Token.Text("Text").ToNode();
+
+            var html = sut.Render(new[] { node });
+
+            html.Should().Be("Text");
+        }
+        
+        [Test]
+        public void Render_ShouldReturnFormattedText_WhenSurroundsByCursive()
+        {
+            var node = new TokenNode(Token.Cursive, Token.Text("Text").ToNode());
+
+            var html = sut.Render(new[] { node });
+
+            html.Should().Be("<em>Text</em>");
+        }
+        
+        [Test]
+        public void Render_ShouldReturnFormattedText_WhenSurroundsByBold()
+        {
+            var node = new TokenNode(Token.Bold, Token.Text("Text").ToNode());
+
+            var html = sut.Render(new[] { node });
+
+            html.Should().Be("<strong>Text</strong>");
+        }
+
+        [Test]
+        public void Render_ShouldReturnFormattedText_WhenStartsWithHeader1()
+        {
+            var node = new TokenNode(Token.Header1, Token.Text("Text").ToNode());
+
+            var html = sut.Render(new[] {node});
+
+            html.Should().Be("<h1>Text</h1>");
+        }
+        
+        [Test]
+        public void Render_ShouldReturnFormattedText_WhenHasInnerFormatting()
+        {
+            var node = new TokenNode(Token.Header1, new[]
+            {
+                Token.Text("A").ToNode(),
+                new TokenNode(Token.Cursive, Token.Text("B").ToNode()),
+                Token.Text("C").ToNode(),
+            });
+
+            var html = sut.Render(new[] {node});
+
+            html.Should().Be("<h1>A<em>B</em>C</h1>");
+        }
+    }
+}
--- cs/Markdown/TokenRenderer/HtmlTokenRenderer.cs
+++ cs/Markdown/TokenRenderer/HtmlTokenRenderer.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Markdown.Extensions;
 using Markdown.Tokens;
 
 namespace Markdown.TokenRenderer
 {
     public class HtmlTokenRenderer : ITokenRenderer
     {
-        public string Render(IEnumerable<TokenNode> tokens) => throw new NotImplementedException();
+        public string Render(IEnumerable<TokenNode> tokens)
+        {
+            if (tokens == null) throw new ArgumentNullException(nameof(tokens));
+            return StringUtils.Join(tokens.Select(ToString));
+        }
+
+        private static string ToString(TokenNode node)
+        {
+            if (node.Children.Length == 0)
+                return node.Token.Value;
+            var tag = MapToTag(node.Token.Type);
+            return new StringBuilder()
+                .Append(WrapOpeningTag(tag))
+                .AppendJoin("", node.Children.Select(ToString))
+                .Append(WrapClosingTag(tag))
+                .ToString();
+        }
+
+        private static string MapToTag(TokenType type)
+        {
+            return type switch
+            {
+                TokenType.Header1 => "h1",
+                TokenType.Cursive => "em",
+                TokenType.Bold => "strong",
+                _ => throw new Exception($"Unsupported token: {type}")
+            };
+        }
+
+        private static string WrapOpeningTag(string tag) => $"<{tag}>";
+        
+        private static string WrapClosingTag(string tag) => $"</{tag}>";
     }
+
 }
