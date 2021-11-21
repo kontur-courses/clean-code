6f3dfab19f10880c1f31a46e64e877679260e3cf
makspanshin <makspanshin97@gmail.com>
2021-11-21 20:07:25 +0500

    Add Md Unit Test

---
+++ cs/MarkdownUnitTests/MdTest.cs
@@ -0,0 +1,20 @@
+﻿using Markdown;
+using NUnit.Framework;
+
+namespace MarkdownUnitTests
+{
+    public class MdTest
+    {
+        [TestCase("#  Заголовок __с _разными_ символами__\n#  Заголовок __с _разными_ символами__\n", ExpectedResult = "<h1> Заголовок <strong>с <em>разными</em> символами</strong></h1>\n<h1> Заголовок <strong>с <em>разными</em> символами</strong></h1>\n")]
+        public string Render_ShouldBeCorrectly_When(string input)
+        {
+            var mdRender = new Md(new HtmlConvector());
+
+            return mdRender.Render(input);
+        }
+    }
+}
+
+
+
+
