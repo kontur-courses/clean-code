38a0bb1790fbe4639601842e0f0341a910ee7d50
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-21 14:42:19 +0500

    Clear MarkdownParser and its tests

--- cs/Markdown/MarkdownTests/MarkdownParserShould.cs
+++ cs/Markdown/MarkdownTests/MarkdownParserShould.cs
@@ -6,47 +6,5 @@ namespace Markdown
     [TestFixture]
     public class MarkdownParserShould
     {
-        private MarkdownTree _emptyRoot;
-
-        [SetUp]
-        public void CreateNewEmptyRoot()
-        {
-            _emptyRoot = new MarkdownTree(new Tag(TagKind.Root, TagSide.None), "");
-        }
-
-        [Test]
-        public void ReturnEmptyRoot_WhenInputStringIsEmpty()
-        {
-            var inputString = "";
-
-            var parserResult = MarkdownParser.Parse(inputString);
-
-            parserResult.Should().BeEquivalentTo(_emptyRoot);
-        }
-
-        [Test]
-        public void ReturnOneChildedTree_WhenInputIsPlainText()
-        {
-            var inputString = "Step, step up, step, step up";
-            var child = new MarkdownTree(new Tag(TagKind.PlainText, TagSide.None),
-                "Step, step up, step, step up");
-            _emptyRoot.AddChild(child);
-
-            var parserResult = MarkdownParser.Parse(inputString);
-
-            parserResult.Should().BeEquivalentTo(_emptyRoot);
-        }
-
-        //[Test]
-        //public void ParseSimpleTextWithHeadersCorrect()
-        //{
-        //    var inputString = "Hi all!\n#What's up?\n";
-        //    _emptyRoot.AddChild(new MarkdownTree(new Tag(TagKind.PlainText, TagSide.None), "Hi all!\n"));
-        //    _emptyRoot.AddChild(new MarkdownTree(new Tag(TagKind.Header, TagSide.Opening), "What's up?\n"));
-
-        //    var parserResult = MarkdownParser.Parse(inputString);
-
-        //    parserResult.Should().BeEquivalentTo(_emptyRoot);
-        //}
     }
 }
