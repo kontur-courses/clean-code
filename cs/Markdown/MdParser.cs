e5a30df1c901b4232a201d9cb2ad403f5b211499
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-21 16:38:44 +0500

    Add main loop into MdParser.Parse method; start to work with checking rules

--- cs/Markdown/MdParser.cs
+++ cs/Markdown/MdParser.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Markdown.Tag_Classes;
 
 namespace Markdown
@@ -17,9 +20,62 @@ namespace Markdown
             _parsedTags = new List<TagEvent>();
         }
 
-        public IReadOnlyList<TagEvent> Parse(string rawInput)
+        public IReadOnlyList<TagEvent> Parse(string input)
         {
+            var plainText = new StringBuilder();
+            var tag = new StringBuilder();
+            for (var symbolPos = 0; symbolPos < input.Length; symbolPos++)
+            {
+                if (_tagSymbols.Contains(input[symbolPos]))
+                {
+                    if (plainText.Length > 0)
+                    {
+                        _parsedTags.Add(new TagEvent(TagSide.None,TagKind.PlainText, plainText.ToString()));
+                        plainText.Clear();
+                    }
+                    tag.Append(input[symbolPos]);
+                    continue;
+                }
+
+                if (tag.Length > 0)
+                {
+                    _parsedTags.Add(GetTagEventCheckingRules(input, symbolPos, tag.ToString()));
+                    tag.Clear();
+                }
+                plainText.Append(input[symbolPos]);
+            }
             return _parsedTags;
+        }
+
+        private TagEvent GetTagEventCheckingRules(string input, int symbolPos, string tag)
+        {
+            if (tag == "#")
+                return GetHashtagTagEvent(tag);
+            if (tag == "_")
+                return GetSingleUnderlineTagEvent(input, symbolPos, tag);
+        }
+
+        private TagEvent GetSingleUnderlineTagEvent(string input, int symbolPos, string tag)
+        {
+            if (TagIsNotOpeningUnderline(input, symbolPos))
+                return new TagEvent(TagSide.None, TagKind.PlainText, tag);
+        }
+
+        private TagEvent GetHashtagTagEvent(string tag)
+        {
+            if (TagIsHeader())
+                return new TagEvent(TagSide.Opening, TagKind.Header, tag);
+            return new TagEvent(TagSide.None, TagKind.PlainText, tag);
+        }
+
+        private bool TagIsNotOpeningUnderline(string input, int symbolPos)
+        {
+            return symbolPos >= input.Length - 1 || input[symbolPos + 1] == ' ';
+        }
+
+        private bool TagIsHeader()
+        {
+            return _parsedTags.Count == 0 || _parsedTags.Last().TagContent.EndsWith("\n");
         }
     }
 }
