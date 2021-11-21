fcca11344902ae063a2853d13dcff040d633ab9a
makspanshin <makspanshin97@gmail.com>
2021-11-21 20:10:31 +0500

    Implementation Md

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -1,13 +1,38 @@
-﻿using Markdown.Interfaces;
+﻿using System;
+using Markdown.Interfaces;
+using System.Linq;
+using System.Text;
 
 namespace Markdown
 {
     public class Md
     {
-        public string Render(string text, IConvectorMarkup convecter)
+        public IMarkupConverter MarkupConverter { get; set; }
+
+        public Md(IMarkupConverter markupConverter)
         {
-            Tokenizer tokenizer = new Tokenizer();
-            return convecter.Convert(tokenizer.Parse(text));
+            this.MarkupConverter = markupConverter;
+        }
+
+        public string Render(string markDownText)
+        {
+            if (MarkupConverter == null)
+                throw new Exception("MarkupConverter is null");
+
+            var tokenizer = new Tokenizer();
+
+            var buffer = new StringBuilder();
+
+            foreach (var token in markDownText.Split('\n').Select(tokenizer.Parse))
+            {
+                if (token.Count() > 0)
+                {
+                    buffer.Append(MarkupConverter.Convert(token));
+                    buffer.Append('\n');
+                }
+            }
+
+            return buffer.ToString();
         }
     }
 }
