6a1189d140882d025f850ae75f15765ec5019f44
waverma <63812725+waverma@users.noreply.github.com>
2021-11-21 23:07:56 +0500

    hide TagTranslatorConfigurator to internal

--- Markdown/TagTranslatorConfigurator.cs
+++ Markdown/TagTranslatorConfigurator.cs
@@ -14,21 +14,17 @@ namespace Markdown
             this.tags = tags;
         }
         
-        public static TagTranslatorConfigurator CreateTokenTranslator()
+        internal static TagTranslatorConfigurator CreateTokenTranslator()
         {
             return new TagTranslatorConfigurator();
         }
         
-        public TagTranslatorFromConfigurator SetReference()
+        internal TagTranslatorFromConfigurator SetReference()
         {
-            // var  confType = typeof(TagTranslatorToConfigurator);
-            // var constructorInfoObj = confType.GetConstructor(new[] {typeof(Dictionary<Tag, Tag>)});
-            // return (TagTranslatorFromConfigurator) constructorInfoObj.Invoke(new object[] {tags});
-            
             return new TagTranslatorFromConfigurator(tags);
         }
 
-        public ITagTranslator Configure()
+        internal ITagTranslator Configure()
         {
             var translator = new TagTranslator();
             
@@ -40,52 +36,39 @@ namespace Markdown
             return translator;
         }
         
-        public class TagTranslatorToConfigurator
+        internal class TagTranslatorToConfigurator
         {
             private readonly Dictionary<Tag, Tag> tags = new();
             private readonly Tag fromTag;
         
             private TagTranslatorToConfigurator(){}
 
-            public TagTranslatorToConfigurator(Dictionary<Tag, Tag> tags, Tag fromTag)
+            internal TagTranslatorToConfigurator(Dictionary<Tag, Tag> tags, Tag fromTag)
             {
                 this.fromTag = fromTag;
                 this.tags = tags;
             }
         
-            public TagTranslatorConfigurator To(Tag toTag)
+            internal TagTranslatorConfigurator To(Tag toTag)
             {
                 tags[fromTag] = toTag ?? throw new ArgumentNullException();
                 return new TagTranslatorConfigurator(tags);
             }
         } 
     
-        public class TagTranslatorFromConfigurator
+        internal class TagTranslatorFromConfigurator
         {
             private readonly Dictionary<Tag, Tag> tags = new();
         
             private TagTranslatorFromConfigurator(){}
 
-            public TagTranslatorFromConfigurator(Dictionary<Tag, Tag> tags)
+            internal TagTranslatorFromConfigurator(Dictionary<Tag, Tag> tags)
             {
                 this.tags = tags;
             }
         
-            public TagTranslatorToConfigurator From(Tag tag)
+            internal TagTranslatorToConfigurator From(Tag tag)
             {
-                // if (tag is null) throw new ArgumentNullException();
-                // if (tags.ContainsKey(tag)) 
-                //     throw new ArgumentException($"tag {tag.Start} already translating to tag {tags[tag].Start}");
-                //
-                // var  confType = typeof(TagTranslatorToConfigurator);
-                // var paramsTypes = new[]
-                // {
-                //     typeof(Dictionary<Tag, Tag>),
-                //     typeof(Tag)
-                // };
-                // var constructorInfoObj = confType.GetConstructor(paramsTypes);
-                // return (TagTranslatorToConfigurator) constructorInfoObj.Invoke(new object[] {tags, tag});
-
                 return new TagTranslatorToConfigurator(tags, tag);
             }
         }
