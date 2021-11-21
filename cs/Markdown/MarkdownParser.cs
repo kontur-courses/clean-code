7a4105a9c2d6072c20269690c1cce60ba90e0606
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-21 14:43:01 +0500

    Remove MarkdownTree class

--- cs/Markdown/MarkdownParser.cs
+++ cs/Markdown/MarkdownParser.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Markdown
 {
-    public static class MarkdownParser
+    public class MarkdownParser
     {
     }
 }
--- cs/Markdown/Tag Classes/TagEvent.cs
+++ cs/Markdown/Tag Classes/TagEvent.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Markdown.Tag_Classes
 {
-    class TagEvent
+    public class TagEvent
     {
+        public readonly TagSide tagSide;
+        public readonly TagKind tagKind;
+        public readonly string tagContent;
+
+        public TagEvent(TagSide tagSide, TagKind tagKind, string content)
+        {
+            this.tagSide = tagSide;
+            this.tagKind = tagKind;
+            this.tagContent = content;
+        }
     }
 }
