0ef1258ac7966cf0a63f289d0e881cb64d6f8cce
makspanshin <makspanshin97@gmail.com>
2021-11-21 20:08:26 +0500

    Rename interface

--- cs/Markdown/Interfaces/IConvectorMarkup.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Markdown.Interfaces
-{
-    public interface IConvectorMarkup
-    {
-        string Convert(IEnumerable<TagToken> inputTagTypes);
-    }
-}
---
+++ cs/Markdown/Interfaces/IMarkupConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Markdown.Interfaces
+{
+    public interface IMarkupConverter
+    {
+        string Convert(IEnumerable<IToken> inputTagTokens);
+    }
+}
