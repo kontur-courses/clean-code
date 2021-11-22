3443d378e0fd1c41cd2cbf3a2a526ca8b7264927
Georgy Grantsev <mrxenonmr@gmail.com>
2021-11-22 13:59:41 +0500

    Сделал методы расширения public, чтобы при создании новых токенов извне можно было использовать готовые реализации

--- cs/Markdown/Tokens/TokenExtensions.cs
+++ cs/Markdown/Tokens/TokenExtensions.cs
@@ -2,24 +2,24 @@
 
 namespace Markdown.Tokens
 {
-    internal static class TokenExtensions
+    public static class TokenExtensions
     {
-        internal static void ValidatePlacedCorrectly(this Token token, string text)
+        public static void ValidatePlacedCorrectly(this Token token, string text)
         {
             token.IsCorrect = !(token.IsInsideTextWithDigits(text) || token.IsInsideDifferentWords(text) || token.IsTokenEmpty(text));
         }
 
-        internal static bool IsIntersectWith(this Token thisToken, Token otherToken)
+        public static bool IsIntersectWith(this Token thisToken, Token otherToken)
         {
             return thisToken.OpenIndex < otherToken.OpenIndex && otherToken.OpenIndex < thisToken.CloseIndex;
         }
 
-        private static bool IsTokenEmpty(this Token token, string text)
+        public static bool IsTokenEmpty(this Token token, string text)
         {
             return token.GetTokenContent(text).Length == 0;
         }
 
-        private static bool IsInsideDifferentWords(this Token token, string text)
+        public static bool IsInsideDifferentWords(this Token token, string text)
         {
             var openInsideWord = IsSeparatorInsideWord(token.OpenIndex, token.GetSeparator().Length, text);
             var closeInsideWord = IsSeparatorInsideWord(token.CloseIndex, token.GetSeparator().Length, text);
@@ -28,7 +28,7 @@ namespace Markdown.Tokens
             return (openInsideWord || closeInsideWord) && tokenContent.Any(x => x == ' ');
         }
 
-        private static bool IsInsideTextWithDigits(this Token token, string text)
+        public static bool IsInsideTextWithDigits(this Token token, string text)
         {
             var openInsideWord = IsSeparatorInsideTextWithDigits(token.OpenIndex, token.GetSeparator().Length, text);
             var closeInsideWord = IsSeparatorInsideTextWithDigits(token.CloseIndex, token.GetSeparator().Length, text);
@@ -36,7 +36,7 @@ namespace Markdown.Tokens
             return openInsideWord || closeInsideWord;
         }
 
-        private static bool IsSeparatorInsideWord(int index, int separatorLength, string text)
+        public static bool IsSeparatorInsideWord(int index, int separatorLength, string text)
         {
             var isLeftLetter = index > 0 && char.IsLetter(text[index - 1]);
             var isRightLetter = index + separatorLength < text.Length - 1 && char.IsLetter(text[index + separatorLength]);
@@ -44,7 +44,7 @@ namespace Markdown.Tokens
             return isLeftLetter && isRightLetter;
         }
 
-        private static bool IsSeparatorInsideTextWithDigits(int index, int separatorLength, string text)
+        public static bool IsSeparatorInsideTextWithDigits(int index, int separatorLength, string text)
         {
             var isLeftLetter = index > 0 && char.IsDigit(text[index - 1]);
             var isRightLetter = index + separatorLength < text.Length - 1 && char.IsDigit(text[index + separatorLength]);
@@ -52,7 +52,7 @@ namespace Markdown.Tokens
             return isLeftLetter && isRightLetter;
         }
 
-        private static string GetTokenContent(this Token token, string text)
+        public static string GetTokenContent(this Token token, string text)
         {
             var contentStartIndex = token.OpenIndex + token.GetSeparator().Length;
             var contentLength = token.CloseIndex - contentStartIndex;
