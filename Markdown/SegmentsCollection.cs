6546abd6736057634671d818ae8fea563b8915cb
waverma <63812725+waverma@users.noreply.github.com>
2021-11-22 19:30:32 +0500

    add to SegmentsCollection.Union empty collection check

--- Markdown/SegmentsCollection.cs
+++ Markdown/SegmentsCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Markdown
@@ -6,6 +7,13 @@ namespace Markdown
     public class SegmentsCollection
     {
         private readonly IEnumerable<TokenSegment> segments;
+
+        private static SegmentsCollection Empty => new();
+
+        private SegmentsCollection()
+        {
+            segments = Enumerable.Empty<TokenSegment>();
+        }
 
         internal SegmentsCollection(IEnumerable<TokenSegment> segments)
         {
@@ -17,13 +25,26 @@ namespace Markdown
             return segments.OrderBy(x => x.StartPosition);
         }
 
-        public static SegmentsCollection Union(IEnumerable<SegmentsCollection> collections)
+        private static SegmentsCollection UnionNotEmptyCollections(IList<SegmentsCollection> collections)
         {
             return new SegmentsCollection(
                 collections
                     .Select(x => x.segments)
                     .Where(x => x.Any())
                     .Aggregate((x, y) => x.Union(y)));
+        }
+
+        public static SegmentsCollection Union(IEnumerable<SegmentsCollection> collections)
+        {
+            if (collections is null) throw new ArgumentNullException();
+            
+            var fixedCollections = collections
+                .Where(x => x.segments.Any())
+                .ToList();
+            
+            return !fixedCollections.Any() 
+                ? Empty 
+                : UnionNotEmptyCollections(fixedCollections);
         }
     }
 }
