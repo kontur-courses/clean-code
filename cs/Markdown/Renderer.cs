53f109d78c44362d8bfc7f94c7dce6497e78af6d
FreeTrally <serega2012111@gmail.com>
2021-11-23 01:08:18 +0500

    Renderer

---
+++ cs/Markdown/Renderer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown
+{
+    public static class Renderer
+    {
+        public static string Render(this IEnumerable<Token> tokens)
+        {
+            var result = new StringBuilder();
+            foreach (var token in tokens)
+                result.Append(token.GetValue());
+            return result.ToString();
+        }
+    }
+}
