6b01e5d75ffe2f9a559d01cc95a5b02065adf770
waverma <63812725+waverma@users.noreply.github.com>
2021-11-24 01:46:45 +0500

    convert TokenInfo from record to class

--- Markdown/Tokens/TokenInfo.cs
+++ Markdown/Tokens/TokenInfo.cs
@@ -1,4 +1,27 @@
 ﻿namespace Markdown
 {
-    internal record TokenInfo(int Position, Token Token, bool CloseValid, bool OpenValid, bool WordPartPlaced, bool Valid);
+    internal record TokenInfo
+    {
+        public int Position { get; }
+        public Token Token { get; }
+        public bool CloseValid { get; }
+        public bool OpenValid { get; }
+        public bool WordPartPlaced { get; }
+        public bool Valid { get; set; }
+
+        public TokenInfo(int position, Token token, bool closeValid, bool openValid, bool wordPartPlaced, bool valid)
+        {
+            Position = position;
+            Token = token;
+            CloseValid = closeValid;
+            OpenValid = openValid;
+            WordPartPlaced = wordPartPlaced;
+            Valid = valid;
+        }
+        
+        public void Deconstruct(out int position, out Token token, out bool closeValid, out bool openValid, out bool wordPartPlaced, out bool valid)
+        {
+            (position, token, closeValid, openValid, wordPartPlaced, valid) = (Position, Token, CloseValid, OpenValid, WordPartPlaced, Valid);
+        }
+    }
 }
