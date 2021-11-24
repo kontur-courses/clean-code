7e447ad05df032096b0b44956f26589e41ed4096
Lyalin_Artem <aalyalin22@gmail.com>
2021-11-24 15:56:23 +0500

    Moved OpenTag to new file

---
+++ cs/Markdown/Parser/OpenTag.cs
@@ -0,0 +1,16 @@
+﻿using Markdown.Tags;
+
+namespace Markdown.Parser
+{
+    public class OpenTag
+    {
+        public Tag Tag { get; }
+        public int OpenIndex { get; }
+
+        public OpenTag(Tag tag, int openIndex)
+        {
+            Tag = tag;
+            OpenIndex = openIndex;
+        }
+    }
+}
