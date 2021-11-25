9da647a972f0c6e67655bfef2b3b76e84ca05a5c
Titov Stepan <titovstepan@mail.ru>
2021-11-25 15:19:39 +0500

    почистили юзинги

--- cs/Markdown/MdToTokenTranslator.cs
+++ cs/Markdown/MdToTokenTranslator.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using Markdown.Extensions;
 using Markdown.Tokens;
-using Microsoft.VisualBasic;
 
 namespace Markdown
 {
