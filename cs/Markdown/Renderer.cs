828f63d5c759c07517a02b3bc16b38671b44850b
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-11-26 22:12:01 +0500

    Render realisation and tests

---
+++ cs/Markdown/Renderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Markdown.Interfaces;
+
+namespace Markdown
+{
+    public class Renderer : ITokenRenderer
+    {
+        public string Render(TokenTree[] trees)
+        {
+            var htmlBuilder = new StringBuilder();
+
+            foreach (var component in trees)
+            {
+                htmlBuilder.Append(ToString(component));
+            }
+
+            return htmlBuilder.ToString();
+        }
+
+        private string ToString(TokenTree token)
+        {
+            if (token.Children.Count == 0)
+                return token.Value;
+            var tag = GetTag(token.TokenType);
+            return new StringBuilder()
+                .Append(ConvertValueToHtml(tag,string.Join("", token.Children.Select(ToString))))
+                .ToString();
+        }
+        
+        private string GetTag(TokenType type)
+        {
+            return type switch
+            {
+                TokenType.Header1 => "h1",
+                TokenType.Italics => "em",
+                TokenType.Strong => "strong",
+                _ => throw new ArgumentException($"Unsupported token: {type}")
+            };
+        }
+        
+        private string ConvertValueToHtml(string tag, string value) => $"<{tag}>{value}</{tag}>";
+    }
+}
---
+++ cs/MarkdownTest/RendererTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Markdown;
+using NUnit.Framework;
+
+namespace MarkdownTest
+{
+    public class RendererTests
+    {
+        private Renderer renderer;
+        [SetUp]
+        public void Setup()
+        {
+            renderer = new Renderer();
+        }
+
+        [TestCaseSource(nameof(RendererTestCaseData))]
+        public void Render_ShouldRenderRightHTMLCode_When(TokenTree[] trees, string expected)
+        {
+            var actual = renderer.Render(trees);
+
+            actual.Should().Be(expected);
+        }
+
+        private static IEnumerable<TestCaseData> RendererTestCaseData()
+        {
+            yield return new TestCaseData(new TokenTree[]
+            {
+                new TokenTree(new TokenHeader1(),
+                    new List<TokenTree>
+                    {
+                        new TokenTree(new TokenStrong(),
+                            new List<TokenTree>
+                            {
+                                new TokenTree("text")
+                            }),
+                        new TokenTree(new TokenItalics(),
+                            new List<TokenTree>
+                            {
+                                new TokenTree("text")
+                            })
+                    })
+            }, "<h1><strong>text</strong><em>text</em></h1>").SetName("Nested structure"); 
+            
+            yield return new TestCaseData(new TokenTree[]
+            {
+                new TokenTree("text "),
+                new TokenTree("text")
+            }, "text text").SetName("Not nested structure"); 
+        }
+    }
+}
