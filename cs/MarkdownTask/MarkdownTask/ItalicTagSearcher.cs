0d0adf9a4cb220600c25543fa9ab92a201e78610
SquirrelLeonid <rm200002@yandex.ru>
2021-12-23 04:40:38 +0500

    Minor ItalicTagSearcher refactoring

--- cs/MarkdownTask/MarkdownTask/ItalicTagSearcher.cs
+++ cs/MarkdownTask/MarkdownTask/ItalicTagSearcher.cs
@@ -4,32 +4,22 @@ namespace MarkdownTask
 {
     public class ItalicTagSearcher : ITagSearcher
     {
-        private const char TagSign = '_';
+        private const string ItalicTag = "_";
         private int currentPosition;
 
         public List<Tag> SearchForTags(string mdText)
         {
+            PrepareToSearch();
             var result = new List<Tag>();
-            currentPosition = 0;
 
-            while (currentPosition < mdText.Length)
-            {
-                if (mdText[currentPosition] == TagSign)
-                {
-                    if (IsPossibleOpenTag(mdText))
+            for (; currentPosition < mdText.Length; currentPosition++)
+                if (IsCharItalicTag(mdText[currentPosition]))
+                    if (IsPossibleOpenItalicTag(mdText))
                     {
                         var tag = GetTagFromCurrentPosition(mdText);
                         if (tag is not null)
                             result.Add(tag);
                     }
-                    else
-                    {
-                        currentPosition++;
-                    }
-                }
-
-                currentPosition++;
-            }
 
             return result;
         }
@@ -37,33 +27,48 @@ namespace MarkdownTask
         private Tag GetTagFromCurrentPosition(string mdText)
         {
             var startPos = currentPosition;
-            var length = 1;
-            var tagOpened = true;
+            var length = ItalicTag.Length;
             currentPosition++;
 
-            while (currentPosition < mdText.Length)
+            for (; currentPosition < mdText.Length; currentPosition++)
             {
                 length++;
-                if (mdText[currentPosition] >= 48 && mdText[currentPosition] <= 57)
-                    break;
-                if (mdText[currentPosition] == ' ')
-                    break;
-                if (mdText[currentPosition] == TagSign)
-                {
-                    tagOpened = false;
-                    break;
-                }
-
-                currentPosition++;
+                if (!IsTagStillAbleExist(mdText[currentPosition]))
+                    return null;
+                if (IsCharItalicTag(mdText[currentPosition]))
+                    return new Tag(startPos, length, TagType.Italic);
             }
 
-            return tagOpened ? null : new Tag(startPos, length, TagType.Italic);
+            return null;
         }
 
-        private bool IsPossibleOpenTag(string mdText)
+        private bool IsCharItalicTag(char ch)
         {
-            return currentPosition + 1 < mdText.Length
-                   && mdText[currentPosition + 1] != ' ' && mdText[currentPosition + 1] != '_';
+            return "" + ch == ItalicTag;
+        }
+
+        private bool IsPossibleOpenItalicTag(string mdText)
+        {
+            if (currentPosition + 1 >= mdText.Length)
+                return false;
+
+            var nextCharIsValid = !char.IsNumber(mdText[currentPosition + 1])
+                                  && !char.IsWhiteSpace(mdText[currentPosition + 1])
+                                  && !IsCharItalicTag(mdText[currentPosition + 1]);
+
+            return currentPosition - 1 < 0
+                ? nextCharIsValid
+                : nextCharIsValid && !IsCharItalicTag(mdText[currentPosition - 1]);
+        }
+
+        private bool IsTagStillAbleExist(char currentChar)
+        {
+            return !char.IsWhiteSpace(currentChar) && !char.IsNumber(currentChar);
+        }
+
+        private void PrepareToSearch()
+        {
+            currentPosition = 0;
         }
     }
 }
