b64dbe312da9366f8ef1ec78fbce90963c09238d
Seekard <maks_byx@mail.ru>
2022-11-14 22:22:27 +0300

    Initial commit

---
+++ cs/Markdown.Tests/MdTests.cs
@@ -0,0 +1,16 @@
+namespace Markdown.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- cs/Markdown/Program.cs
+++ cs/Markdown/Program.cs
@@ -6,7 +6,7 @@ namespace Markdown
     {
         public static void Main(string[] args)
         {
-
+            
         }
     }
 }
