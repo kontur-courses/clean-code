750066ca6d96cbad22a8a893883ba2549bea9635
Hrenaki <shikhovwtr@mail.ru>
2022-11-20 14:08:50 +0300

    Added base realization

---
+++ cs/Markdown/MarkdownRules.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markdown
+{
+    internal enum TokenType
+    {
+        Word,
+        Space,
+        Underscore,
+        DoubleUnderscore,
+        Newline,
+        Backslash,
+        HeaderStart
+    }
+
+    internal class Token
+    {
+        public TokenType Type { get; set; }
+        public bool HasBody { get; set; }
+        public string Text { get; set; }
+    }
+
+    internal class StateToStatePath
+    {
+        public StateNode Destination { get; set; }
+        public Func<char, bool> Match { get; set; }
+
+        public StateToStatePath(StateNode destination, Func<char, bool> match)
+        {
+            Destination = destination;
+            Match = match;
+        }
+    }
+
+    internal class StateNode
+    {
+        public List<StateToStatePath> Pathes { get; } = new List<StateToStatePath>();
+        public Func<string, Token> GetResult { get; set; }
+
+        public StateNode(Func<string, Token> getResult)
+        {
+            GetResult = getResult;
+        }
+    }
+
+    internal class StateGraph
+    {
+        public List<StateNode> Nodes { get; } = new List<StateNode>();
+        public StateNode CurrentState { get; set; }
+
+        public StateGraph()
+        {
+            var whitespace = ' ';
+            var underscore = '_';
+            var backslash = '\\';
+            var newline = '\n';
+
+            var wordNode = new StateNode(buffer => new Token() { Type = TokenType.Word, Text = buffer });
+            var spaceNode = new StateNode(buffer => new Token() { Type = TokenType.Space, Text = buffer });
+            var underscoreNode = new StateNode(buffer => new Token { Type = TokenType.Underscore, Text = buffer, HasBody = true });
+            var newlineNode = new StateNode(buffer => new Token { Type = TokenType.Newline, Text = buffer });
+            var backslashNode = new StateNode(buffer => new Token { Type = TokenType.Backslash, Text = buffer });
+            var headerStartNode = new StateNode(buffer => new Token { Type = TokenType.HeaderStart, Text = buffer, HasBody = true });
+
+            var toWordNodePath = new StateToStatePath(wordNode, c => char.IsLetterOrDigit(c));
+            var toSpaceNodePath = new StateToStatePath(spaceNode, c => c == whitespace);
+            var toUnderscoreNodePath = new StateToStatePath(underscoreNode, c => c == underscore);
+            var toNewlineNodePath = new StateToStatePath(newlineNode, c => c == newline);
+            var toBackslashNodePath = new StateToStatePath(backslashNode, c => c == backslash);
+
+            wordNode.Pathes.AddRange(new[]
+            {
+                toWordNodePath,
+                toSpaceNodePath,
+                toUnderscoreNodePath,
+                toNewlineNodePath,
+                toBackslashNodePath
+            });
+
+            spaceNode.Pathes.AddRange(new[]
+            {
+                toWordNodePath,
+                toUnderscoreNodePath,
+                toBackslashNodePath
+            });
+
+            underscoreNode.Pathes.AddRange(new[]
+            {
+                toWordNodePath,
+                toBackslashNodePath,
+                toSpaceNodePath
+            });
+
+            backslashNode.Pathes.AddRange(new[]
+            {
+                toWordNodePath,
+                toSpaceNodePath,
+                toNewlineNodePath,
+                toUnderscoreNodePath
+            });
+
+            CurrentState = wordNode;
+        }
+    }
+}
---
+++ cs/Markdown/MarkdownToHtmlParser.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    internal class MarkdownToHtmlParser
+    {
+        private static StateGraph stateGraph = new StateGraph();
+
+        public string Render(string markdownText)
+        {
+            if (string.IsNullOrEmpty(markdownText))
+                throw new ArgumentException(nameof(markdownText));
+
+            var tokens = ParseStringToTokens(markdownText);
+            ApplySymbolShielding(tokens);
+
+            return ParseTokensToHtml(tokens);
+        }
+
+        private static List<Token> ParseStringToTokens(string text)
+        {
+            var tokens = new List<Token>();
+
+            int index = 0;
+            bool pathFound = false;
+            StringBuilder bufferSb = new StringBuilder();
+            char currentChar = ' ';
+            while (index < text.Length)
+            {
+                currentChar = text[index];
+                foreach (var path in stateGraph.CurrentState.Pathes)
+                {
+                    if (!path.Match(currentChar))
+                        continue;
+
+                    pathFound = true;
+                    if (path.Destination == stateGraph.CurrentState)
+                    {
+                        bufferSb.Append(currentChar);
+                    }
+                    else
+                    {
+                        tokens.Add(stateGraph.CurrentState.GetResult(bufferSb.ToString()));
+                        bufferSb.Clear();
+
+                        stateGraph.CurrentState = path.Destination;
+                        bufferSb.Append(currentChar);
+                    }
+                    
+                    index++;
+                    break;
+                }
+
+                if (!pathFound)
+                {
+                    //throw new ArgumentException($"Unexpected symbol '{text[index]}' at {index}");
+                    tokens.Add(new Token()
+                    {
+                        Type = TokenType.Word,
+                        HasBody = false,
+                        Text = bufferSb.ToString()
+                    });
+                    bufferSb.Clear();
+                }
+
+                pathFound = false;
+            }
+
+            if(bufferSb.Length > 0)
+            {
+                tokens.Add(stateGraph.CurrentState.GetResult(bufferSb.ToString()));
+            }
+
+            return tokens;
+        }
+
+        private static void ApplySymbolShielding(List<Token> tokens)
+        {
+            for (int i = 0; i < tokens.Count - 1; i++)
+            {
+                var currentToken = tokens[i];
+                if (currentToken.Type != TokenType.Backslash)
+                    continue;
+
+                var nextToken = tokens[i + 1];
+                if (nextToken.Type == TokenType.Word)
+                    continue;
+
+                if (nextToken.Type == TokenType.Newline)
+                    nextToken.Text = currentToken.Text + nextToken.Text;
+                nextToken.Type = TokenType.Word;
+                tokens.RemoveAt(i);
+            }
+        }
+
+        private static string ParseTokensToHtml(List<Token> tokens)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var token in tokens)
+                sb.Append(token.Text);
+            return sb.ToString();
+        }
+    }
+}
