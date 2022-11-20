9b5dfa8b66caaff6a63497ff7a1d32d0d0181622
Seekard <maks_byx@mail.ru>
2022-11-20 21:56:59 +0300

    Symbols were implemented.

---
+++ cs/Markdown/Symbol.cs
@@ -0,0 +1,19 @@
+﻿namespace Markdown;
+
+public class Symbol
+{
+    public string Value { get; }
+    public string RelatedTag { get; }
+    public string RelatedCloseTag { get; }
+
+    public int Length => Value.Length;
+
+    public static implicit operator string(Symbol symbol) => symbol.Value;
+    
+    public Symbol(string value, string relatedTag, string relatedCloseTag)
+    {
+        Value = value;
+        RelatedTag = relatedTag;
+        RelatedCloseTag = relatedCloseTag;
+    }
+}
--- cs/Markdown/Symbols.cs
+++ cs/Markdown/Symbols.cs
@@ -1,19 +1,45 @@
 ﻿namespace Markdown;
 
 public class Symbols
-    {
-        public static readonly string ItalicSymbol = "_";
-        public static readonly string EscapeSymbol = @"\";
-        public static readonly string BoldSymbol = "__";
-        public static readonly string HeaderSymbol = "#";
-
-        public static bool IsMarkdownSymbol(string symbol)
+{
+        private const string EscapeSymbol = @"\";
+        public static readonly Symbol EmptySymbol = new Symbol("", "", "");
+        public static readonly Dictionary<string, Symbol> symbols = new Dictionary<string, Symbol>()
         {
-            return symbol == ItalicSymbol || symbol == BoldSymbol || symbol == HeaderSymbol || symbol == EscapeSymbol;
+            {"", new Symbol("", "", "")},
+            {"_", new Symbol("_", "<em>", "</em>")},
+            {"__", new Symbol("__", "<strong>", "</strong>")},
+            {"#", new Symbol("#", "<h1>", "</h1>\n")},
+        };
+        
+        public static Symbol? IsMarkdownSymbol(string text, int indexOfSymbol)
+        {
+            Symbol symbol = null;
+            var possibleMarkdownSymbols = symbols.Keys.ToHashSet();
+            
+            for (int i = indexOfSymbol; i < text.Length; i++)
+            {
+                var segmentToCheck = text.Substring(indexOfSymbol, i - indexOfSymbol + 1);
+                possibleMarkdownSymbols.RemoveWhere(markdownSymbol => markdownSymbol.StartsWith(segmentToCheck) == false);
+                if (possibleMarkdownSymbols.Count == 0)
+                    return symbol;
+                symbol = symbols[segmentToCheck];
+            }
+
+            return symbol;
         }
 
-        public static bool IsEscapeSymbol(string symbol)
+        public static string? IsEscapeSymbol(string text, int indexOfSymbol)
         {
-            return symbol == EscapeSymbol;
+            for (var i = indexOfSymbol; i < text.Length; i++)
+            {
+                var segmentToCheck = text.Substring(indexOfSymbol, i - indexOfSymbol + 1);
+                if (EscapeSymbol.StartsWith(segmentToCheck) == false)
+                    return null;
+                if (segmentToCheck is EscapeSymbol)
+                    return EscapeSymbol;
+            }
+
+            return null;
         }
     }
