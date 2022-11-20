b8263f318922b67a66bfad4f634ca50864cb6656
Tupikov Ivan <i.tupikov12@gmail.com>
2022-11-20 20:17:16 +0500

    Added MarkdownTests project

---
+++ cs/Markdown/MarkdownTests/MdTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using FluentAssertions;
+using Markdown;
+
+
+namespace MarkdownTests
+{
+    
+    public class MdTest
+    {
+        private Md _md;
+        [SetUp]
+        public void Setup()
+        {
+            _md = new Md();
+        }
+
+        [TestCase("_text_", "<em>text</em>")]
+        public void Test1(string text, string exp)
+        {
+            var result = _md.Render(text);
+            result.Should().BeEquivalentTo(exp);
+        }
+    }   
+}
