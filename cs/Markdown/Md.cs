4ba818ae3d83649763a0d86a55cbeaa56f4d3acb
Seekard <maks_byx@mail.ru>
2022-11-20 21:57:44 +0300

    Md, TextSegment were implemented.

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -4,9 +4,144 @@ namespace Markdown;
 
 public class Md
 {
-    private readonly Stack<TextSegment> _segmentsStack = new Stack<TextSegment>();
-    public string Render(string text)
+    public static string Render(string text)
     {
-        throw new NotImplementedException();
+        var segmentsStack = new Stack<TextSegment>();
+        var mainSegment = new TextSegment(Symbols.EmptySymbol, null, false);
+        var segment = mainSegment;
+        
+        for (var i = 0; i < text.Length; i++)
+        {            
+            if (Symbols.IsEscapeSymbol(text, i) is not null)
+            {
+                HandleEscapeSymbol(segment, text, ref i);
+                continue;
+            }
+
+            if (text[i] == ' ' && segment.IsInsideWord)
+                segment = segmentsStack.Pop();
+
+            var symbol = Symbols.IsMarkdownSymbol(text, i);
+            if (i == 0 || text[i - 1] == '\n')
+            {
+                if (symbol is not null)
+                {
+                    if (symbol == "#")
+                    {
+                        segment = AddSegment(segmentsStack, segment, symbol);
+                        i += symbol.Length - 1;
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (segment.StartSymbol == "#")
+                        segment = CloseSegment(segmentsStack, segment);
+                }
+            }
+            
+            if (symbol is not null)
+            {
+                segment = HandleUnderlineSymbol(segmentsStack, segment, symbol, text, ref i);
+                continue;
+            }
+            segment.AddSymbol(text[i]);
+        }
+
+        return mainSegment.Render();
+    }
+
+    private static void HandleEscapeSymbol(TextSegment segment, string text, ref int indexOfSymbol)
+    {
+        var nextSymbol = Symbols.IsMarkdownSymbol(text, indexOfSymbol + 1);
+        if (nextSymbol is null)
+        {
+            segment.AddSymbol(text[indexOfSymbol]);
+            if (indexOfSymbol + 1 != text.Length && Symbols.IsEscapeSymbol(text, indexOfSymbol + 1) is null)
+                segment.AddSymbol(text[indexOfSymbol + 1]);
+            indexOfSymbol++;
+        }
+        else
+        {
+            segment.AddSymbol(nextSymbol);
+            indexOfSymbol += nextSymbol.Length;
+        }
+    }
+
+    private static TextSegment HandleUnderlineSymbol(Stack<TextSegment> segmentsStack, TextSegment segment, Symbol symbol, string text, ref int indexOfSymbol)
+    {
+        if (segment.StartSymbol == symbol)
+            segment = HandleUnderlineCloseSymbol(segmentsStack, segment, symbol, text, ref indexOfSymbol);
+        else
+            segment = HandleUnderlineOpenSymbol(segmentsStack, segment, symbol, text, ref indexOfSymbol);
+
+        indexOfSymbol += symbol.Length - 1;
+        return segment;
+    }
+    
+    private static TextSegment HandleUnderlineOpenSymbol(Stack<TextSegment> segmentsStack, TextSegment segment, Symbol symbol, string text, ref int indexOfSymbol)
+    {
+        if (IsNextSymbolEmpty(text, indexOfSymbol))
+        {
+            segment.AddSymbol(symbol);
+        }
+        else if (IsSymbolInMiddleOfWord(text, indexOfSymbol, symbol))
+        {
+            segment = AddSegment(segmentsStack, segment, symbol, true);
+        }
+        else
+        {
+            segment = AddSegment(segmentsStack, segment, symbol);
+        }
+        return segment;
+    }
+    
+    private static TextSegment HandleUnderlineCloseSymbol(Stack<TextSegment> segmentsStack, TextSegment segment, Symbol symbol, string text, ref int indexOfSymbol)
+    {
+        if (IsPreviousSymbolEmpty(text, indexOfSymbol))
+            segment.AddSymbol(symbol);
+        else
+            segment = CloseSegment(segmentsStack, segment);
+        return segment;
+    }
+    
+    private static TextSegment AddSegment(Stack<TextSegment> segmentsStack, TextSegment segment, Symbol symbol, bool isInsideWord = false)
+    {
+        var innerSegment = new TextSegment(symbol, segment, isInsideWord);
+        segment.AddInnerSegment(innerSegment);
+        segmentsStack.Push(segment);
+        return innerSegment;
+    }
+
+    private static TextSegment CloseSegment(Stack<TextSegment> segmentsStack, TextSegment segment)
+    {
+        segment.Close();
+        return segmentsStack.Pop();
+    }
+
+    private static bool IsSymbolInMiddleOfWord(string text, int indexOfSymbol, Symbol symbol)
+    {
+        return IsPreviousSymbolEmpty(text, indexOfSymbol) == false && indexOfSymbol != 0 &&
+               IsNextSymbolEmpty(text, indexOfSymbol + symbol.Length - 1) == false && indexOfSymbol + 1 != text.Length &&
+               Symbols.IsEscapeSymbol(text, indexOfSymbol - 1) is null;
+    }
+    
+    private static bool IsPreviousSymbolEmpty(string text, int indexOfSymbol)
+    {
+        if (indexOfSymbol == 0)
+            return false;
+        return char.IsWhiteSpace(text[indexOfSymbol - 1]);
+    }
+    
+    private static bool IsNextSymbolEmpty(string text, int indexOfSymbol)
+    {
+        if (indexOfSymbol + 1 == text.Length)
+            return true;
+        return char.IsWhiteSpace(text[indexOfSymbol + 1]);
+    }
+
+    private static bool NextSymbolIsMarkdownSymbol(string text, int indexOfSymbol)
+    {
+        return Symbols.IsMarkdownSymbol(text, indexOfSymbol) != null;
     }
 }
--- cs/Markdown/TextSegment.cs
+++ cs/Markdown/TextSegment.cs
@@ -4,23 +4,119 @@ namespace Markdown;
 
 public class TextSegment
     {
-        private StringBuilder _content;
-        private List<TextSegment> _innerSegments;
+        private readonly StringBuilder _content = new StringBuilder();
+        private readonly List<(int index, TextSegment segment)> _innerSegmentsWithPosition = new();
+        private readonly List<TextSegment> _parentSegments = new();
+        private bool _isClosed = false;
+        public Symbol StartSymbol { get; }
+        public bool IsInsideWord { get; }
 
-        public int Length => _content.Length;
+        public TextSegment(Symbol startSymbol, TextSegment? parentSegment, bool isInsideWord)
+        {
+            StartSymbol = startSymbol;
+            IsInsideWord = isInsideWord;
+            _content.Append(startSymbol);
+            if (parentSegment != null)
+            {
+                _parentSegments.Add(parentSegment);
+                foreach (var segment in parentSegment._parentSegments)
+                    _parentSegments.Add(segment);
+            }
+        }
 
+        public void Close()
+        {
+            if (StartSymbol == "#")
+                _content.Replace("\n", "#", _content.Length - StartSymbol.Length, StartSymbol.Length);
+            else
+                AddSymbol(StartSymbol.Value);
+            Console.WriteLine($"_content: {_content}");
+            _isClosed = true;
+        }
+        
         public void AddSymbol(string symbol)
         {
-            throw new NotImplementedException();
+            _content.Append(symbol);
+        }
+        
+        public void AddSymbol(char symbol)
+        {
+            _content.Append(symbol);
         }
 
         public void AddInnerSegment(TextSegment segment)
         {
-            throw new NotImplementedException();
+            var startIndexOfSegment = _content.Length;
+            _innerSegmentsWithPosition.Add((startIndexOfSegment, segment));
         }
 
         public string Render()
         {
-            throw new NotImplementedException();
+            RenderInnerSegments();
+            if (StartSymbol == "_")
+            {
+                if (HasDigitsInContent())
+                    return _content.ToString();
+            }
+
+            if (StartSymbol == "__" || StartSymbol == "_")
+            {
+                bool isSegmentEmpty = true;
+                foreach (var chank in _content.GetChunks())
+                {
+                    if (chank.ToArray().Any(c => c != '_' && char.IsWhiteSpace(c) == false))
+                    {
+                        isSegmentEmpty = false;
+                        break;
+                    }
+                }
+
+                if (isSegmentEmpty)
+                    return _content.ToString();
+            }
+            if (StartSymbol == "__" && _parentSegments.Any(segment => segment.StartSymbol == "_"))
+                return _content.ToString();
+            if (StartSymbol == "#")
+            {
+                _content.Append("#");
+                _isClosed = true;
+            }
+            if (_isClosed)
+                ReplaceBorderMarkdownSymbolsToTags();
+            return _content.ToString();
+        }
+
+        private void RenderInnerSegments()
+        {
+            var sortedInnerSegments = _innerSegmentsWithPosition.OrderBy(segment => segment.index);
+            var segmentShift = 0;
+            
+            foreach (var segmentWithPosition in sortedInnerSegments)
+            {
+                var startContentLength = _content.Length;
+                var segmentIndex = segmentWithPosition.index + segmentShift;
+                var segmentContent = segmentWithPosition.segment.Render();
+                Console.WriteLine($"#1{_content}");
+                _content.Insert(segmentIndex, segmentContent);
+                Console.WriteLine($"#2{_content}");
+                segmentShift += _content.Length - startContentLength;
+            }
+        }
+
+        private string RenderWithoutReplacingMarkdownSymbols()
+        {
+            RenderInnerSegments();
+            return _content.ToString();
+        }
+
+        private bool HasDigitsInContent()
+        {
+            return _content.ToString().Any(char.IsDigit);
+        }
+
+        private void ReplaceBorderMarkdownSymbolsToTags()
+        {
+            _content.Replace(StartSymbol.Value, StartSymbol.RelatedTag, 0, StartSymbol.Length).
+                Replace(StartSymbol.Value, StartSymbol.RelatedCloseTag, _content.Length - StartSymbol.Length, StartSymbol.Length);
         }
     }
