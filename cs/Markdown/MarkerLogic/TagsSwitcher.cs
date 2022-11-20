2d52412b0b886e1a22aa8e24abf84c741a0c2419
Art <bartalamu@list.ru>
2022-11-21 00:11:46 +0500

    TagsSwitcher update - реализована логика замены тегов в тексте на их html версии - реализовано удаление экранов из текста (если экранируются теги или '\')

---
+++ cs/Markdown/MarkerLogic/TagsSwitcher.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using Markdown.Interfaces;
+using Markdown.TagClasses;
+
+namespace Markdown.MarkerInnerClasses
+{
+    public class TagsSwitcher : ITagsSwitcher
+    {
+        public string SwitchTags(List<TagInfo> tags, string paragraph)
+        {
+            tags.Sort();
+
+            var result = new StringBuilder(paragraph);
+            foreach (var tag in tags)
+            {
+                SwitchTag(result, tag);
+                AdjustPositions(tags, tag);
+            }
+
+            var header = tags.FirstOrDefault(x => !x.IsEscaped && x.Type == TagType.Header);
+            if (header is not null)
+            {
+                result.Remove(0, 1);
+                result.Insert(0, "<h1>");
+                result.Append("</h1>");
+            }
+
+            result.Replace(@"\\", @"\");
+            return result.ToString();
+        }
+
+        private void AdjustPositions(List<TagInfo> tags, TagInfo tag)
+        {
+            if (tag.IsEscaped)
+            {
+                tags.Where(x => x.Position > tag.Position).ToList().ForEach(x => x.Position--);
+                return;
+            }
+
+            tags.Where(x => x.Position > tag.Position).ToList()
+                .ForEach(x => x.Position += tag.GetHtmlTag().Length - tag.Length);
+        }
+
+        private void SwitchTag(StringBuilder paragraph, TagInfo tag)
+        {
+            if (tag.IsEscaped)
+            {
+                paragraph.Remove(tag.Position - 1, 1);
+                return;
+            }
+
+            paragraph.Remove(tag.Position, tag.Length);
+            paragraph.Insert(tag.Position, tag.GetHtmlTag());
+        }
+    }
+}
