5bd4cfe3b315783d5642e8c574cb1f0de5ec1305
Zuguki <guskov.zahar20@gmail.com>
2022-11-22 21:19:41 +0300

    OneCharTests was added

--- cs/Markdown.Tests/MarkdownTest.cs
+++ cs/Markdown.Tests/MarkdownTest.cs
@@ -21,4 +21,20 @@ public class Tests
         var rendererCheck = (Action) (() => markdown!.Render(mdInput));
         rendererCheck.Should().Throw<ArgumentException>();
     }
+
+    [TestCase("_Something_", "<em>Something</em>")]
+    [TestCase("#Something#", "<h1>Something</h1>")]
+    public void Renderer_OneCharMarkdownTag_LineWithHtml(string mdInput, string result)
+    {
+        markdown!.Render(mdInput).Should().Be(result);
+    }
+
+    [TestCase("_Some_ word _with_ tags", "<em>Some</em> word <em>with</em> tags")]
+    [TestCase("#Some# word #with# tags", "<h1>Some</h1> word <h1>with</h1> tags")]
+    [TestCase("#Some# word _with_ tags", "<h1>Some</h1> word <em>with</em> tags")]
+    [TestCase("#Some# _word_ _with_ #tags#", "<h1>Some</h1> <em>word</em> <em>with</em> <h1>tags</h1>")]
+    public void Renderer_OneCharMarkdownTagSeveral_LineWithHtml(string mdInput, string result)
+    {
+        markdown!.Render(mdInput).Should().Be(result);
+    }
 }
--- cs/Markdown/Translators/Implementation/MarkdownTranslator.cs
+++ cs/Markdown/Translators/Implementation/MarkdownTranslator.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Markdown.Helpers;
 using Markdown.Tags;
 
@@ -6,16 +7,57 @@ namespace Markdown.Translators.Implementation;
 public class MarkdownTranslator : ITranslator
 {
     private List<ITag?> tags;
-    private Stack<ITag> stackOfTags;
+    private Stack<TagWithIndex> stackOfTags;
+    private StringBuilder sb;
+    private int tagsLength = 0;
 
     public MarkdownTranslator()
     {
         tags = TagHelper.GetAllTags<ITag>()!.ToList();
-        stackOfTags = new Stack<ITag>();
+        stackOfTags = new Stack<TagWithIndex>();
+        sb = new StringBuilder();
     }
     
     public string Translate(string input)
     {
-        throw new NotImplementedException();
+        for (var index = 0; index < input.Length; index++)
+        {
+            var currentTag = tags.FirstOrDefault(tag => tag!.SourceName == input[index].ToString());
+            if (currentTag is not null && stackOfTags.Count == 0 ||
+                currentTag is not null && stackOfTags.Peek().Tag != currentTag) 
+            {
+                stackOfTags.Push(new TagWithIndex(currentTag, index));
+            }
+            else if (currentTag is not null && stackOfTags.Peek().Tag == currentTag)
+            {
+                var previewTag = stackOfTags.Pop();
+                ReplaceByIndex(previewTag.Tag, previewTag.Index);
+            }
+            else
+                sb.Append(input[index]);
+        }
+
+        return sb.ToString();
+    }
+
+    private void ReplaceByIndex(ITag tag, int index)
+    {
+        var sbLength = sb.Length;
+        var translateName = TagHelper.GetHtmlFormat(tag.TranslateName);
+        sb.Insert(index + tagsLength, translateName.start);
+        sb.Append(translateName.end);
+        tagsLength += sb.Length - sbLength - 2;
+    }
+}
+
+public class TagWithIndex
+{
+    public ITag Tag;
+    public int Index;
+
+    public TagWithIndex(ITag tag, int index)
+    {
+        Tag = tag;
+        Index = index;
     }
 }
