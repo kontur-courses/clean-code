7a355922471e9f28f93072660ff0149264045dfe
Igor Sotarev <igor@sotarev.ru>
2022-11-23 18:48:55 +0500

    Extend TokenTree

--- cs/Markdown/Token/TokenTree.cs
+++ cs/Markdown/Token/TokenTree.cs
@@ -4,19 +4,39 @@ namespace Markdown.Token;
 
 public class TokenTree
 {
-    public bool IsLeaf => Value != null;
+    public bool IsLeaf => children.Count == 0;
     public string? Value { get; set; }
-    public Tag Tag { get; }
+    public Tag? Tag { get; }
+    public bool Closed { get; private set; }
+    public bool Valid { get; private set; }
     public IEnumerable<TokenTree> Children => children;
     private readonly List<TokenTree> children = new ();
+    
+    public TokenTree()
+    {
+        Closed = false;
+        Valid = true;
+    }
     
     public TokenTree(Tag tag)
     {
         Tag = tag;
+        Closed = false;
+        Valid = true;
     }
 
     public void AppendChild(TokenTree child)
     {
         children.Add(child);
+    }
+
+    public void Invalidate()
+    {
+        Valid = false;
+    }
+
+    public void Close()
+    {
+        Closed = true;
     }
 }
---
+++ cs/Markdown/Token/TokenTreeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Markdown.Tags;
+
+namespace Markdown.Token;
+
+public static class TokenTreeExtensions
+{
+    public static bool IsTaggedWith(this TokenTree tree, Tag tag)
+    {
+        return tree.Tag != null && tree.Tag.Equals(tag);
+    }
+
+    public static IEnumerable<TokenTree> TaggedChildren(this TokenTree tree)
+    {
+        return tree.Children.Where(child => child.Tag != null);
+    }
+
+    public static bool HasChildrenWithOpenTag(this TokenTree tree, Tag tag)
+    {
+        return tree.TaggedChildren()
+            .Where(child => !child.Closed)
+            .Any(child => child.IsTaggedWith(tag));
+    }
+}
