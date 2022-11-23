7541db04209dce809707f1ee9eca468866e13a6e
speedUpDev <70540091+speedUpDev@users.noreply.github.com>
2022-11-23 22:48:26 +0500

    Tests added

--- cs/Markdown.Tests/MdRenderer_Should.cs
+++ cs/Markdown.Tests/MdRenderer_Should.cs
@@ -46,6 +46,16 @@ public class MdRenderer_Should
         var htmlText = markdownRenderer.Render(line);
         htmlText.Should().BeEquivalentTo(expected);
     }
+    
+    [TestCase("\\\\__bold__", "\\<strong>bold</strong>")]
+    [TestCase("\\__bold__", "__bold__")]
+    [TestCase("\\_italic_", "_italic_")]
+    [TestCase("_italic\\_", "_italic_")]
+    public void WorkCorrect_WithEscapeTags(string line, string expected)
+    {
+        var htmlText = markdownRenderer.Render(line);
+        htmlText.Should().BeEquivalentTo(expected);
+    }
 
     [TestCase("# simple header", "<h1> simple header</h1>")]
     [TestCase("__bold__ and _italic_", "<strong>bold</strong> and <em>italic</em>")]
@@ -56,12 +66,36 @@ public class MdRenderer_Should
         var htmlText = markdownRenderer.Render(line);
         htmlText.Should().BeEquivalentTo(expected);
     }
-
+    
     [TestCase("# _sequence_ with __a __lot__ of t_a_gs __wo_r_d1 _word2_ _word3_ da__",
         "<h1> <em>sequence</em> with __a <strong>lot</strong> of t<em>a</em>gs __wo<em>r</em>d1 _word2_ _word3_ da__</h1>")]
     public void WorkCorrect_WithHardCases(string line, string expected)
     {
         var htmlText = markdownRenderer.Render(line);
         htmlText.Should().BeEquivalentTo(expected);
+    }
+    
+    [TestCase("tags",
+        "tags")]
+    [TestCase("long text without tags, <tag></tag>",
+        "long text without tags, <tag></tag>")]
+    public void WorkCorrect_WithTextWOTags(string line, string expected)
+    {
+        var htmlText = markdownRenderer.Render(line);
+        htmlText.Should().BeEquivalentTo(expected);
+    }
+    
+    [Test]
+    public void WorkCorrect_WithEmptyString()
+    {
+        var htmlText = markdownRenderer.Render("");
+        htmlText.Should().BeEquivalentTo("");
+    }
+
+    [Test]
+    public void WorkCorrect_WithNull()
+    {
+        var htmlText = markdownRenderer.Render(null);
+        htmlText.Should().BeEquivalentTo(null);
     }
 }
