9c08fd7960521b57945cea8372ae7d171c1b253d
Maksim Tsvetkov <atm.tsvetk@gmail.com>
2022-11-25 01:29:14 +0500

    Implemented html string build

--- cs/Markdown/Html/HtmlBuilder.cs
+++ cs/Markdown/Html/HtmlBuilder.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Markdown.Enums;
 using Markdown.Interfaces;
 
 namespace Markdown.Html
 {
     public class HtmlBuilder : IBuilder
     {
+        private readonly StringBuilder htmlTextBuilder = new ();
+
         public string Build(IEnumerable<Token> tokens)
         {
-            throw new System.NotImplementedException();
+            foreach (var token in tokens)
+                htmlTextBuilder.Append(BuildToken(token));
+
+            return htmlTextBuilder.ToString();
+        }
+
+        private string BuildToken(Token token)
+        {
+            var tokenContentBuilder = new StringBuilder();
+            
+            tokenContentBuilder.Append(token.Tag.Opening);
+            tokenContentBuilder.Append(token.Content);
+
+            foreach (var children in token.Childrens)
+            {
+                tokenContentBuilder.Append(BuildToken(children));
+            }
+
+            tokenContentBuilder.Append(token.Tag.Closing);
+
+            return tokenContentBuilder.ToString();
         }
     }
 }
