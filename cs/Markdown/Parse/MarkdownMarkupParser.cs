ba81acfac4d72d971430635be51b6ab22302a6e9
Igor Sotarev <igor@sotarev.ru>
2022-11-25 19:08:17 +0500

    Refactor MarkdownMarkupParser

--- cs/Markdown/Parse/MarkdownMarkupParser.cs
+++ cs/Markdown/Parse/MarkdownMarkupParser.cs
@@ -6,128 +6,116 @@ namespace Markdown.Parse;
 
 public class MarkdownMarkupParser : IMarkupParser
 {
-    private readonly HashSet<char> specialChars = new() { '_', '#' };
-    private readonly HashSet<Tag> tagsThatCloseOnNewLine = new() { MarkdownTags.Heading };
     private const char EscapeChar = '\\';
 
     public TokenTree Parse(string text)
     {
         var root = new TokenTree();
-        var stack = new Stack<TokenTree>();
+        var lines = text.Split("\r\n");
+        var newLinesCount = 0;
+
+        foreach (var line in lines)
+            root.AppendChild(ParseLine(line, newLinesCount++ < lines.Length - 1));
+        
+        return root;
+    }
+
+    private static TokenTree ParseLine(string line, bool appendNewLine)
+    {
         var stringBuilder = new StringBuilder();
+        var stack = new Stack<TokenTree>();
+        var root = new TokenTree();
         var currentNode = root;
-        var currentIndex = 0;
-
-        while (currentIndex < text.Length)
+        for (var i = 0; i < line.Length; i++)
         {
-            var currentChar = text[currentIndex];
-
-            if (IsNewLine(text, currentIndex, out var newLineSequence))
+            var currentChar = line[i];
+            
+            
+            if (currentChar == EscapeChar)
             {
-                currentIndex += newLineSequence.Length - 1;
-                stringBuilder.Append(newLineSequence);
-                FlushStringBuilder(stringBuilder, currentNode);
-                currentNode = CloseNestedTags(stack, currentNode) ?? root;
+                stringBuilder.Append(GetCharToAppendOnEscape(line, ref i));
+                continue;
             }
-            else if (currentChar == EscapeChar)
+            if (!MdRules.IsSpecialChar(currentChar))
             {
-                if (currentIndex + 1 < text.Length && IsSpecialChar(text[currentIndex + 1]))
-                {
-                    stringBuilder.Append(text[currentIndex + 1]);
-                    currentIndex++;
-                }
-                else
-                {
-                    stringBuilder.Append(text[currentIndex]);
-                }
+                stringBuilder.Append(currentChar);
+                continue;
             }
-            else if (IsSpecialChar(currentChar))
-            {
-                var tag = MdRules.ParseTag(stack, currentNode, text, currentIndex, 
-                    out var nextIndex);
+            
+            var tag = MdRules.ParseTag(stack, currentNode, line, i, out var nextIndex);
 
-                if (IsClosing(text, tag, stack, currentNode, currentIndex))
-                {
-                    FlushStringBuilder(stringBuilder, currentNode);
-                    if (currentNode.IsTaggedWith(tag)) 
-                        currentNode.Close();
-                    else 
-                        CloseTag(tag, stack);
-                    currentNode = stack.Pop();
-                }
-                else if (Intersects(tag, stack, root))
-                {
-                    var father = stack.Count > 0 ? stack.Peek() : root;
-                    foreach (var intersection in MdRules.GetPossibleIntersections(tag))
-                    {
-                        var intersectionFound = false;
-                        foreach (var child in father.TaggedChildren())
-                        {
-                            if (!child.IsTaggedWith(intersection) || !child.Closed) continue;
-                            if (!child.HasChildrenWithOpenTag(tag)) continue;
-                            child.Invalidate();
-                            stringBuilder.Append(text.AsSpan(currentIndex, nextIndex - currentIndex + 1));
-                            intersectionFound = true;
-                            break;
-                        }
-                        if (intersectionFound) break;
-                    }
-                } 
-                else if (IsOpening(text, tag, stack, currentIndex))
-                {
-                    FlushStringBuilder(stringBuilder, currentNode);
-                    currentNode = OpenNewNode(currentNode, stack, tag);
-                }
+            if (IsClosing(line, tag, stack, currentNode, i))
+            {
+                FlushTextToChildNode(stringBuilder, currentNode);
+                if (currentNode.Tag == tag)
+                    currentNode.Close();
                 else
-                    stringBuilder.Append(text.AsSpan(currentIndex, nextIndex - currentIndex + 1));
-                currentIndex = nextIndex;
+                    CloseTag(tag, stack);
+                currentNode = stack.Pop();
+            }
+            else if (Intersects(tag, stack, root))
+            {
+                stringBuilder.Append(line.AsSpan(i, nextIndex - i + 1));
+                var father = stack.Count > 0 ? stack.Peek() : root;
+                InvalidateIntersection(tag, father);
+            }
+            else if (IsOpening(line, tag, stack, i))
+            {
+                FlushTextToChildNode(stringBuilder, currentNode);
+                currentNode = OpenNewNode(currentNode, stack, tag);
             }
             else
             {
-                stringBuilder.Append(currentChar);
+                stringBuilder.Append(line.AsSpan(i, nextIndex - i + 1));
             }
-            
-            currentIndex++;
+
+            i = nextIndex;
         }
-        
-        FlushStringBuilder(stringBuilder, currentNode);
+
+        if (appendNewLine) stringBuilder.Append("\r\n");
+        FlushTextToChildNode(stringBuilder, currentNode);
         CloseNestedTags(stack, currentNode);
         
         return root;
     }
 
-    private TokenTree? CloseNestedTags(Stack<TokenTree> stack, TokenTree currentNode)
+    private static void InvalidateIntersection(Tag tag, TokenTree father)
     {
-        if (currentNode.Tag != null && tagsThatCloseOnNewLine.Contains(currentNode.Tag))
+        foreach (var intersection in MdRules.GetPossibleIntersections(tag))
+        {
+            var intersectionFound = false;
+            foreach (var child in father.TaggedChildren())
+            {
+                if (!child.IsTaggedWith(intersection) || !child.Closed) continue;
+                if (!child.HasChildrenWithOpenTag(tag)) continue;
+                child.Invalidate();
+                intersectionFound = true;
+                break;
+            }
+
+            if (intersectionFound) break;
+        }
+    }
+
+    private static char GetCharToAppendOnEscape(string text, ref int index)
+    {
+        if (index + 1 < text.Length && MdRules.IsSpecialChar(text[index + 1]))
+            return text[++index];
+        
+        return text[index];
+    }
+
+    private static void CloseNestedTags(Stack<TokenTree> stack, TokenTree currentNode)
+    {
+        if (currentNode.Tag != null && currentNode.Tag.ClosesOnNewLine)
             currentNode.Close();
         else while (stack.Count > 2)
         {
             var currentLevel = stack.Pop();
-            if (currentLevel.Tag == null) continue;
-            if (!tagsThatCloseOnNewLine.Contains(currentLevel.Tag)) continue;
+            if (currentLevel.Tag == null || !currentLevel.Tag.ClosesOnNewLine) continue;
             currentLevel.Close();
             break;
         }
-        
-        return stack.Count > 0 ? stack.Peek() : null;
-    }
-
-    private static bool Intersects(Tag tag, Stack<TokenTree> stack, TokenTree root)
-    {
-        return MdRules.Intersects(tag, stack, root);
-    }
-
-    private static bool IsOpening(string text, Tag tag, Stack<TokenTree> stack, int currentIndex)
-    {
-        return MdRules.IsValidOpening(tag, stack, text, currentIndex);
-    }
-    
-    private static bool IsClosing(string text, Tag tag, Stack<TokenTree> stack, 
-        TokenTree currentNode, int currentIndex)
-    {
-        return MdRules.IsValidClosing(tag, stack, text, currentIndex) 
-               && (currentNode.IsTaggedWith(tag) 
-                   || stack.Any(node => node.IsTaggedWith(tag) && !node.Closed));
     }
 
     private static TokenTree OpenNewNode(TokenTree currentNode, Stack<TokenTree> stack, Tag tag)
@@ -153,7 +141,7 @@ public class MarkdownMarkupParser : IMarkupParser
         currentLevel.Close();
     }
 
-    private static void FlushStringBuilder(StringBuilder stringBuilder, TokenTree currentNode)
+    private static void FlushTextToChildNode(StringBuilder stringBuilder, TokenTree currentNode)
     {
         if (stringBuilder.Length == 0) return;
         
@@ -161,25 +149,22 @@ public class MarkdownMarkupParser : IMarkupParser
         currentNode.AppendChild(textNode);
         stringBuilder.Clear();
     }
-
-    private static bool IsNewLine(string text, int currentIndex, out string newLineSequence)
+    
+    private static bool Intersects(Tag tag, Stack<TokenTree> stack, TokenTree root)
     {
-        switch (text[currentIndex])
-        {
-            case '\n':
-                newLineSequence = "\n";
-                return true;
-            case '\r' when currentIndex + 1 < text.Length && text[currentIndex + 1] == '\n':
-                newLineSequence = "\r\n";
-                return true;
-            default:
-                newLineSequence = "";
-                return false;
-        }
+        return MdRules.Intersects(tag, stack, root);
     }
 
-    private bool IsSpecialChar(char c)
+    private static bool IsOpening(string text, Tag tag, Stack<TokenTree> stack, int currentIndex)
     {
-        return specialChars.Contains(c);
+        return MdRules.IsValidOpening(tag, stack, text, currentIndex);
+    }
+    
+    private static bool IsClosing(string text, Tag tag, Stack<TokenTree> stack, 
+        TokenTree currentNode, int currentIndex)
+    {
+        return MdRules.IsValidClosing(tag, stack, text, currentIndex) 
+               && (currentNode.IsTaggedWith(tag) 
+                   || stack.Any(node => node.IsTaggedWith(tag) && !node.Closed));
     }
 }
--- cs/Markdown/Parse/MdRules.cs
+++ cs/Markdown/Parse/MdRules.cs
@@ -1,33 +1,45 @@
 ﻿using System.Collections.Immutable;
 using Markdown.Tags;
+using Markdown.Tags.MarkdownTags;
 using Markdown.Token;
 
 namespace Markdown.Parse;
 
 public static class MdRules
 {
-    private static readonly HashSet<Tag> TagsThatCanIntersect = new()
-    {
-        MarkdownTags.Bold,
-        MarkdownTags.Italics,
-    };
+    private static readonly List<Tag> TagsThatCanIntersect;
+    private static readonly List<Tag> SupportedTags;
+    private static readonly List<char> SpecialChars;
 
-    private static readonly Dictionary<Tag, HashSet<Tag>> PossibleIntersections = new()
+    static MdRules()
     {
-        { MarkdownTags.Bold, new HashSet<Tag> { MarkdownTags.Italics } },
-        { MarkdownTags.Italics, new HashSet<Tag> { MarkdownTags.Bold } }
-    };
+        SupportedTags = MarkdownTagProvider.SupportedTags;
+        TagsThatCanIntersect = SupportedTags.Where(tag => tag.CanIntersect).ToList();
+        SpecialChars = new List<char>();
+        foreach (var tag in SupportedTags)
+        {
+            if (tag.Opening.Length > 0 && !SpecialChars.Contains(tag.Opening[0]))
+                SpecialChars.Add(tag.Opening[0]);
+            if (tag.Closing.Length > 0 && !SpecialChars.Contains(tag.Closing[0]))
+                SpecialChars.Add(tag.Closing[0]);
+        }
+    }
+    
+    public static bool IsSpecialChar(char c)
+    {
+        return SpecialChars.Contains(c);
+    }
 
     public static IEnumerable<Tag> GetPossibleIntersections(Tag tag)
     {
-        return CanIntersect(tag) ? PossibleIntersections[tag] : ImmutableArray<Tag>.Empty;
+        return tag.CanIntersect 
+            ? TagsThatCanIntersect.Where(intersection => intersection != tag) 
+            : ImmutableArray<Tag>.Empty;
     }
-
-    public static bool CanIntersect(Tag tag) => TagsThatCanIntersect.Contains(tag);
 
     public static bool Intersects(Tag tag, Stack<TokenTree> stack, TokenTree root)
     {
-        if (!CanIntersect(tag)) return false;
+        if (!tag.CanIntersect) return false;
         
         var father = stack.Count > 0 ? stack.Peek() : root;
         return GetPossibleIntersections(tag)
@@ -36,48 +48,48 @@ public static class MdRules
                 .Any(child => child.HasChildrenWithOpenTag(tag)));
     }
 
-    public static Tag ParseTag(Stack<TokenTree> stack, TokenTree currentNode, string text, int position, 
-        out int newPosition)
+    public static Tag ParseTag(Stack<TokenTree> stack, TokenTree currentNode, string text, int position, out int newPosition)
     {
         newPosition = position;
         switch (text[position])
         {
             case '_':
-                if (position + 1 >= text.Length || text[position + 1] != '_') 
-                    return MarkdownTags.Italics;
-                
+                if (position + 1 >= text.Length || text[position + 1] != '_')
+                    return MarkdownTagProvider.Italics;
+        
                 if (position + 2 < text.Length && text[position + 1] == '_' && text[position + 2] == '_')
                 {
-                    if (currentNode.IsTaggedWith(MarkdownTags.Bold))
+                    if (currentNode.IsTaggedWith(MarkdownTagProvider.Bold))
                     {
                         newPosition++;
-                        return MarkdownTags.Bold;
+                        return MarkdownTagProvider.Bold;
                     }
-
-                    if (currentNode.IsTaggedWith(MarkdownTags.Italics))
+        
+                    if (currentNode.IsTaggedWith(MarkdownTagProvider.Italics))
                     {
-                        return MarkdownTags.Italics;
+                        return MarkdownTagProvider.Italics;
                     }
-
+        
                     foreach (var tree in stack)
                     {
-                        if (tree.IsTaggedWith(MarkdownTags.Bold))
+                        if (tree.IsTaggedWith(MarkdownTagProvider.Bold))
                         {
                             newPosition++;
-                            return MarkdownTags.Bold;
+                            return MarkdownTagProvider.Bold;
                         }
-                        
-                        if (tree.IsTaggedWith(MarkdownTags.Italics))
+        
+                        if (tree.IsTaggedWith(MarkdownTagProvider.Italics))
                         {
-                            return MarkdownTags.Italics;
+                            return MarkdownTagProvider.Italics;
                         }
                     }
                 }
-
+        
                 newPosition++;
-                return MarkdownTags.Bold;
-            case '#':
-                return MarkdownTags.Heading;
+                return MarkdownTagProvider.Bold;
+            case '#' when position + 1 < text.Length && text[position + 1] == ' ':
+                newPosition++;
+                return MarkdownTagProvider.Heading;
             default:
                 throw new NotSupportedException();
         }
@@ -85,82 +97,11 @@ public static class MdRules
 
     public static bool IsValidOpening(Tag tag, Stack<TokenTree> stack, string text, int position)
     {
-        if (tag.Equals(MarkdownTags.Bold)) return IsValidBoldOpening(stack, text, position);
-        if (tag.Equals(MarkdownTags.Italics)) return IsValidItalicsOpening(stack, text, position);
-        if (tag.Equals(MarkdownTags.Heading)) return IsValidHeadingOpening(stack, text, position);
-
-        return false;
+        return tag.IsValidOpening(stack, text, position);
     }
     
     public static bool IsValidClosing(Tag tag, Stack<TokenTree> stack, string text, int position)
     {
-        if (tag.Equals(MarkdownTags.Bold)) return IsValidBoldClosing(stack, text, position);
-        if (tag.Equals(MarkdownTags.Italics)) return IsValidItalicsClosing(stack, text, position);
-
-        return false;
-    }
-    
-    private static bool IsValidBoldOpening(Stack<TokenTree> stack, string text, int position)
-    {
-        if (position + 1 >= text.Length || position + 2 >= text.Length) return false;
-        if (text[position] != '_' || text[position + 1] != '_') return false;
-
-        var nextChar = text[position + 2];
-        if (nextChar is ' ' or '\r' or '\n') return false;
-        
-        if (position > 0 && char.IsDigit(text[position - 1]) && char.IsDigit(nextChar)) return false;
-        if (stack.Where(tree => tree.Tag != null).Any(tree => tree.Tag.Equals(MarkdownTags.Italics))) return false;
-        
-        return true;
-    }
-
-    private static bool IsValidBoldClosing(Stack<TokenTree> stack, string text, int position)
-    {
-        if (position + 1 >= text.Length) return false;
-        if (text[position] != '_' || text[position + 1] != '_') return false;
-        if (position - 1 < 0) return false;
-        
-        var previousChar = text[position - 1];
-        if (previousChar is ' ' or '\r' or '\n') return false;
-
-        if (position + 2 < text.Length && char.IsDigit(previousChar) && char.IsDigit(text[position + 2])) 
-            return false;
-
-        return true;
-    }
-
-    private static bool IsValidItalicsOpening(Stack<TokenTree> stack, string text, int position)
-    {
-        if (position + 1 >= text.Length) return false;
-        if (text[position] != '_') return false;
-
-        var nextChar = text[position + 1];
-        if (nextChar is ' ' or '\r' or '\n') return false;
-
-        if (position > 0 && char.IsDigit(text[position - 1]) && char.IsDigit(nextChar)) return false;
-
-        return true;
-    }
-
-    private static bool IsValidItalicsClosing(Stack<TokenTree> stack, string text, int position)
-    {
-        if (text[position] != '_') return false;
-        if (position - 1 < 0) return false;
-        
-        var previousChar = text[position - 1];
-        if (previousChar is ' ' or '\r' or '\n') return false;
-        
-        if (position + 1 < text.Length && char.IsDigit(previousChar) && char.IsDigit(text[position + 1])) 
-            return false;
-
-        return true;
-    }
-
-    private static bool IsValidHeadingOpening(Stack<TokenTree> stack, string text, int position)
-    {
-        if (position == 0) return true;
-        if (text[position - 1] != '\n' && text[position - 1] != '\r') return false;
-
-        return true;
+        return tag.IsValidClosing(stack, text, position);
     }
 }
