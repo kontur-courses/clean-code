4f30654ae08a5ceb4a7aa330f5b21007e9724f75
Anton Yaskov <yaskovanton333@gmail.com>
2022-11-25 21:20:59 +0500

    Rework TokenParser

--- cs/Markdown/TokenParser.cs
+++ cs/Markdown/TokenParser.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Markdown;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,91 +14,165 @@ namespace Markdown
     public class TokenParser
     {
         public readonly List<Tag> Tags;
-        private HashSet<int> usedIndices;
 
-        public TokenParser(List<Tag> tags)
+        public TokenParser()
         {
-            Tags = tags;
-            usedIndices = new HashSet<int>();
+            Tags = new List<Tag>
+            {
+                new Tag("__", "__", "strong",
+                    new [] {"hasSpace", "hasLetter"},
+            (tag, token, text, startInd) =>
+                            text.TryGetChar(startInd + tag.OpenMark.Length, out var ch1)
+                            && !char.IsWhiteSpace(ch1),
+            (tag, token, text, endInd) =>
+                            text.TryGetChar(endInd - 1, out var ch1)
+                            && !char.IsWhiteSpace(ch1), 
+           (tag, token, text, endInd) => 
+                            token.CustomBools["hasLetter"] &&
+                               (!(text.TryGetChar(token.StartOpenMark - 1, out var ch1)
+                                  && !char.IsWhiteSpace(ch1)
+                                  || text.TryGetChar(endInd + tag.CloseMark.Length, out var ch2)
+                                  && !char.IsWhiteSpace(ch2))
+                                || !token.CustomBools["hasSpace"]),
+        (tag, token, ch) =>
+                        {
+                            if (char.IsWhiteSpace(ch))
+                                token.ChangeCustomBool("hasSpace", true);
+                            if (char.IsLetter(ch))
+                                token.ChangeCustomBool("hasLetter", true);
+                            return false;
+                        },
+        new List<Func<Token, List<Token>, bool>>
+                        {
+                            (token, tokens) => token.Parent?.Tag?.HtmlTag != "em",
+                        }),
+
+                new Tag("_", "_", "em",
+                    new [] {"hasSpace", "hasLetter"},
+            (tag, token, text, startInd) =>
+                            text.TryGetChar(startInd + tag.OpenMark.Length, out var ch1)
+                            && !char.IsWhiteSpace(ch1), 
+           (tag, token, text, endInd) =>
+                            text.TryGetChar(endInd - 1, out var ch1)
+                            && !char.IsWhiteSpace(ch1),
+          (tag, token, text, endInd) =>
+                            token.CustomBools["hasLetter"] &&
+                            (!(text.TryGetChar(token.StartOpenMark - 1, out var ch1)
+                               && !char.IsWhiteSpace(ch1)
+                               || text.TryGetChar(endInd + tag.CloseMark.Length, out var ch2)
+                               && !char.IsWhiteSpace(ch2))
+                             || !token.CustomBools["hasSpace"]),
+        (tag, token, ch) =>
+                        {
+                            if (char.IsWhiteSpace(ch))
+                                token.ChangeCustomBool("hasSpace", true);
+                            if (char.IsLetter(ch))
+                                token.ChangeCustomBool("hasLetter", true);
+                            return false;
+                        },
+        new List<Func<Token, List<Token>, bool>>()),
+
+                new Tag("# ", Environment.NewLine, "h1",
+                    Array.Empty<string>(),
+                    (tag, token, text, startInd) =>
+                        {
+                            for (int i = startInd; i > 0; i--)
+                                if (!char.IsWhiteSpace(text[i]))
+                                    return false;
+                            return true;
+                        },
+                    (tag, token, text, endInd) => true,
+                    (tag, token, text, endInd) => true,
+                    (tag, token, ch) => false,
+                    new List<Func<Token, List<Token>, bool>>())
+            };
         }
 
         public List<Token> ParseTokens(string text)
         {
-            var tokens = FindAllTokens(text);
+            var tokens = FindTokens(text);
+            RemoveUnreadyParents(tokens);
             tokens = RemoveIncorrectInterractedTokens(tokens);
-            return tokens;
+            return tokens
+                .OrderBy(t => t.StartOpenMark)
+                .ToList();
         }
 
         private List<Token> RemoveIncorrectInterractedTokens(List<Token> tokens)
         {
             return tokens
-                .Where(t => 
+                .Where(t =>
                     t.Tag.TagInteractionRules
                         .All(rule => rule(t, tokens)))
                 .ToList();
         }
-        
-        private List<Token> FindAllTokens(string text)
-        {
-            var tokens = new List<Token>();
-            foreach (var tag in Tags)
-            {
-                var (startIndices, endIndices) = FindAllIndices(tag, text);
-                var rightestIndex = -1;
-                foreach (var openMarkInd in startIndices)
-                {
-                    if (openMarkInd <= rightestIndex) continue;
-                    foreach (var closeMarkInd in endIndices)
-                    {
-                        if (closeMarkInd <= openMarkInd) continue;
 
-                        var textStart = openMarkInd + tag.OpenMark.Length;
-                        var textEnd = closeMarkInd;
-                        var txt = text.Substring(textStart, textEnd - textStart);
-                        var isPartial = text.TryGetChar(openMarkInd - 1, out var ch) && ch != ' ';
-                        if (!tag.TextContentRules.All(f => 
-                                f(tag, isPartial, txt)))
-                            continue;
-                        rightestIndex = closeMarkInd;
-                        tokens.Add(new Token(tag, openMarkInd, closeMarkInd + tag.CloseMark.Length - 1));
-                        break;
+        private List<Token> FindTokens(string text)
+        {
+            List<Token> tokens = new List<Token>();
+            var current = new Token(null!, null!, 0);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '\\')
+                {
+                    i++;
+                    continue;
+                }
+
+                if (!IsMarkHere(text, i, out var tag, out var isStart))
+                {
+                    if (current.ProcessChar(text[i]))
+                        current = current.Parent;
+                    continue;
+                }
+
+                if (!isStart)
+                {
+                    if (current.TryCloseToken(tag, text, ref i, tokens))
+                    {
+                        while (current.EndCloseMark != -1)
+                            current = current.Parent;
+                        continue;
                     }
                 }
+                current = current.TryOpenChildToken(tag, text, ref i);
             }
             return tokens;
         }
 
-        private (List<int>, List<int>) FindAllIndices(Tag tag, string text)
+        private void RemoveUnreadyParents(List<Token> tokens)
         {
-            var startIndices = FindUnusedIndices(text, tag.OpenMark);
-            var endIndices = tag.OpenMark != tag.CloseMark ? 
-                FindUnusedIndices(text, tag.CloseMark)
-                : startIndices;
-
-            if (tag.CloseMark == Environment.NewLine)
-                endIndices.Add(text.Length);
-            return (startIndices, endIndices);
+            foreach (var token in tokens)
+            {
+                var cur = token;
+                while (cur.Parent?.EndCloseMark == -1)
+                    cur = cur.Parent;
+                token.Parent = cur.Parent;
+            }
         }
 
-        private List<int> FindUnusedIndices(string text, string toFind)
+        private bool IsMarkHere(string text, int index, out Tag tag, out bool isStart)
         {
-            var indices = text
-                .AllIndicesOf(toFind)
-                .Where(ind =>
-                    !usedIndices.Contains(ind)
-                    && text.CountOfCharBeforeIndex('\\', ind) % 2 == 0)
-                .ToList();
+            foreach (var t in Tags)
+            {
+                if (text.ContainsItOnIndex(t.CloseMark, index))
+                {
+                    isStart = false;
+                    tag = t;
+                    return true;
+                }
 
-            foreach (var index in indices)
-                AddIndicesToUsed(index, toFind.Length);
+                if (text.ContainsItOnIndex(t.OpenMark, index))
+                {
+                    isStart = true;
+                    tag = t;
+                    return true;
+                }
+            }
 
-            return indices;
-        }
-
-        private void AddIndicesToUsed(int startIndex, int length)
-        {
-            for (int i = startIndex; i < startIndex + length; i++)
-                usedIndices.Add(i);
+            tag = null!;
+            isStart = false;
+            return false;
         }
     }
 }
