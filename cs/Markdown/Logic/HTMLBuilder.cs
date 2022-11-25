0c031d56288ac893ed34343f00ede47d8c00cc1e
massshasch <massshasch@gmail.com>
2022-11-25 17:11:46 +0500

    Made builder

--- cs/Markdown/Logic/HTMLBuilder.cs
+++ cs/Markdown/Logic/HTMLBuilder.cs
@@ -1,14 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Markdown.DataStructures;
 
 namespace Markdown.Logic
 {
     public class HTMLBuilder : IHTMLBuilder
     {
-        public string Build(string text, IEnumerable<Token> tokens, List<int> escapeSymbolsIndexes)
+        public string Build(string text, TokensTree tree, List<int> escapeSymbolsIndexes)
         {
-            throw new NotImplementedException();
+            StringBuilder builder = new StringBuilder();
+            Dfs(text, tree.RootToken, escapeSymbolsIndexes, builder);
+            return builder.ToString();
+        }
+
+        public void Dfs(string text, Token token, List<int> escapeSymbolsIndexes, StringBuilder builder)
+        {
+            if (token.Tag != null)
+            builder.Append(token.Tag.OpeningTag);
+            for (int i = token.StartIndex; i <= token.EndIndex; i++)
+            {
+                if (escapeSymbolsIndexes.Contains(i))
+                    continue;
+                if (token.Children.Any(c => c.StartIndex - c.Tag.MarkdownName.Length == i))
+                {
+                    var f = token.Children.First(c => c.StartIndex - c.Tag.MarkdownName.Length == i);
+                    Dfs(text, f, escapeSymbolsIndexes, builder);
+                    i = f.EndIndex + f.Tag.MarkdownName.Length;
+                }
+                else
+                    builder.Append(text[i]);
+            }
+
+            if (token.Tag != null)
+                builder.Append(token.Tag.ClosingTag);
         }
     }
 }
