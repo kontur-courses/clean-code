19e69f52203702709e43db687625d6b6556a3012
Igor Sotarev <igor@sotarev.ru>
2022-11-25 17:44:33 +0500

    Fix header specs in tests, refactor timed test

--- cs/Markdown.Tests/MdTests.cs
+++ cs/Markdown.Tests/MdTests.cs
@@ -3,9 +3,11 @@ using NUnit.Framework;
 
 namespace Markdown.Tests;
 
+[TestFixture]
+[Parallelizable]
 public class MdTests
 {
-    [TestCase("# heading", "<h1> heading</h1>")]
+    [TestCase("# heading", "<h1>heading</h1>")]
     [TestCase("_italics_", "<em>italics</em>")]
     [TestCase("__bold__", "<strong>bold</strong>")]
     public void Should_RenderSingleTag(string input, string expectedResult)
@@ -14,23 +16,23 @@ public class MdTests
         var result = converter.Render(input);
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
-    [TestCase("# heading _italics_", "<h1> heading <em>italics</em></h1>")]
-    [TestCase("# heading __bold__", "<h1> heading <strong>bold</strong></h1>")]
+
+    [TestCase("# heading _italics_", "<h1>heading <em>italics</em></h1>")]
+    [TestCase("# heading __bold__", "<h1>heading <strong>bold</strong></h1>")]
     [TestCase("__bold _italics___", "<strong>bold <em>italics</em></strong>")]
-    [TestCase("# heading __bold _italics___", "<h1> heading <strong>bold <em>italics</em></strong></h1>")]
+    [TestCase("# heading __bold _italics___", "<h1>heading <strong>bold <em>italics</em></strong></h1>")]
     public void Should_RenderNestedTags(string input, string expectedResult)
     {
         var converter = new Md();
         var result = converter.Render(input);
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
-    [TestCase("# heading\r\n", "<h1> heading\r\n</h1>")]
+
+    [TestCase("# heading\r\n", "<h1>heading\r\n</h1>")]
     [TestCase("_italics_\r\n", "<em>italics</em>\r\n")]
     [TestCase("__bold__\r\n", "<strong>bold</strong>\r\n")]
-    [TestCase("# heading\r\n _italics_", "<h1> heading\r\n</h1> <em>italics</em>")]
-    [TestCase("# heading\r\n __bold__", "<h1> heading\r\n</h1> <strong>bold</strong>")]
+    [TestCase("# heading\r\n _italics_", "<h1>heading\r\n</h1> <em>italics</em>")]
+    [TestCase("# heading\r\n __bold__", "<h1>heading\r\n</h1> <strong>bold</strong>")]
     public void Should_RenderTextWithWhitespace(string input, string expectedResult)
     {
         var converter = new Md();
@@ -49,7 +51,7 @@ public class MdTests
         var result = converter.Render(input);
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
+
     [TestCase("_italics\r\n_", "_italics\r\n_")]
     [TestCase("__italics\r\n__", "__italics\r\n__")]
     public void ShouldNot_RenderSeparatedTags(string input, string expectedResult)
@@ -58,7 +60,7 @@ public class MdTests
         var result = converter.Render(input);
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
+
     [TestCase("_aaa__bbb_ccc__", "_aaa__bbb_ccc__")]
     [TestCase("__aaa_bbb__ccc_", "__aaa_bbb__ccc_")]
     public void ShouldNot_RenderIntersectingTags(string input, string expectedResult)
@@ -67,7 +69,7 @@ public class MdTests
         var result = converter.Render(input);
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
+
     [TestCase("123_456_789", "123_456_789")]
     [TestCase("123__456__789", "123__456__789")]
     public void ShouldNot_RenderTagsBetweenNumbers(string input, string expectedResult)
@@ -79,13 +81,13 @@ public class MdTests
 
     [TestCase("# afa")]
     [TestCase("# heading __strong _strong em_ strong__ heading _em_ __y_afa_ ghg _jkj_u__\r\n")]
+    [TestCaseSource(nameof(GetSpecificationText))]
     public void Should_BeLinear(string input)
     {
         var sw = new Stopwatch();
         sw.Reset();
 
         var converter = new Md();
-        GC.Collect();
         sw.Start();
         converter.Render(input);
         sw.Stop();
@@ -94,11 +96,21 @@ public class MdTests
 
         var text = Enumerable.Repeat(input, 10).ToString();
 
-        GC.Collect();
         sw.Start();
         converter.Render(text!);
         sw.Stop();
 
         Assert.That(sw.Elapsed.Ticks / elapsed, Is.LessThanOrEqualTo(10));
+    }
+
+    private static IEnumerable<string> GetSpecificationText()
+    {
+        var directory = Directory.GetParent(TestContext.CurrentContext.TestDirectory)!.Parent!.Parent!.Parent!.Parent!
+            .ToString();
+        const string file = "MarkdownSpec.md";
+        var fullName = Path.Combine(directory, file);
+        var task = File.ReadAllTextAsync(fullName);
+        var result = task.Result;
+        yield return result;
     }
 }
