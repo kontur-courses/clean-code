699a29d64984abd6151d362f5c1b1ffa7a518d13
Ruden <karimov.ruden@mail.ru>
2022-11-25 11:46:58 +0300

    refactor Md.cs

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -1,20 +1,34 @@
-﻿using Markdown.Tokens;
+﻿using Markdown.Enums;
+using Markdown.Interfaces;
+using Markdown.Tokens;
 
 namespace Markdown
 {
-    public static class Md
+    public class Md
     {
-        public static string Render(string markdown)
+        private IConverter _converter;
+
+        public Md(IConverter converter)
+        {
+            _converter = converter;
+        }
+        public string Render(string markdown)
         {
             if (markdown == null)
                 throw new ArgumentNullException();
+
             var lines = markdown.Split('\n');
 
             for (int i = 0; i < lines.Length; i++)
             {
-                var tokenizer = new Tokenizer();
-                var tokens = tokenizer.TokenizeLine(lines[i]);
-                lines[i] = HtmlConverter.ConvertTokensToHtml(tokens);
+                var tagCondition = new TagCondition();
+                var tokenTyper = new TokenTyper(lines[i], tagCondition);
+                var tokenBuilder = new TokenBuilder(tagCondition);
+                var tokenSetter = new TokenSetter(tokenBuilder, tagCondition);
+
+                var tokenizer = new Tokenizer<TokenType>(lines[i], tokenSetter, tokenTyper);
+                var tokens = tokenizer.TokenizeLine();
+                lines[i] = _converter.ConvertTokens(tokens);
             }
 
             return string.Join('\n', lines);
