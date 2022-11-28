2c9def95e39b23e9a762de55b6fd199ad4bd304f
Luvr681 <secondgoldvip@gmail.com>
2022-11-28 08:33:08 +0500

    Переименовал проект тестов

--- cs/Markdown/Program.cs
+++ cs/Markdown/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Markdown;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("It works"); 
+    }
+}
