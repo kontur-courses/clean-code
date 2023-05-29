f4769fe7a4fb1fd79158bfaa4600f79ca06c3496
kostya05983 <kostya05983@mail.ru>
2023-05-30 00:56:59 +0700

    Перевёл isbn13 и snils

---
+++ java/src/main/java/ru/kontur/courses/control/digit/snils/Snils.java
@@ -0,0 +1,7 @@
+package ru.kontur.courses.control.digit.snils;
+
+public class Snils {
+    public static int calculate(long number) {
+        throw new RuntimeException("Not implemented");
+    }
+}
