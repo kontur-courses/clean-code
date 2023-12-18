c2568c8e04696f257dd20611b36269c60050507e
daHil <ssocmain@gmail.com>
2023-12-18 17:45:41 +0500

    added parser realization

--- cs/Markdown/Markdown/Program.cs
+++ cs/Markdown/Markdown/Program.cs
@@ -5,7 +5,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var text = "# Заголовок __с _разными_ символами__";
+        var text = "# Заголовок __с _разными_ символами__\n _SomeText_";
         var escapeCharacters = new HashSet<string> { "\\" };
         var parser = new MarkdownParser(escapeCharacters, MarkdownTagsContainer.GetTags());
         var htmlText = Markdown.Markdown.Render(text, parser);
