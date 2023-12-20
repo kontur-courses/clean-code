b8909616701809c8be58671447287c5a392f6201
IlyaO <rikgogs@gmail.com>
2023-12-20 11:21:37 +0500

    add testcases names

--- cs/Markdown.Tests/Markdown_Should.cs
+++ cs/Markdown.Tests/Markdown_Should.cs
@@ -24,58 +24,96 @@ namespace Markdown.Tests
             action.Should().Throw<ArgumentException>();
         }
 
-        [TestCase("_Hello_", "<em>Hello</em>")]
-        [TestCase("# Hello", "<h1>Hello</h1>")]
-        [TestCase("__Hello__", "<strong>Hello</strong>")]
+        [TestCase("_Hello_", "<em>Hello</em>", 
+            TestName = "Correct conversion of a italic tags")]
+        [TestCase("# Hello", "<h1> Hello</h1>", 
+            TestName = "Correct conversion of a header tags")]
+        [TestCase("__Hello__", "<strong>Hello</strong>", 
+            TestName = "Correct conversion of bold tags")]
 
-        [TestCase("_ Hello_", "_ Hello_")]
-        [TestCase("_Hello _", "_Hello _")]
-        [TestCase("__Hello __", "__Hello __")]
-        [TestCase("__ Hello__", "__ Hello__")]
-        [TestCase("#Hello", "#Hello")]
+        [TestCase("_ Hello_", "_ Hello_", 
+            TestName = "Correct conversion when there is a space after the opening italic tag")]
+        [TestCase("_Hello _", "_Hello _", 
+            TestName = "Correct conversion if there is a space before the closing italic tag")]
+        [TestCase("__Hello __", "__Hello __", 
+            TestName = "Correct conversion if there is a space before the closing bold tag")]
+        [TestCase("__ Hello__", "__ Hello__",
+            TestName = "Correct conversion when there is a space after the opening bold tag")]
+        [TestCase("#Hello", "#Hello", 
+            TestName = "Correct conversion if there is no space after the header tag")]
 
-        [TestCase("_Hello", "_Hello")]
-        [TestCase("Hello_", "Hello_")]
-        [TestCase("__Hello", "__Hello")]
-        [TestCase("Hello__", "Hello__")]
-        [TestCase("Hello__", "Hello__")]
+        [TestCase("_Hello", "_Hello", 
+            TestName = "Correct conversion if there is no closing italic tag")]
+        [TestCase("Hello_", "Hello_", 
+            TestName = "Correct conversion if there is no opening italic tag")]
+        [TestCase("__Hello", "__Hello", 
+            TestName = "Correct conversion if there is no closing bold tag")]
+        [TestCase("Hello__", "Hello__", 
+            TestName = "Correct conversion if there is no opening bold tag")]
 
-        [TestCase("_Hello_world_", "<em>Hello</em>world_")]
-        [TestCase("_Hello_wor_ld_", "<em>Hello</em>wor<em>ld</em>")]
-        [TestCase("_111_", "<em>111</em>")]
-        [TestCase("1_1Hellow_", "1_1Hello_")]
-        [TestCase("ра_зных сл_овах", "ра_зных сл_овах")]
-        [TestCase("_разных сл_овах", "_разных сл_овах")]
-        [TestCase("ра_зных словах_", "ра_зных словах_")]
+        [TestCase("_Hello_world_", "<em>Hello</em>world_", 
+            TestName = "Correct conversion if there are several italic tags and there are extra ones")]
+        [TestCase("_Hello_wor_ld_", "<em>Hello</em>wor<em>ld</em>", 
+            TestName = "Correct conversion if there are several tags in italics and all are working")]
 
-        [TestCase("_окруженный с двух сторон_", "<em>окруженный с двух сторон</em>")]
-        [TestCase("_нач_але, и в сер_еди_не, и в кон_це._", "<em>нач</em>але, и в сер<em>еди</em>не, и в кон<em>це.</em>")]
-        [TestCase("Hello1_2 word _ word_", "<em>Hello1_2 word _ word</em>")]
+        [TestCase("ра_зных сл_овах", "ра_зных сл_овах", 
+            TestName = "No highlighting if the italic tags are in different words")]
+        [TestCase("_разных сл_овах", "_разных сл_овах", 
+            TestName = "No highlighting if the italic tags are in different words")]
+        [TestCase("ра_зных словах_", "ра_зных словах_", 
+            TestName = "No highlighting if the  italic tags are in different words")]
+        [TestCase("ра__зных сл__овах", "ра__зных сл__овах", 
+            TestName = "No highlighting if the bold tags are in different words")]
+        [TestCase("__разных сл__овах", "__разных сл__овах", 
+            TestName = "No highlighting if the bold tags are in different words")]
+        [TestCase("ра__зных словах__", "ра__зных словах__", 
+            TestName = "No highlighting if the bold tags are in different words")]
 
-        [TestCase("__", "__")]
-        [TestCase("____", "____")]
-        [TestCase("__ __", "__ __")]
+        [TestCase("_окруженный с двух сторон_", "<em>окруженный с двух сторон</em>", 
+            TestName = "Correct tagging there are several words highlighted")]
+        [TestCase("_нач_але, и в сер_еди_не, и в кон_це._", "<em>нач</em>але, и в сер<em>еди</em>не, и в кон<em>це.</em>",
+            TestName = "Correct highlighting if italic tags highlight part of a word")]
+        [TestCase("__нач__але, и в сер__еди__не, и в кон__це.__",
+            "<strong>нач</strong>але, и в сер<strong>еди</strong>не, и в кон<strong>це.</strong>",
+             TestName = "Correct highlighting if bold tags highlight part of a word")]
+        [TestCase("Hello1_2 word_", "Hello1_2 word_", 
+            TestName = "The correct definition of the handwriting of the numbers inside the word")]
 
-        [TestCase("__Hello__world__", "<strong>Hello</strong>world__")]
-        [TestCase("__111__", "<strong>111</strong>")]
-        [TestCase("ра__зных сл__овах", "ра__зных сл__овах")]
-        [TestCase("__разных сл__овах", "__разных сл__овах")]
-        [TestCase("ра__зных словах__", "ра__зных словах__")]
-        [TestCase("__Hello world__", "<strong>Hello world</strong>")]
-        [TestCase("__нач__але, и в сер__еди__не, и в кон__це.__", 
-            "<strong>нач</strong>але, и в сер<strong>еди</strong>не, и в кон<strong>це.</strong>")]
+        [TestCase("__", "__",
+            TestName = "Don't convert tags if there is nothing inside them")]
+        [TestCase("____", "____", 
+            TestName = "Don't convert tags if there is nothing inside them")]
+        [TestCase("__ __", "__ __", 
+            TestName = "Don't convert tags if there is only a space inside them")]
 
-        [TestCase("Внутри __двойн выдел _одина_ тоже__ раб","Внутри <strong>двойн выдел <em>одина</em> тоже</strong> раб")]
-        [TestCase("__пересечения _двойных__ и одинарных_", "__пересечения _двойных__ и одинарных_")]
-        [TestCase("Внутри _одинарного __двойное__ не_ работает", "Внутри <em>одинарного __двойное__ не</em> работает")]
-        [TestCase("__Непарные_ символы", "__Непарные_ символы")]
+        [TestCase("__Hello__world__", "<strong>Hello</strong>world__", 
+            TestName = "Correct conversion if there are several bold tags and there are extra ones")]
+        [TestCase("_111_", "<em>111</em>", 
+            TestName = "Correct italic tagging of a number")]
+        [TestCase("__111__", "<strong>111</strong>", 
+            TestName = "Correct bold tagging of a number")]
+        [TestCase("__Hello world__", "<strong>Hello world</strong>", 
+            TestName = "Correct conversion if bold tags highlight multiple words")]
 
-        [TestCase(@"\Hello\", @"\Hello\")]
-        [TestCase(@"\_Hello_", @"_Hello_")]
-        [TestCase(@"\__Hello__", @"__Hello__")]
-        [TestCase(@"\\_Hello_", @"\<em>Hello</em>")]
-        [TestCase(@"\\__Hello__", @"\<strong>Hello</strong>")]
-        [TestCase(@"\# Hello", @"# Hello")]
+        [TestCase("Внутри __двойн выдел _одина_ тоже__ раб","Внутри <strong>двойн выдел <em>одина</em> тоже</strong> раб",
+            TestName = "Correct converting if there are italic tags inside bold tags")]
+        [TestCase("Внутри _одинарного __двойное__ не_ работает", "Внутри <em>одинарного __двойное__ не</em> работает",
+            TestName = "Correct converting if there are bold tags inside italic tags")]
+        [TestCase("__пересечения _двойных__ и одинарных_", "__пересечения _двойных__ и одинарных_", 
+            TestName = "Correct conversion if there is an intersection of tags")]
+        [TestCase("__Непарные_ символы", "__Непарные_ символы", 
+            TestName = "Correct conversion of unpaired tags")]
+
+        [TestCase(@"\Hello\", @"\Hello\", 
+            TestName = "Correct conversion if the escape symbol does not escape anything")]
+        [TestCase(@"\_Hello_", @"_Hello_", 
+            TestName = "Correct conversion if the escaping character escapes the italic tag")]
+        [TestCase(@"\__Hello__", @"__Hello__", 
+            TestName = "Correct conversion if the escaping character escapes the bold tag")]
+        [TestCase(@"\\_Hello_", @"\<em>Hello</em>", 
+            TestName = "Correct conversion if the escaping character escapes another escaping character")]
+        [TestCase(@"\# Hello", @"# Hello", 
+            TestName = "Correct conversion if the escaping character escapes the header tag")]
         public void Correct_Render_When_ValidInputString(string markdownText, string htmlText)
         {
             var converter = new HtmlConverter();
