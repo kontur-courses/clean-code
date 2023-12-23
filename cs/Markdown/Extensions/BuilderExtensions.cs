cd0c50529d3f4d1985165a490f8d05e3208cc11a
Arman Mukanov <mukanovarman777@gmail.com>
2023-12-23 10:56:27 +0500

    feat: вспомогательная сущность

---
+++ cs/Markdown/Extensions/BuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace Markdown.Extensions;
+
+public static class BuilderExtensions
+{
+    public static void AppendEscapedCharacter(this StringBuilder builder, string text, int index)
+    {
+        builder.Append(index + 1 == text.Length ? '\\' : text[index + 1]);
+    }
+}
---
+++ cs/Markdown/StringManipulator.cs
@@ -0,0 +1,33 @@
+﻿namespace Markdown;
+
+public class StringManipulator
+{
+    public string OldString { get; }
+    private int EndInnerString { get; set; }
+    private int StartInnerString { get; set; }
+    public string Content { get; private set; }
+
+    public StringManipulator(string content, string oldString, int startInnerString, int endInnerString)
+    {
+        if (startInnerString < 0 || content.Length <= startInnerString)
+            throw new ArgumentOutOfRangeException(nameof(startInnerString));
+
+        if (endInnerString < 0 || content.Length <= endInnerString || endInnerString < startInnerString)
+            throw new ArgumentOutOfRangeException(nameof(endInnerString));
+
+        Content = content;
+        OldString = oldString;
+        StartInnerString = startInnerString;
+        EndInnerString = endInnerString;
+    }
+
+    public string GetInnerString() => Content.Substring(StartInnerString, EndInnerString - StartInnerString);
+
+    public void ReplaceInnerString(string text)
+    {
+        Content = Content[..StartInnerString] + text + Content[EndInnerString..];
+        EndInnerString += text.Length - (EndInnerString - StartInnerString);
+    }
+
+    public static StringManipulator Default(string text) => new(text, text, 0, text.Length);
+}
