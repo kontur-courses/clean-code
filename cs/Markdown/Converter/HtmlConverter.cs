0955047c635d9f709223366eaf4ea8e9100114be
rikgo <rikgogs@gmail.com>
2024-02-05 20:53:15 +0500

    refactor: divide the methods in HtmlConverter into sub-methods

--- cs/Markdown/Converter/HtmlConverter.cs
+++ cs/Markdown/Converter/HtmlConverter.cs
@@ -1,37 +1,51 @@
 ﻿using System.Text;
 using Markdown.Tags;
+using Microsoft.VisualStudio.CodeCoverage;
 
 namespace Markdown.Converter
 {
     public class HtmlConverter : IHtmlConverter
     {
         private Dictionary<Tag, string> htmlTagsMarkupDict = MarkdownConfig.HtmlTags;
+        private int shift;
 
         public string ConvertFromMarkdownToHtml(string markdownText, List<Token> tokens)
         {
             var htmlResultText = new StringBuilder(markdownText);
             var htmlTags = ConvertToHtmlTags(tokens);
-            var shift = 0;
+            shift = 0;
 
             foreach (var tag in htmlTags)
             {
-                var mdTaglength = 1;
-                if (tag.Tag == Tag.Bold)
-                {
-                    mdTaglength = 2;
-                    shift--;
-                }
-                if (tag.IsClosing && (tag.Tag == Tag.Header || tag.Tag == Tag.EscapedSymbol)) 
-                {
-                    mdTaglength = 0;
-                    shift++;
-                }
-                htmlResultText.Remove(tag.Index + shift, mdTaglength);
-                htmlResultText.Insert(tag.Index + shift, tag.GetMarkup());
+                ReplaceMarkdownWithHtml(htmlResultText, tag);
                 shift = htmlResultText.Length - markdownText.Length;
             }
 
             return htmlResultText.ToString();
+        }
+
+        private void ReplaceMarkdownWithHtml(StringBuilder htmlResultText, HtmlTag tag)
+        {
+            var mdTagLength = GetMdTagLength(tag);
+            htmlResultText.Remove(tag.Index + shift, mdTagLength);
+            htmlResultText.Insert(tag.Index + shift, tag.GetMarkup());
+        }
+
+        private int GetMdTagLength(HtmlTag tag)
+        {
+            if (tag.Tag == Tag.Bold)
+            {
+                shift--;
+                return 2;
+            }
+
+            if (tag.IsClosing && (tag.Tag == Tag.Header || tag.Tag == Tag.EscapedSymbol))
+            {
+                shift++;
+                return 0;
+            }
+
+            return 1;
         }
 
         private List<HtmlTag> ConvertToHtmlTags(List<Token> tokens)
@@ -40,11 +54,16 @@ namespace Markdown.Converter
 
             foreach (var token in tokens)
             {
-                htmlTags.Add(new HtmlTag(token.TagType, token.StartIndex, false, htmlTagsMarkupDict[token.TagType]));
-                htmlTags.Add(new HtmlTag(token.TagType, token.EndIndex , true, htmlTagsMarkupDict[token.TagType]));
+                AddHtmlTag(htmlTags, token.TagType, token.StartIndex, false, htmlTagsMarkupDict[token.TagType]);
+                AddHtmlTag(htmlTags, token.TagType, token.EndIndex , true, htmlTagsMarkupDict[token.TagType]);
             }
 
             return htmlTags.OrderBy(tag => tag.Index).ToList();
+        }
+
+        private void AddHtmlTag(List<HtmlTag> htmlTags, Tag tagType, int index, bool isClosing, string htmlMarkup)
+        {
+            htmlTags.Add(new HtmlTag(tagType, index, isClosing, htmlMarkup));
         }
     }
 }
