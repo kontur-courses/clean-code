3b24b567019168e6775e9421f09a18bd92f0500b
RRAleshev <artbetep@yandex.ru>
2024-02-15 10:37:27 +0500

    refactor: HtmlProcessor

--- cs/MarkdownTask/HtmlTools/HtmlProcessor.cs
+++ cs/MarkdownTask/HtmlTools/HtmlProcessor.cs
@@ -5,34 +5,53 @@ namespace MarkdownTask.HtmlTools
 {
     public static class HtmlProcessor
     {
-        public static string Process(string text, ICollection<Token> tokens)
+        public static string Process(string inputText, ICollection<Token> tokens)
         {
-            var sb = new StringBuilder(text);
-            var shift = 0;
+            var processedText = new StringBuilder(inputText);
+            var shiftOffset = 0;
 
             foreach (var token in TokensProcessing.Process(tokens))
             {
-                sb.Remove(token.Position + shift, token.TagLength);
+                processedText.Remove(token.Position + shiftOffset, token.TagLength);
+                processedText.Insert(token.Position + shiftOffset, GetHtlmTag(inputText, token));
 
-                if (token.TagType == TagType.Link)
-                {
-                    sb.Insert(token.Position + shift, LinkTagBuilder.Build(text.Substring(token.Position, token.TagLength)));
-                    shift = sb.Length - text.Length;
-                    continue;
-                }
-
-                var htmlTag = token.TagType.GetHtmlString();
-
-                if (token.Tag == Tag.Close)
-                {
-                    htmlTag = htmlTag.Insert(1, "/");
-                }
-
-                sb.Insert(token.Position + shift, htmlTag);
-                shift = sb.Length - text.Length;
+                shiftOffset = processedText.Length - inputText.Length;
             }
 
-            return sb.ToString();
+            return processedText.ToString();
+        }
+
+        private static string GetHtlmTag(string inputText, Token token)
+        {
+            var htmlTag = String.Empty;
+
+            if (token.TagType == TagType.Link)
+            {
+                htmlTag = BuildLinkTag(inputText, token);
+            }
+            else
+            {
+                htmlTag = BuildHtmlTag(inputText, token);
+            }
+
+            return htmlTag;
+        }
+
+        private static string BuildHtmlTag(string text, Token token)
+        {
+            var htmlTag = token.TagType.GetHtmlString();
+
+            if (token.Tag == Tag.Close)
+            {
+                htmlTag = htmlTag.Insert(1, "/");
+            }
+
+            return htmlTag;
+        }
+
+        private static string BuildLinkTag(string text, Token token)
+        {
+            return LinkTagBuilder.Build(text.Substring(token.Position, token.TagLength));
         }
     }
 }
