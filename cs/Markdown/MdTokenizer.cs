4728d2cd706de762fa51921719c35167c3471f60
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-26 18:30:42 +0500

    feat(MdTokenizer.cs): теперь проходит тесты

--- cs/Markdown/MdTokenizer.cs
+++ cs/Markdown/MdTokenizer.cs
@@ -5,10 +5,24 @@ namespace Markdown;
 
 public class MdTokenizer
 {
-    private ITokenScanner[] scanners = [new SpecScanner(), new TextScanner()];
+    private readonly ITokenScanner[] scanners = [
+        new SpecScanner(), new TextScanner(), new NumberScanner()
+    ];
 
     public List<Token> Tokenize(string text)
     {
-        throw new NotImplementedException();
+        var begin = 0;
+        var tokenList = new List<Token>();
+
+        while (begin < text.Length)
+        {
+            var token = scanners
+                .Select(scanner => scanner.Scan(text, begin))
+                .First(token => token != null);
+            
+            begin += token!.Length;
+            tokenList.Add(token);
+        }
+        return tokenList;
     }
 }
