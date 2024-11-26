f9013192cb67e2b8f4b8ec2d068fbb27e8ade4ae
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-26 17:44:36 +0500

    add(SpecScannerTest.cs): добавил тесты для SpecScanner.cs

---
+++ cs/MarkdownTests/Scanners/SpecScannerTest.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using Markdown.Scanners;
+using Markdown.Tokens;
+
+namespace MarkdownTests.Scanners;
+
+[TestFixture]
+[TestOf(typeof(SpecScanner))]
+public class SpecScannerTest
+{
+
+    [TestCase(" ", 0, TokenType.Space)]
+    [TestCase("\n", 0, TokenType.Newline)]
+    [TestCase("_", 0, TokenType.Underscore)]
+    [TestCase("_abc_", 0, TokenType.Underscore)]
+    [TestCase("1234ab_", 6, TokenType.Underscore)]
+    [TestCase("#", 0, TokenType.Octothorpe)]
+    public void Scan_ShouldScanValidTokenType_WhenBeginPointsOnSpec
+        (string text, int begin, TokenType expectedType)
+    {
+        var scanner = new SpecScanner();
+        
+        var token = scanner.Scan(text, begin);
+        
+        token!.Type.Should().Be(expectedType);
+    }
+
+    [TestCase("abc", 0)]
+    [TestCase(" abc", 1)]
+    [TestCase("abc\n", 0)]
+    [TestCase("_abc_", 1)]
+    public void Scan_ShouldScanNull_WhenBeginPointsNotOnSpec
+        (string text, int begin)
+    {
+        var scanner = new SpecScanner();
+        
+        var token = scanner.Scan(text, begin);
+        
+        token.Should().BeNull();
+    }
+}
