b9e4b68ecc0505c36abf953d58655d11a4177570
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-26 17:45:12 +0500

    feat(SpecScanner.cs): теперь проходит тесты

--- cs/Markdown/Scanners/SpecScanner.cs
+++ cs/Markdown/Scanners/SpecScanner.cs
@@ -4,11 +4,27 @@ namespace Markdown.Scanners;
 
 public class SpecScanner : ITokenScanner
 {
-    public Token? Scan(string text) => text[0] switch
+    public Token? Scan(string text, int begin = 0)
     {
-        '_' => new Token(TokenType.Underscore, "_"),
-        '\n' => new Token(TokenType.Newline, "\n"),
-        '#' => new Token(TokenType.Octothorpe, "#"),
+        var tokenType = GetTokenType(text[begin]);
+        
+        if (tokenType is null)
+        {
+            return null;
+        }
+        var notNullType = (TokenType)tokenType;
+        
+        return new Token(notNullType, begin, 1, text);
+    }
+
+    public static bool CanScan(char symbol) => GetTokenType(symbol) != null;
+    
+    private static TokenType? GetTokenType(char symbol) => symbol switch
+    {
+        ' ' => TokenType.Space,
+        '\n' => TokenType.Newline,
+        '_' => TokenType.Underscore,
+        '#' => TokenType.Octothorpe,
         _ => null
     };
 }
