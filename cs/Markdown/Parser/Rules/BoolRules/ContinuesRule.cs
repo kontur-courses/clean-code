c559bb66dbf4dfc38d9403403f421c4fba68d87d
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-03 20:49:08 +0500

    add(ContinuesRule.cs): добавил вспомогательный рул

---
+++ cs/Markdown/Parser/Rules/BoolRules/ContinuesRule.cs
@@ -0,0 +1,28 @@
+using Markdown.Parser.Nodes;
+using Markdown.Tokens;
+
+namespace Markdown.Parser.Rules.BoolRules;
+
+public class ContinuesRule(IParsingRule rule, IParsingRule continuesRule) : IParsingRule
+{
+    public Node? Match(List<Token>? tokens, int begin = 0)
+    {
+        if (rule.Match(tokens, begin) is { } node)
+        {
+            var newBegin = begin + node.Consumed;
+            return HasRightContinues(tokens, newBegin) ? node : null;
+        }
+        
+        return null;
+    }
+
+    private bool HasRightContinues(List<Token> tokens, int begin)
+    {
+        if (tokens.Count == begin)
+        {
+            return true;
+        }
+
+        return continuesRule.Match(tokens, begin) is not null;
+    }
+}
