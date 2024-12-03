2488de8e10a9ccece6b04d40b2d5271879083554
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-03 20:04:08 +0500

    add(Tools): добавил вспомогательные ListExtension'ы

---
+++ cs/Markdown/Parser/Rules/Tools/ListOrderExtensions.cs
@@ -0,0 +1,7 @@
+namespace Markdown.Parser.Rules.Tools;
+
+public static class ListOrderExtensions
+{
+    public static T? Second<T>(this List<T> list) => list.Count < 2 ? default : list[1];
+    public static T? Third<T>(this List<T> list) => list.Count < 3 ? default : list[2];
+}
