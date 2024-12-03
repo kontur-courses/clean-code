7c5029c4aa4d01bcd53cff2127014f1325586f0a
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-03 20:08:05 +0500

    add(TextRule.cs): добавил функционал, закрепленный в тестах

---
+++ cs/Markdown/Parser/Rules/TextRule.cs
@@ -0,0 +1,19 @@
+using Markdown.Parser.Nodes;
+using Markdown.Tokens;
+
+namespace Markdown.Parser.Rules;
+
+public class TextRule : IParsingRule
+{
+    public Node? Match(List<Token> tokens, int begin = 0)
+    {
+        var textLength = tokens
+            .Skip(begin)
+            .TakeWhile(IsText)
+            .Count();
+        return textLength == 0 ? null : new TextNode(begin, textLength, tokens);
+    }
+
+    private static bool IsText(Token token) 
+        => token.TokenType is TokenType.Word or TokenType.Space;
+}
