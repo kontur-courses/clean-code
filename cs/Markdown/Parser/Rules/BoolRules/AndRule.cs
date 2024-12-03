05f77a86c4a23e3c6bfa52e8a9d6b7c397287d31
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-03 20:42:03 +0500

    feat(AndRule.cs): добавил функциональности в конструкторах AndRule.cs по аналогии с OrRule.cs

--- cs/Markdown/Parser/Rules/BoolRules/AndRule.cs
+++ cs/Markdown/Parser/Rules/BoolRules/AndRule.cs
@@ -1,13 +1,27 @@
 using Markdown.Parser.Nodes;
+using Markdown.Parser.Rules.Tools;
 using Markdown.Tokens;
 
 namespace Markdown.Parser.Rules.BoolRules;
 
-public class AndRule(IParsingRule firstRule, IParsingRule secondRule) : IParsingRule
+public class AndRule(List<IParsingRule> pattern) : IParsingRule
 {
+    public AndRule(IParsingRule firstRule, IParsingRule secondRule) :
+        this([firstRule, secondRule])
+    {
+        
+    }
+
+    public AndRule(TokenType firstType, TokenType secondType) :
+        this([new PatternRule(firstType), new PatternRule(secondType)])
+    {
+        
+    }
+    
     public Node? Match(List<Token> tokens, int begin = 0)
     {
-        var firstMatch = firstRule.Match(tokens, begin);
-        return firstMatch is null ? null : secondRule.Match(tokens, begin);
+        var nodes = tokens.MatchPattern(pattern, begin);
+        var consumed = nodes.Aggregate(0, (acc, node) => acc + node.Consumed);
+        return consumed == 0 ? null : new SpecNode(nodes, consumed);
     }
 }
