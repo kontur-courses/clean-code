5e70a07ef2cb5d666cfa8f9235812b2c88337add
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-03 20:07:14 +0500

    add(PatternRule.cs): реализовал функционал, закрепленный в тестах

---
+++ cs/Markdown/Parser/Rules/PatternRule.cs
@@ -0,0 +1,35 @@
+using Markdown.Parser.Nodes;
+using Markdown.Tokens;
+
+namespace Markdown.Parser.Rules;
+
+public class PatternRule(List<TokenType> pattern) : IParsingRule
+{
+    public PatternRule(TokenType tokenType) 
+        : this([tokenType])
+    {
+    }
+
+    public Node? Match(List<Token> tokens, int begin = 0)
+    {
+        if (pattern.Count == 0)
+        {
+            return null;
+        }
+
+        if (tokens.Count - begin < pattern.Count)
+        {
+            return null;
+        }
+
+        var isMatched = tokens
+            .Skip(begin)
+            .Take(pattern.Count)
+            .Zip(pattern)
+            .All(pair => pair.First.TokenType == pair.Second);
+        return !isMatched ? null : new TextNode(begin, pattern.Count, tokens);
+    }
+
+    public static PatternRule DoubleUnderscoreRule()
+        => new([TokenType.Underscore, TokenType.Underscore]);
+}
