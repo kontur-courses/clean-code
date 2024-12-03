d9f2de64498953931f05d118c1cc2970cc7d279e
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-03 20:36:33 +0500

    add(ParagraphRuleTest.cs): добавил тесты для ParagraphRule.cs

---
+++ cs/MarkdownTests/Parser/Rules/ParagraphRuleTest.cs
@@ -0,0 +1,64 @@
+using FluentAssertions;
+using Markdown;
+using Markdown.Parser.Nodes;
+using Markdown.Parser.Rules;
+using Markdown.Parser.Rules.Tools;
+
+namespace MarkdownTests.Parser.Rules;
+
+[TestFixture]
+[TestOf(typeof(ParagraphRule))]
+public class ParagraphRuleTest
+{
+    private readonly ParagraphRule rule = new();
+    private readonly MdTokenizer tokenizer = new();
+    
+    [TestCase("abcdefghi")]
+    [TestCase("abc def ghi ")]
+    public void Match_ShouldMatch_SimpleCase(string text)
+    {
+        var tokens = tokenizer.Tokenize($"{text}\n");
+
+        var node = rule.Match(tokens) as TagNode;
+        
+        node.Should().NotBeNull();
+        node.NodeType.Should().Be(NodeType.Paragraph);
+        node.Consumed.Should().Be(tokens.Count);
+        node.ToText().Should().Be(text);
+    }
+
+    [Test]
+    public void Match_ShouldMatch_WhenParagraphWithInnerTags()
+    {
+        const string text = "abc _def_ __ghi jkl__";
+        var tokens = tokenizer.Tokenize($"{text}\n");
+        
+        var node = rule.Match(tokens) as TagNode;
+        
+        node.Should().NotBeNull();
+        node.NodeType.Should().Be(NodeType.Paragraph);
+        
+        node.Consumed.Should().Be(tokens.Count);
+        node.Children.Select(n => n.NodeType)
+            .Should().HaveCount(4)
+            .And.BeEquivalentTo([NodeType.Text, NodeType.Italic, NodeType.Bold, NodeType.Text]);
+        
+        node.Children.First(n => n.NodeType == NodeType.Bold).ToText().Should().Be("ghi jkl");
+        node.Children.First(n => n.NodeType == NodeType.Italic).ToText().Should().Be("def");
+    }
+
+    [TestCase("_abc __def ghi_ jkl__")]
+    [TestCase("_abc __def ghi jkl")]
+    public void Match_ShouldMatchAsText_WhenInnerTagsIntersect(string text)
+    {
+        var tokens = tokenizer.Tokenize($"{text}\n");
+        
+        var node = rule.Match(tokens) as TagNode;
+        
+        node.Should().NotBeNull();
+        node.NodeType.Should().Be(NodeType.Paragraph);
+        node.Consumed.Should().Be(tokens.Count);
+        node.Children.Should().OnlyContain(n => n.NodeType == NodeType.Text);
+        node.ToText().Should().Be(text);
+    }
+}
