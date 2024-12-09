63b5adc914f0b2dfc0f46d49c9e3e4fda26fa858
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-09 21:28:04 +0500

    add(ConditionalRuleTest.cs): добавил тесты для ConditionalRule

---
+++ cs/MarkdownTests/Parser/Rules/BoolRules/ConditionalRuleTest.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using FluentAssertions;
+using Markdown;
+using Markdown.Parser.Nodes;
+using Markdown.Parser.Rules;
+using Markdown.Parser.Rules.BoolRules;
+using Markdown.Parser.Rules.Tools;
+
+namespace MarkdownTests.Parser.Rules.BoolRules;
+
+[TestFixture]
+[TestOf(typeof(ConditionalRule))]
+public class ConditionalRuleTest
+{
+    private readonly TextRule primaryRule = new();
+    private readonly MdTokenizer tokenizer = new();
+    private static IEnumerable CasesThatMatchesPrimaryRule
+    {
+        get
+        {
+            yield return new TestCaseData("abc def");
+            yield return new TestCaseData(" abc def ghi ");
+        }
+    }
+    [Test, TestCaseSource(nameof(CasesThatMatchesPrimaryRule))]
+    public void ConditionalRule_Match_ShouldMatchNodeWithRightCondition(string text)
+    {
+        var tokens = tokenizer.Tokenize(text);
+        var rule = new ConditionalRule(primaryRule, node => node.NodeType == NodeType.Text);
+        
+        var match = rule.Match(tokens);
+        match.Should().NotBeNull();
+        match.ToText().Should().Be(text);
+        match.NodeType.Should().Be(NodeType.Text);
+    }
+    
+    [Test, TestCaseSource(nameof(CasesThatMatchesPrimaryRule))]
+    public void ConditionalRule_Match_ShouldNotMatchWithWrongCondition(string text)
+    {
+        var tokens = tokenizer.Tokenize(text);
+        var rule = new ConditionalRule(primaryRule, _ => false);
+        
+        var match = rule.Match(tokens);
+        match.Should().BeNull();
+    }
+}
