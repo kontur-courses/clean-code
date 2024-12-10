cc08bb451cd7a10091ccc3d3d8a6b0270d0764fb
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-10 22:44:29 +0500

    add(ListItemRule.cs): реализовал ListItemRule.cs

---
+++ cs/Markdown/Parser/Rules/ListItemRule.cs
@@ -0,0 +1,24 @@
+using Markdown.Parser.Nodes;
+using Markdown.Parser.Rules.BoolRules;
+using Markdown.Parser.Rules.Tools;
+using Markdown.Tokens;
+
+namespace Markdown.Parser.Rules;
+
+public class ListItemRule : IParsingRule
+{
+    public Node? Match(List<Token> tokens, int begin = 0)
+    {
+        var resultRule = new AndRule([
+            new PatternRule([TokenType.Asterisk, TokenType.Space]),
+            new ParagraphRule(),
+        ]);
+        return resultRule.Match(tokens, begin) is SpecNode node ? BuildNode(node) : null;
+    }
+
+    private static TagNode BuildNode(SpecNode specNode)
+    {
+        var valueNode = (specNode.Nodes.Second() as TagNode)!;
+        return new TagNode(NodeType.ListItem, valueNode.Children, specNode.Start, specNode.Consumed);
+    }
+}
