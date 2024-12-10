c4adfd1fed6ceaa140ae6cf7a20d53aa9e9011c1
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-10 22:49:10 +0500

    feat(MdTests.cs): написал тест на производительность

--- cs/MarkdownTests/MdTests.cs
+++ cs/MarkdownTests/MdTests.cs
@@ -1,15 +1,49 @@
+using System.Diagnostics;
+using System.Text;
+using FluentAssertions;
+using Markdown;
+using Markdown.Generator;
+using Markdown.Parser.Rules;
+
 namespace MarkdownTests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
+    private readonly BodyRule rule = new();
+    private readonly HtmlGenerator generator = new();
+    private readonly MdTokenizer tokenizer = new();
+    
     [Test]
-    public void Test1()
+    public void Markdown_Render_ShouldWorkFast()
     {
-        Assert.Pass();
+        const int scale = 2;
+        var sw = new Stopwatch();
+        var results = new List<TimeSpan>();
+        for (var len = 640; len <= 5120; len *= scale)
+        {
+            var markdown = GenerateMarkdown(len);
+            sw.Start(); RenderMarkdown(markdown); sw.Stop();
+            results.Add(sw.Elapsed); sw.Reset();
+        }
+        
+        Enumerable.Range(1, results.Count - 1)
+            .Select(i => results[i].Ticks / results[i - 1].Ticks)
+            .Should().OnlyContain(timeRatio => timeRatio < scale * scale);
+    }
+    private static string GenerateMarkdown(int len)
+    {
+        var rand = new Random();
+        List<string> specElements = [" ", "_", "__", "#", "\\", Environment.NewLine];
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXY".Select(char.ToString).ToList();
+        
+        var allElements = specElements.Concat(alphabet).ToList();
+        return Enumerable.Range(0, len).Aggregate(new StringBuilder(), 
+            (sb, _) => sb.Append(allElements[rand.Next(allElements.Count)])).ToString();
+    }
+    private string RenderMarkdown(string markdown)
+    {
+        var tokens = tokenizer.Tokenize($"{markdown}\n");
+        var root = rule.Match(tokens)!;
+        return generator.Render(root, tokens);
     }
 }
