29e1fa7157f2b9f8191ff06f7706463e8d587e26
Azkraft <azanov-2004@mail.ru>
2025-11-11 14:46:36 +0500

    feat(MdToHtmlMapping.cs): separate mapping to html

---
+++ Markdown/MdToHtmlMapping.cs
@@ -0,0 +1,16 @@
+﻿namespace Markdown;
+
+public class MdToHtmlMapping
+{
+	private static readonly Dictionary<MdKeySymbol, string> mapping = new()
+	{
+		[new(MdSymbolType.Underscore, MdSymbolRelativePosition.Open, 0)] = "<em>",
+		[new(MdSymbolType.Underscore, MdSymbolRelativePosition.Close, 0)] = "</em>",
+		[new(MdSymbolType.DoubleUnderscore, MdSymbolRelativePosition.Open, 0)] = "<strong>",
+		[new(MdSymbolType.DoubleUnderscore, MdSymbolRelativePosition.Close, 0)] = "</strong>",
+		[new(MdSymbolType.NumberSign, MdSymbolRelativePosition.Open, 0)] = "<h1>",
+		[new(MdSymbolType.NumberSign, MdSymbolRelativePosition.Close, 0)] = "</h1>"
+	};
+
+	public static IReadOnlyDictionary<MdKeySymbol, string> Mapping => mapping;
+}
