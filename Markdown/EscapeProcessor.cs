0ceb0180d1102ac0eb9de67e742e7294b2a0cc44
dm7672 <dmitrsem10@gmail.com>
2025-11-28 23:56:26 +0500

    Обработчики

---
+++ Markdown/EscapeProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markdown
+{
+    internal sealed class EscapeProcessor : ITokenProcessor
+    {
+        public List<Token> Process(List<Token> tokens)
+        {
+            var result = new List<Token>();
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                var t = tokens[i];
+                if (t.Type != TokenType.Backslash)
+                {
+                    result.Add(t);
+                    continue;
+                }
+                if(i == tokens.Count - 1 || 
+                    (tokens[i + 1].Type != TokenType.DoubleUnderscore &&
+                    tokens[i + 1].Type != TokenType.Underscore &&
+                    tokens[i + 1].Type != TokenType.Backslash &&
+                    tokens[i+1].Type != TokenType.Grid))
+                {
+                    result.Add(new Token(TokenType.Text, false, false, "\\"));
+                    continue;
+                }
+
+                result.Add(new Token(TokenType.Text, false, false, tokens[i+1].Text));
+                i += 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Markdown/RenderProcess.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Markdown
+{
+    internal class RenderProcess
+    {
+        internal string Render(List<Block> blocks)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                foreach (var token in blocks[i].tokens)
+                {
+                    sb.Append(token.Text);
+                }
+                if (i != blocks.Count - 1)
+                {
+                    sb.Append('\n');
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
