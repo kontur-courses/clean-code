25eead8ac1476654af3aabdfba2aad2a26b224ca
Vladimir Golubev <gor.prast@bk.ru>
2025-12-09 02:32:06 +0500

    feat: add realization for IBlockParser

---
+++ cs/MarkupConverter/Parsers/BlockParsers/ParagraphParser.cs
@@ -0,0 +1,17 @@
+using MarkupConverter.Parsers.BlockParsers.OpenBlocks;
+
+namespace MarkupConverter.Parsers.BlockParsers;
+
+public class ParagraphParser : IBlockParser
+{
+    public bool CanParse(string line)
+    {
+        return !string.IsNullOrWhiteSpace(line);
+    }
+    public IOpenBlock Parse(string line)
+    {
+        var content = line.Trim();
+        
+        return new ParagraphOpenBlock(content);
+    }
+}
