ca8411b771c2ce1bf4953212f17f50b2111862ce
Vladimir Golubev <gor.prast@bk.ru>
2025-12-09 02:33:30 +0500

    feat: add first version of Html renderer

---
+++ cs/MarkupConverter/Renderers/HtmlRenderer.cs
@@ -0,0 +1,50 @@
+using MarkupConverter.AST.Blocks;
+using MarkupConverter.AST.Inlines;
+
+namespace MarkupConverter.Renderers;
+
+public class HtmlRenderer : IRenderer
+{
+    public string Render(Document document)
+    {
+        var htmlBlocks = document.Blocks.Select(Render);
+        return string.Join(Environment.NewLine, htmlBlocks);
+    }
+
+    private string Render(Block block)
+    {
+        return block switch
+        {
+            Header header => RenderHeader(header),
+            Paragraph paragraph => RenderParagraph(paragraph),
+            _ => throw new NotSupportedException($"Block type {block.GetType().Name} not supported")
+        };
+    }
+
+    private string RenderHeader(Header header)
+    {
+        var tag = $"h{header.Level}";
+        var content = RenderInline(header.Inlines);
+        return $"<{tag}>{content}</{tag}>";
+    }
+
+    private string RenderParagraph(Paragraph paragraph)
+    {
+        var content = RenderInline(paragraph.Inlines);
+        return $"<p>{content}</p>";
+    }
+
+
+    private string RenderInline(List<Inline> inlineContent)
+    {
+        var res = string.Empty;
+        foreach (var variabInline in inlineContent)
+        {
+            if (variabInline is Text text)
+            {
+                res = text.Content;
+            }
+        }
+        return res;
+    }
+}
